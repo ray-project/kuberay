bc37f6eaef578b65baf8cbef858c648b8b2d3532
Jiaxin Shan <seedjeffwan@gmail.com>
2021-07-06 12:06:16 -0700

    Adding RayCluster group version
    This is default output from kubebuilder without customization.
    
    `kubebuilder create api --group ray.io --version v1alpha1 --kind RayCluster`
    
    Signed-off-by: Jiaxin Shan <seedjeffwan@gmail.com>

--- bytedance/main.go
+++ bytedance/main.go
@@ -30,6 +30,9 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	rayiov1alpha1 "github.com/ray-project/ray-contrib/bytedance/api/v1alpha1"
+	"github.com/ray-project/ray-contrib/bytedance/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -41,6 +44,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(rayiov1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -74,6 +78,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controllers.RayClusterReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("RayCluster"),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "RayCluster")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
