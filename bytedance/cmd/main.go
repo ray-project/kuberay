8bbbbc36a982595fe77b7850c4cb0cecb10304bc
Jiaxin Shan <seedjeffwan@gmail.com>
2021-08-04 00:07:56 -0700

    Fix leader election problem
    Original name has a “.” which doesn’t meet requirement of  DNS-1123 (subdomain must consist of lower case alphanumeric characters,  '-' or '.', and must start and end with an alphanumeric character)

--- bytedance/cmd/main.go
+++ bytedance/cmd/main.go
@@ -71,7 +71,7 @@ func main() {
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "6adafd3f.",
+		LeaderElectionID:       "6adafd3f",
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
