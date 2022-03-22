722a0ebb6361176b1ffba4fc20adf90572651b43
chenk008 <kongchen28@gmail.com>
2022-03-23 05:57:56 +0800

    Simplify k8s client creation (#179)
    Co-authored-by: wuhua.ck <wuhua.ck@alibaba-inc.com>

--- apiserver/pkg/client/cluster.go
+++ apiserver/pkg/client/cluster.go
@@ -1,20 +1,13 @@
 package client
 
 import (
-	"path/filepath"
 	"time"
 
-	"github.com/pkg/errors"
-	"github.com/ray-project/kuberay/apiserver/pkg/util"
-	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
-
-	"github.com/cenkalti/backoff"
 	"github.com/golang/glog"
+	"github.com/ray-project/kuberay/apiserver/pkg/util"
 	rayclusterclient "github.com/ray-project/kuberay/ray-operator/pkg/client/clientset/versioned"
 	rayiov1alpha1 "github.com/ray-project/kuberay/ray-operator/pkg/client/clientset/versioned/typed/raycluster/v1alpha1"
-	"k8s.io/klog/v2"
+	"sigs.k8s.io/controller-runtime/pkg/client/config"
 )
 
 type ClusterClientInterface interface {
@@ -30,51 +23,13 @@ func (cc RayClusterClient) RayClusterClient(namespace string) rayiov1alpha1.RayC
 }
 
 func NewRayClusterClientOrFatal(initConnectionTimeout time.Duration, options util.ClientOptions) ClusterClientInterface {
-	var rayClusterClient rayiov1alpha1.RayV1alpha1Interface
-	var err error
-	var operation = func() error {
-		// in cluster
-		restConfig, err := rest.InClusterConfig()
-		if err != nil {
-			return errors.Wrap(err, "Failed to initialize RestConfig.")
-		}
-		restConfig.QPS = options.QPS
-		restConfig.Burst = options.Burst
-
-		rayClusterClient = rayclusterclient.NewForConfigOrDie(restConfig).RayV1alpha1()
-		return nil
-	}
-
-	// out of cluster
-	rayClusterClient, err = newOutOfClusterRayClusterClient()
-	if err == nil {
-		return &RayClusterClient{client: rayClusterClient}
-	}
-	klog.Infof("(Expected when in cluster) Failed to create RayCluster client by out of cluster kubeconfig. Error: %v", err)
-
-	klog.Infof("Starting to create RayCluster client by in cluster config.")
-	b := backoff.NewExponentialBackOff()
-	b.MaxElapsedTime = initConnectionTimeout
-	err = backoff.Retry(operation, b)
-
+	cfg, err := config.GetConfig()
 	if err != nil {
-		glog.Fatalf("Failed to create TokenReview client. Error: %v", err)
+		glog.Fatalf("Failed to create RayCluster client. Error: %v", err)
 	}
+	cfg.QPS = options.QPS
+	cfg.Burst = options.Burst
+
+	rayClusterClient := rayclusterclient.NewForConfigOrDie(cfg).RayV1alpha1()
 	return &RayClusterClient{client: rayClusterClient}
-}
-
-func newOutOfClusterRayClusterClient() (rayiov1alpha1.RayV1alpha1Interface, error) {
-	home := homedir.HomeDir()
-	if home == "" {
-		return nil, errors.New("Cannot get home dir")
-	}
-
-	defaultKubeConfigPath := filepath.Join(home, ".kube", "config")
-	// use the current context in kubeconfig
-	config, err := clientcmd.BuildConfigFromFlags("", defaultKubeConfigPath)
-	if err != nil {
-		return nil, err
-	}
-	rayClusterClient := rayclusterclient.NewForConfigOrDie(config).RayV1alpha1()
-	return rayClusterClient, nil
 }
