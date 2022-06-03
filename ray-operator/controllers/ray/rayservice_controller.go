e80e2038d398ab1b683677fd3d409ace7e3186b6
Dmitri Gekhtman <62982571+DmitriGekhtman@users.noreply.github.com>
2022-06-03 01:54:56 -0400

    [hotfix] Fix Service account typo (#285)
    Fixes a typo in the operator's role that prevents it from managing service accounts, which in turn blocks setup of autoscaling Ray clusters.

--- ray-operator/controllers/ray/rayservice_controller.go
+++ ray-operator/controllers/ray/rayservice_controller.go
@@ -45,7 +45,7 @@ func NewRayServiceReconciler(mgr manager.Manager) *RayServiceReconciler {
 // +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=core,resources=services/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,verbs=get;list;create;update
-// +kubebuilder:rbac:groups=core,resources=serviceaccount,verbs=get;list;watch;create;delete
+// +kubebuilder:rbac:groups=core,resources=serviceaccounts,verbs=get;list;watch;create;delete
 // +kubebuilder:rbac:groups="rbac.authorization.k8s.io",resources=roles,verbs=get;list;watch;create;delete;update
 // +kubebuilder:rbac:groups="rbac.authorization.k8s.io",resources=rolebindings,verbs=get;list;watch;create;delete
 
