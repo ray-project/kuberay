360a21f772d376352058f3a994e65c3b8455db1a
Dmitri Gekhtman <dmitri.m.gekhtman@gmail.com>
2022-06-09 18:02:08 -0700

    Add RayResources field.

--- ray-operator/apis/ray/v1alpha1/raycluster_types.go
+++ ray-operator/apis/ray/v1alpha1/raycluster_types.go
@@ -35,8 +35,10 @@ type HeadGroupSpec struct {
 	Replicas *int32 `json:"replicas"`
 	// RayStartParams are the params of the start command: node-manager-port, object-store-memory, ...
 	RayStartParams map[string]string `json:"rayStartParams"`
-	// Template is the eaxct pod template used in K8s depoyments, statefulsets, etc.
+	// Template is the exact pod template used in K8s depoyments, statefulsets, etc.
 	Template v1.PodTemplateSpec `json:"template"`
+	// RayResources is a string-int map specifying custom resources and resource overrides.
+	RayResources map[string]int32 `json:"rayResources"`
 }
 
 // WorkerGroupSpec are the specs for the worker pods
