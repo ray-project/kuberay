b6b00c87722d7b85274889ffc0477f34b3ee6f76
jasoonn <35865970+jasoonn@users.noreply.github.com>
2022-10-08 18:34:17 -0700

    [Feature] Docker support for chart-testing (#623)
    * update ct script
    
    * split long command
    
    * Update DEVELOPMENT.md
    
    Signed-off-by: jasoonn <35865970+jasoonn@users.noreply.github.com>
    
    Signed-off-by: jasoonn <35865970+jasoonn@users.noreply.github.com>
    Co-authored-by: Ubuntu <azureuser@Ubuntu.pqxb1uggpgbehcpt4orv5untcb.rx.internal.cloudapp.net>

--- helm-chart/script/chart-test.sh
+++ helm-chart/script/chart-test.sh
@@ -8,4 +8,10 @@ export CURRENT_PATH=$(cd "${PWD}">/dev/null; pwd)
 export KUBERAY_HOME=${CURRENT_PATH}/../../
 
 cd $KUBERAY_HOME
-ct lint --all --chart-dirs helm-chart/ --validate-maintainers=false
+if [ "$#" == 1 ] && [ "$1" == "local" ]; then
+  ct lint --all --chart-dirs helm-chart/ --validate-maintainers=false
+else
+  docker run -it --network host --workdir=/data --volume ~/.kube/config:/root/.kube/config:ro \
+  --volume $(pwd):/data quay.io/helmpack/chart-testing:v3.5.0 \
+  ct lint --all --chart-dirs helm-chart/ --validate-maintainers=false
+fi
