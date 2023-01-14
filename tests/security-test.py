0e6136c3bf5973f5734fb995522bb0758e8a0531
lyc <luyc58576@gmail.com>
2023-01-14 13:12:29 -0600

    get operator image from env

--- tests/security-test.py
+++ tests/security-test.py
@@ -1,4 +1,5 @@
 """Test for the Pod security standard in CI"""
+import os
 import logging
 import unittest
 import yaml
@@ -28,19 +29,16 @@ class PodSecurityTestCase(unittest.TestCase):
     """
     def test_pod_security(self):
         '''
-        Create a RayCluster pod with proper securityContext configurations and a simple pod without
-        securityContext configurations in pod-security namespace. pod-security namespace forces the
-        restricted Pod security standard to all pods. Without securityContext configurationsthe,pod
-        will be rejected since it violate the policies defined in restricted security standard.
+        The differences with the test described in the document are that this test installs
+        the operator in the default namespace and ensures pods without securityContext in
+        the pod-security namespace will be forbidden by creating a light pod instead of RayCluster.
         '''
         K8S_CLUSTER_MANAGER.delete_kind_cluster()
-        # kind_config enables audit logging with the audit policy
-        # that listen to the Pod events in the namespace pod-security
         kind_config = CONST.REPO_ROOT.joinpath("ray-operator/config/security/kind-config.yaml")
         K8S_CLUSTER_MANAGER.create_kind_cluster(kind_config = kind_config)
-        # apply the restricted Pod security standard to all Pods in the namespace pod-security.
+        # Apply the restricted Pod security standard to all Pods in the namespace pod-security.
         # The label pod-security.kubernetes.io/enforce=restricted means that
-        # the Pod that violate the policies will be rejected.
+        # The Pod that violate the policies will be rejected.
         cluster_namespace = "pod-security"
         shell_subprocess_run(f"kubectl create ns {cluster_namespace}")
         shell_subprocess_run(f"kubectl label --overwrite ns {cluster_namespace} \
@@ -53,7 +51,7 @@ class PodSecurityTestCase(unittest.TestCase):
         # Install the KubeRay operator in default namespace(for now)
         image_dict = {
             CONST.RAY_IMAGE_KEY: 'rayproject/ray-ml:2.2.0',
-            CONST.OPERATOR_IMAGE_KEY: 'kuberay/operator:nightly'
+            CONST.OPERATOR_IMAGE_KEY: os.getenv('OPERATOR_IMAGE', default='kuberay/operator:nightly'),
         }
         operator_manager = OperatorManager(image_dict)
         operator_manager.prepare_operator()
@@ -66,26 +64,21 @@ class PodSecurityTestCase(unittest.TestCase):
                 if k8s_object['kind'] == 'RayCluster':
                     context['cr'] = k8s_object
                     break
-        try:
-            #create a RayCluster with securityContext configurations in namespace pod-security.
-            ray_cluster_add_event = RayClusterAddCREvent(
-                custom_resource_object = context['cr'],
-                rulesets = [],
-                timeout = 90,
-                namespace=cluster_namespace,
-                filepath = context['filepath']
-            )
-            ray_cluster_add_event.trigger()
-        except Exception as ex:
-            logger.error(f"RayServiceAddCREvent fails to converge: {str(ex)}")
-            raise Exception("create_ray_cluster fails") from ex
-        # Create a node without securityContext configurations in namespace pod-security.
-        # The pod should be forbidden.
+        # Create a RayCluster with securityContext configurations in namespace pod-security.
+        ray_cluster_add_event = RayClusterAddCREvent(
+            custom_resource_object = context['cr'],
+            rulesets = [],
+            timeout = 90,
+            namespace=cluster_namespace,
+            filepath = context['filepath']
+        )
+        ray_cluster_add_event.trigger()
+        # Create a pod without securityContext configurations in namespace pod-security.
         if shell_subprocess_run(
-            f'kubectl run -n {cluster_namespace} busybox --image=busybox --restart=Never',
+            f'kubectl run -n {cluster_namespace} busybox --image=busybox',
             check = False) == 0:
-            logger.error(f'forceing the restricted Pod security standard fails')
-            raise Exception("forceing the restricted Pod security standard fails")
+            logger.error(f'pod without securityContext has been wrongly created. It violate the restricted security policies')
+            raise Exception("pod without securityContext has been wrongly created. It violate the restricted security policies")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
