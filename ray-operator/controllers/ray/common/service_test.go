42938e254865deec2d1b633a6a22b3f7f49dfe9f
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2023-05-15 13:55:42 -0700

    Run gofumpt
    Signed-off-by: Archit Kulkarni <architkulkarni@users.noreply.github.com>

--- ray-operator/controllers/ray/common/service_test.go
+++ ray-operator/controllers/ray/common/service_test.go
@@ -16,59 +16,61 @@ import (
 	"k8s.io/utils/pointer"
 )
 
-var headServiceAnnotationKey1 = "HeadServiceAnnotationKey1"
-var headServiceAnnotationValue1 = "HeadServiceAnnotationValue1"
-var headServiceAnnotationKey2 = "HeadServiceAnnotationKey2"
-var headServiceAnnotationValue2 = "HeadServiceAnnotationValue2"
-var instanceWithWrongSvc = &rayiov1alpha1.RayCluster{
-	ObjectMeta: metav1.ObjectMeta{
-		Name:      "raycluster-sample",
-		Namespace: "default",
-	},
-	Spec: rayiov1alpha1.RayClusterSpec{
-		RayVersion: "1.0",
-		HeadServiceAnnotations: map[string]string{
-			headServiceAnnotationKey1: headServiceAnnotationValue1,
-			headServiceAnnotationKey2: headServiceAnnotationValue2,
+var (
+	headServiceAnnotationKey1   = "HeadServiceAnnotationKey1"
+	headServiceAnnotationValue1 = "HeadServiceAnnotationValue1"
+	headServiceAnnotationKey2   = "HeadServiceAnnotationKey2"
+	headServiceAnnotationValue2 = "HeadServiceAnnotationValue2"
+	instanceWithWrongSvc        = &rayiov1alpha1.RayCluster{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "raycluster-sample",
+			Namespace: "default",
 		},
-		HeadGroupSpec: rayiov1alpha1.HeadGroupSpec{
-			Replicas: pointer.Int32Ptr(1),
-			RayStartParams: map[string]string{
-				"port":                "6379",
-				"object-manager-port": "12345",
-				"node-manager-port":   "12346",
-				"object-store-memory": "100000000",
-				"num-cpus":            "1",
+		Spec: rayiov1alpha1.RayClusterSpec{
+			RayVersion: "1.0",
+			HeadServiceAnnotations: map[string]string{
+				headServiceAnnotationKey1: headServiceAnnotationValue1,
+				headServiceAnnotationKey2: headServiceAnnotationValue2,
 			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Namespace: "default",
-					Labels: map[string]string{
-						"groupName": "headgroup",
-					},
+			HeadGroupSpec: rayiov1alpha1.HeadGroupSpec{
+				Replicas: pointer.Int32Ptr(1),
+				RayStartParams: map[string]string{
+					"port":                "6379",
+					"object-manager-port": "12345",
+					"node-manager-port":   "12346",
+					"object-store-memory": "100000000",
+					"num-cpus":            "1",
 				},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:  "ray-head",
-							Image: "rayproject/autoscaler",
-							Ports: []corev1.ContainerPort{
-								{
-									ContainerPort: 6379,
-									Name:          "gcs",
+				Template: corev1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Namespace: "default",
+						Labels: map[string]string{
+							"groupName": "headgroup",
+						},
+					},
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Name:  "ray-head",
+								Image: "rayproject/autoscaler",
+								Ports: []corev1.ContainerPort{
+									{
+										ContainerPort: 6379,
+										Name:          "gcs",
+									},
+									{
+										ContainerPort: 8265,
+									},
 								},
-								{
-									ContainerPort: 8265,
-								},
-							},
-							Command: []string{"python"},
-							Args:    []string{"/opt/code.py"},
-							Env: []corev1.EnvVar{
-								{
-									Name: "MY_POD_IP",
-									ValueFrom: &corev1.EnvVarSource{
-										FieldRef: &corev1.ObjectFieldSelector{
-											FieldPath: "status.podIP",
+								Command: []string{"python"},
+								Args:    []string{"/opt/code.py"},
+								Env: []corev1.EnvVar{
+									{
+										Name: "MY_POD_IP",
+										ValueFrom: &corev1.EnvVarSource{
+											FieldRef: &corev1.ObjectFieldSelector{
+												FieldPath: "status.podIP",
+											},
 										},
 									},
 								},
@@ -78,8 +80,8 @@ var instanceWithWrongSvc = &rayiov1alpha1.RayCluster{
 				},
 			},
 		},
-	},
-}
+	}
+)
 
 func TestBuildServiceForHeadPod(t *testing.T) {
 	svc, err := BuildServiceForHeadPod(*instanceWithWrongSvc, nil, nil)
