beb72e5a5ee23ee06522c1c53b5745cf20421969
Siddharth Kodwani <96103980+kodwanis@users.noreply.github.com>
2023-06-05 11:36:51 -0700

    Update service.go
    Signed-off-by: Siddharth Kodwani <96103980+kodwanis@users.noreply.github.com>

--- ray-operator/controllers/ray/common/service.go
+++ ray-operator/controllers/ray/common/service.go
@@ -189,7 +189,7 @@ func BuildServeServiceForRayService(rayService rayv1alpha1.RayService, rayCluste
 		// Add DefaultServePort if it is already not added and ignore any custom ports
 		// Keeping this consistentent with adding only serve port in serve service
 		if len(ports) != 0 {
-      log.Info("Ignoring user provided ports for serve service as default serve port already added")
+                        log.Info("Ignoring user provided ports for serve service as default serve port already added")
 			serveService.Spec.Ports = ports
 		} else {
 			ports := []corev1.ServicePort{}
