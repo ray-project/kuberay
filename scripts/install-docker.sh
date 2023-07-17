fd0b35e62d77e07ccafa12189bab94ae87b13313
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2023-07-17 14:55:35 -0700

    Remove unnecessary deps
    Signed-off-by: Archit Kulkarni <architkulkarni@users.noreply.github.com>

--- scripts/install-docker.sh
+++ scripts/install-docker.sh
@@ -5,14 +5,6 @@ export DEBIAN_FRONTEND=noninteractive
 export TZ=America/Los_Angeles
 
 apt-get update -qq && apt-get upgrade -qq
-apt-get install -y -qq \
-    curl python-is-python3 git build-essential \
-    sudo unzip unrar apt-utils dialog tzdata wget rsync \
-    language-pack-en tmux locales
-
-locale -a
-locale-gen en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
+apt-get install -y -qq curl
 
 curl -o- https://get.docker.com | sh
