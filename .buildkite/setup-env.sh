86653fa34ef39b716e389bed56565a8eab38ef93
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2023-08-15 16:02:01 -0700

    Remove unnecessary test
    Signed-off-by: Archit Kulkarni <architkulkarni@users.noreply.github.com>

--- .buildkite/setup-env.sh
+++ .buildkite/setup-env.sh
@@ -14,46 +14,12 @@ bash scripts/install-docker.sh
 # Delete dangling clusters
 kind delete clusters --all
 
-# Install kubectl.
-curl -LO https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl
-curl -LO "https://dl.k8s.io/release/v1.27.3/bin/linux/amd64/kubectl.sha256"
-echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
-install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-
-# Create the cluster
-time kind create cluster --wait 120s --config tests/framework/config/kind-config-buildkite.yml
-docker ps
-
-# Configure kubectl for kind cluster
-kubectl config set clusters.kind-kind.server https://docker:6443
-
-# Verify that kubectl works
-kubectl version
-kubectl cluster-info
-kubectl get nodes
-kubectl get pods --all-namespaces
-
 # Install Helm
 curl -Lo helm.tar.gz https://get.helm.sh/helm-v3.12.2-linux-amd64.tar.gz
 tar -zxvf helm.tar.gz
 mv linux-amd64/helm /usr/local/bin/helm
 helm repo add kuberay https://ray-project.github.io/kuberay-helm/
 helm repo update
-
-# Install KubeRay operator
-pushd ray-operator
-IMG=kuberay/operator:nightly make docker-image
-kind load docker-image kuberay/operator:nightly
-popd
-
-pushd helm-chart/kuberay-operator
-helm install kuberay-operator --set image.repository=kuberay/operator --set image.tag=nightly --wait --timeout=5m0s .
-popd
-
-echo "Kuberay operator successfully installed."
-
-# Remove kind cluster
-kind delete cluster
 
 # Install python 3.11 and pip
 apt-get update
