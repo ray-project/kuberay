c93eca2e106f6a84ad97228447995170011d4555
kaihsun <kaihsun@anyscale.com>
2023-08-27 06:37:38 +0000

    update

--- tests/compatibility-test.py
+++ tests/compatibility-test.py
@@ -101,7 +101,7 @@ class RayFTTestCase(unittest.TestCase):
 
         # Deploy a Ray Serve model.
         exit_code = pod_exec_command(headpod_name, RayFTTestCase.ray_cluster_ns,
-            "python samples/test_ray_serve_1.py",
+            "ray list nodes; python samples/test_ray_serve_1.py",
             check = False
         )
 
