69dbb09fba45c2f199d8ee1c2e41ab35b01320e3
Archit Kulkarni <architkulkarni@users.noreply.github.com>
2023-09-15 11:25:27 -0700

    Only test custom head svc yaml on nightly
    Signed-off-by: Archit Kulkarni <architkulkarni@users.noreply.github.com>

--- tests/test_sample_rayjob_yamls.py
+++ tests/test_sample_rayjob_yamls.py
@@ -29,11 +29,13 @@ if __name__ == '__main__':
 
     NAMESPACE = 'default'
     SAMPLE_PATH = CONST.REPO_ROOT.joinpath("ray-operator/config/samples/")
-    YAMLs = ['ray_v1alpha1_rayjob.yaml', 'ray_v1alpha1_rayjob.shutdown.yaml', 'ray-job.custom-head-svc.yaml']
+    YAMLs = ['ray_v1alpha1_rayjob.yaml', 'ray_v1alpha1_rayjob.shutdown.yaml']
     if not latest:
         # Test new backwards-incompatible fields.
         # TODO: Move these to "latest" after KubeRay v1.0 is released.
-        YAMLs.extend(['ray_v1alpha1_rayjob.resources.yaml', 'ray-job.runtimeenvyaml.yaml'])
+        # Note: ray-job.custom-head-svc.yaml is included here because the fix it depends on,
+        # https://github.com/ray-project/kuberay/pull/1332, was merged after the latest release 0.6.0.
+        YAMLs.extend(['ray_v1alpha1_rayjob.resources.yaml', 'ray-job.runtimeenvyaml.yaml', 'ray-job.custom-head-svc.yaml'])
 
     sample_yaml_files = []
     for filename in YAMLs:
