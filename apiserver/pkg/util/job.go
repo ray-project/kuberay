7b3e21dc20691ede96b5c2a26ebd9f8339aa4303
Boris Lublinsky <blublinsky@hotmail.com>
2023-10-24 13:06:35 -0500

    Fixed processing of job submitter (#1562)

--- apiserver/pkg/util/job.go
+++ apiserver/pkg/util/job.go
@@ -1,7 +1,7 @@
 package util
 
 import (
-	"errors"
+	"fmt"
 
 	api "github.com/ray-project/kuberay/proto/go_client"
 	rayv1api "github.com/ray-project/kuberay/ray-operator/apis/ray/v1"
@@ -18,10 +18,6 @@ const rayJobDefaultVersion = "1.13"
 
 // NewRayJob creates a RayJob.
 func NewRayJob(apiJob *api.RayJob, computeTemplateMap map[string]*api.ComputeTemplate) (*RayJob, error) {
-	if apiJob.ClusterSelector != nil && apiJob.JobSubmitter == nil {
-		// If cluster selector is specified, ensure that Job submitter is present
-		return nil, errors.New("external Ray cluster requires Job submitter definition")
-	}
 	rayJob := &rayv1api.RayJob{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:        apiJob.Name,
@@ -52,9 +48,19 @@ func NewRayJob(apiJob *api.RayJob, computeTemplateMap map[string]*api.ComputeTem
 		cpus := "1"
 		memorys := "1Gi"
 		if apiJob.JobSubmitter.Cpu != "" {
+			// Ensure that CPU size is formatted correctly
+			_, err := resource.ParseQuantity(apiJob.JobSubmitter.Cpu)
+			if err != nil {
+				return nil, fmt.Errorf("cpu for ray job submitter %s is not specified correctly: %w", apiJob.JobSubmitter.Cpu, err)
+			}
 			cpus = apiJob.JobSubmitter.Cpu
 		}
 		if apiJob.JobSubmitter.Memory != "" {
+			// Ensure that memory size is formatted correctly
+			_, err := resource.ParseQuantity(apiJob.JobSubmitter.Memory)
+			if err != nil {
+				return nil, fmt.Errorf("memory for ray job submitter %s is not specified correctly: %w", apiJob.JobSubmitter.Memory, err)
+			}
 			memorys = apiJob.JobSubmitter.Memory
 		}
 		rayJob.Spec.SubmitterPodTemplate = &v1.PodTemplateSpec{
