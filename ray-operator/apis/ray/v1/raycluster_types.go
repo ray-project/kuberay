5c3c48fcbf34e90b68128109cdd561d282797816
David Xia <dxia@spotify.com>
2023-11-23 10:21:45 -0500

    feat: add all three CRDs to the `all` category
    so they show up with `kubectl get all`.
    
    ## Example usage
    
    ```
    ❯ kubectl get all
    NAME                                DESIRED WORKERS   AVAILABLE WORKERS   STATUS   AGE
    raycluster.ray.io/raycluster-mini                                                  17s
    ```
    
    [docs](https://book.kubebuilder.io/reference/markers/crd)

--- ray-operator/apis/ray/v1/raycluster_types.go
+++ ray-operator/apis/ray/v1/raycluster_types.go
@@ -156,6 +156,7 @@ const (
 
 // RayCluster is the Schema for the RayClusters API
 // +kubebuilder:object:root=true
+// +kubebuilder:resource:categories=all
 // +kubebuilder:subresource:status
 // +kubebuilder:storageversion
 // +kubebuilder:printcolumn:name="desired workers",type=integer,JSONPath=".status.desiredWorkerReplicas",priority=0
