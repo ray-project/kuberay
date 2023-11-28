fb19892c57596924bbf4f9f5c97f2a1ab2784385
evalaiyc98 <evalaiyc98@gmail.com>
2023-11-28 20:15:59 +0800

    Enhance observability

--- ray-operator/controllers/ray/raycluster_controller.go
+++ ray-operator/controllers/ray/raycluster_controller.go
@@ -1093,7 +1093,8 @@ func (r *RayClusterReconciler) buildRedisCleanupJob(instance rayv1.RayCluster) b
 	pod.Spec.Containers = []corev1.Container{pod.Spec.Containers[common.RayContainerIndex]}
 	pod.Spec.Containers[common.RayContainerIndex].Command = []string{"/bin/bash", "-lc", "--"}
 	pod.Spec.Containers[common.RayContainerIndex].Args = []string{
-		"python -c " +
+		"echo \"To get more information about manually delete the storage namespace in Redis and remove the RayCluster's finalizer, please check https://docs.ray.io/en/master/cluster/kubernetes/user-guides/kuberay-gcs-ft.html for more details.\"" +
+			"python -c " +
 			"\"from ray._private.gcs_utils import cleanup_redis_storage; " +
 			"from urllib.parse import urlparse; " +
 			"import os; " +
