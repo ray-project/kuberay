842dd9dbc2b2e4617f1fecd335b81426fc86a635
David Xia <dxia@spotify.com>
2023-12-05 13:46:43 -0500

    fix operator: remove unused mutating and conversion webhook configs (#1705)

--- ray-operator/apis/ray/v1/raycluster_webhook.go
+++ ray-operator/apis/ray/v1/raycluster_webhook.go
@@ -20,17 +20,6 @@ func (r *RayCluster) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-//+kubebuilder:webhook:path=/mutate-ray-io-v1-raycluster,mutating=true,failurePolicy=fail,sideEffects=None,groups=ray.io,resources=rayclusters,verbs=create;update,versions=v1,name=mraycluster.kb.io,admissionReviewVersions=v1
-
-var _ webhook.Defaulter = &RayCluster{}
-
-// Default implements webhook.Defaulter so a webhook will be registered for the type
-func (r *RayCluster) Default() {
-	rayclusterlog.Info("default", "name", r.Name)
-
-	// TODO(user): fill in your defaulting logic.
-}
-
 // TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
 //+kubebuilder:webhook:path=/validate-ray-io-v1-raycluster,mutating=false,failurePolicy=fail,sideEffects=None,groups=ray.io,resources=rayclusters,verbs=create;update,versions=v1,name=vraycluster.kb.io,admissionReviewVersions=v1
 
