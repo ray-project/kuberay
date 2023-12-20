04ac0d00fb92a479781c13c84f8596555ad4b9c1
Archit Kulkarni <archit@anyscale.com>
2023-12-20 11:53:43 -0800

    Update second location of DashboardAgentPort
    Signed-off-by: Archit Kulkarni <archit@anyscale.com>

--- ray-operator/controllers/ray/rayservice_controller.go
+++ ray-operator/controllers/ray/rayservice_controller.go
@@ -1002,7 +1002,7 @@ func (r *RayServiceReconciler) updateStatusForActiveCluster(ctx context.Context,
 	var clientURL string
 	rayServiceStatus := &rayServiceInstance.Status.ActiveServiceStatus
 
-	if clientURL, err = utils.FetchHeadServiceURL(ctx, &r.Log, r.Client, rayClusterInstance, utils.DashboardAgentListenPortName); err != nil || clientURL == "" {
+	if clientURL, err = utils.FetchHeadServiceURL(ctx, &r.Log, r.Client, rayClusterInstance, utils.DashboardPortName); err != nil || clientURL == "" {
 		updateDashboardStatus(rayServiceStatus, false)
 		return err
 	}
@@ -1054,7 +1054,7 @@ func (r *RayServiceReconciler) reconcileServe(ctx context.Context, rayServiceIns
 		return ctrl.Result{RequeueAfter: ServiceDefaultRequeueDuration}, false, err
 	}
 
-	// TODO(architkulkarni): Check the RayVersion. If < 2.8.0, then use DashboardAgentPort. Else, use DashboardPort.
+	// TODO(architkulkarni): Check the RayVersion. If < 2.8.0, error.
 
 	if clientURL, err = utils.FetchHeadServiceURL(ctx, &r.Log, r.Client, rayClusterInstance, utils.DashboardPortName); err != nil || clientURL == "" {
 		return ctrl.Result{RequeueAfter: ServiceDefaultRequeueDuration}, false, err
