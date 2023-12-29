b1fa081b29de62d4c26d9ac9073de1b207a85373
kaihsun <kaihsun@anyscale.com>
2023-12-29 23:52:37 +0000

    update

--- ray-operator/controllers/ray/utils/serve_api_models.go
+++ ray-operator/controllers/ray/utils/serve_api_models.go
@@ -40,39 +40,3 @@ type ServeDetails struct {
 	Applications map[string]ServeApplicationDetails `json:"applications"`
 	DeployMode   string                             `json:"deploy_mode,omitempty"`
 }
-
-// ServingClusterDeployments defines the request sent to the dashboard api server.
-// See https://docs.ray.io/en/master/_modules/ray/serve/schema.html#ServeApplicationSchema for more details.
-type ServingClusterDeployments struct {
-	ImportPath  string                 `json:"import_path"`
-	RuntimeEnv  map[string]interface{} `json:"runtime_env,omitempty"`
-	Deployments []ServeConfigSpec      `json:"deployments,omitempty"`
-	Port        int                    `json:"port,omitempty"`
-}
-
-// ServeConfigSpec defines the (single-application) desired state of RayService, used by Ray Dashboard.
-// Serve schema details: https://docs.ray.io/en/latest/serve/api/doc/ray.serve.schema.ServeApplicationSchema.html
-type ServeConfigSpec struct {
-	Name                      string                 `json:"name"`
-	NumReplicas               *int32                 `json:"num_replicas,omitempty"`
-	RoutePrefix               string                 `json:"route_prefix,omitempty"`
-	MaxConcurrentQueries      *int32                 `json:"max_concurrent_queries,omitempty"`
-	UserConfig                map[string]interface{} `json:"user_config,omitempty"`
-	AutoscalingConfig         map[string]interface{} `json:"autoscaling_config,omitempty"`
-	GracefulShutdownWaitLoopS *int32                 `json:"graceful_shutdown_wait_loop_s,omitempty"`
-	GracefulShutdownTimeoutS  *int32                 `json:"graceful_shutdown_timeout_s,omitempty"`
-	HealthCheckPeriodS        *int32                 `json:"health_check_period_s,omitempty"`
-	HealthCheckTimeoutS       *int32                 `json:"health_check_timeout_s,omitempty"`
-	RayActorOptions           RayActorOptionSpec     `json:"ray_actor_options,omitempty"`
-}
-
-// RayActorOptionSpec defines the desired state of RayActor, used by Ray Dashboard.
-type RayActorOptionSpec struct {
-	RuntimeEnv        map[string]interface{} `json:"runtime_env,omitempty"`
-	NumCpus           *float64               `json:"num_cpus,omitempty"`
-	NumGpus           *float64               `json:"num_gpus,omitempty"`
-	Memory            *uint64                `json:"memory,omitempty"`
-	ObjectStoreMemory *uint64                `json:"object_store_memory,omitempty"`
-	Resources         map[string]interface{} `json:"resources,omitempty"`
-	AcceleratorType   string                 `json:"accelerator_type,omitempty"`
-}
