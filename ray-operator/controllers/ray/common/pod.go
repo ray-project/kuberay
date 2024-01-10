a82ebe9087f2c64378ad001f5668ab8ab944e38b
blublinsky <blublinsky@hotmail.com>
2024-01-10 21:09:19 +0000

    Fixing readiness probe

--- ray-operator/controllers/ray/common/pod.go
+++ ray-operator/controllers/ray/common/pod.go
@@ -283,10 +283,10 @@ func initLivenessAndReadinessProbe(rayContainer *corev1.Container, rayNodeType r
 		// See https://github.com/ray-project/kuberay/pull/1808 for reasons.
 		if enableServeService && rayNodeType == rayv1.WorkerNode {
 			rayContainer.ReadinessProbe.FailureThreshold = utils.ServeReadinessProbeFailureThreshold
-			rayServeProxyHealthCommand := fmt.Sprintf(utils.BaseWgetHealthCommand,
-				utils.FindContainerPort(rayContainer, utils.ServingPortName, utils.DefaultServingPort), utils.RayServeProxyHealthPath)
-			commands = append(commands, rayServeProxyHealthCommand)
-			rayContainer.ReadinessProbe.Exec = &corev1.ExecAction{Command: []string{"bash", "-c", strings.Join(commands, " && ")}}
+//			rayServeProxyHealthCommand := fmt.Sprintf(utils.BaseWgetHealthCommand,
+//				utils.FindContainerPort(rayContainer, utils.ServingPortName, utils.DefaultServingPort), utils.RayServeProxyHealthPath)
+//			commands = append(commands, rayServeProxyHealthCommand)
+//			rayContainer.ReadinessProbe.Exec = &corev1.ExecAction{Command: []string{"bash", "-c", strings.Join(commands, " && ")}}
 		}
 	}
 }
