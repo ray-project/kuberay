546522fc21e98c47723cda3900495ad7c2dcbf56
Vinayak Goyal <vinaygo@google.com>
2024-03-01 18:58:09 +0000

    Update kind version (#1957)

--- .buildkite/setup-env.sh
+++ .buildkite/setup-env.sh
@@ -4,7 +4,7 @@
 export PATH=$PATH:/usr/local/go/bin
 
 # Install kind
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-amd64
 chmod +x ./kind
 mv ./kind /usr/local/bin/kind
 
