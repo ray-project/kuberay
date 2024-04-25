1bd0e3a674a8c96df30c1d8aa30b07b19d2e1059
Andrew Sy Kim <andrewsy@google.com>
2024-04-25 16:33:07 +0000

    add initial perf tests for 100 RayCluster and 100 RayJob
    Signed-off-by: Andrew Sy Kim <andrewsy@google.com>

---
+++ benchmark/perf-tests/100-raycluster/wait-for-rayclusters.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+expect_succeeded=100
+echo "waiting for $expect_succeeded RayClusters to be completed successfully"
+
+while true; do
+  num_succeeded=$(kubectl get raycluster -A -o jsonpath='{range .items[*]}{.metadata.name} {.status.state}{"\n"}' | grep -c ready)
+  echo "$num_succeeded RayClusters ready..."
+
+  if [[ "$num_succeeded" == "$expect_succeeded" ]]; then
+     break;
+  fi
+
+  sleep 5
+done
+
+echo "$num_succeeded RayClusters ready!"
