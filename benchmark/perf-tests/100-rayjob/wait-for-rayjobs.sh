7f45662120fbad6cb961352138db36af421780b2
Andrew Sy Kim <andrewsy@google.com>
2024-05-16 00:39:05 +0000

    update 100 RayJob tests to use PyTorch trainer and Ray Data examples
    Signed-off-by: Andrew Sy Kim <andrewsy@google.com>

--- benchmark/perf-tests/100-rayjob/wait-for-rayjobs.sh
+++ benchmark/perf-tests/100-rayjob/wait-for-rayjobs.sh
@@ -4,7 +4,18 @@ expect_succeeded=100
 echo "waiting for $expect_succeeded RayJobs to be completed successfully"
 
 while true; do
-  num_succeeded=$(kubectl get rayjob -A  -o jsonpath='{range .items[*]}{.metadata.name} {.status.jobStatus}{"\n"}' | grep -c SUCCEEDED)
+  num_succeeded=$(kubectl get rayjob -A -l perf-test=rayjob-pytorch-mnist  -o jsonpath='{range .items[*]}{.metadata.name} {.status.jobStatus}{"\n"}' | grep -c SUCCEEDED)
+  echo "$num_succeeded RayJobs completed..."
+
+  if [[ "$num_succeeded" == "$expect_succeeded" ]]; then
+     break;
+  fi
+
+  sleep 5
+done
+
+while true; do
+  num_succeeded=$(kubectl get rayjob -A -l perf-test=ray-data-image-resize -o jsonpath='{range .items[*]}{.metadata.name} {.status.jobStatus}{"\n"}' | grep -c SUCCEEDED)
   echo "$num_succeeded RayJobs completed..."
 
   if [[ "$num_succeeded" == "$expect_succeeded" ]]; then
