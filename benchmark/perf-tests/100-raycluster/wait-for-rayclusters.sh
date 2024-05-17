2715544b92ea1aa91e7988f50d41badb538158a3
Andrew Sy Kim <andrewsy@google.com>
2024-05-17 01:06:13 -0400

    [perf-test] update 100 RayJob perf tests to use PyTorch trainer and Ray Data examples (#2149)

--- benchmark/perf-tests/100-raycluster/wait-for-rayclusters.sh
+++ benchmark/perf-tests/100-raycluster/wait-for-rayclusters.sh
@@ -4,7 +4,7 @@ expect_succeeded=100
 echo "waiting for $expect_succeeded RayClusters to be completed successfully"
 
 while true; do
-  num_succeeded=$(kubectl get raycluster -A -o jsonpath='{range .items[*]}{.metadata.name} {.status.state}{"\n"}' | grep -c ready)
+  num_succeeded=$(kubectl get raycluster -l perf-test=ray-cluster -A -o jsonpath='{range .items[*]}{.metadata.name} {.status.state}{"\n"}' | grep -c ready)
   echo "$num_succeeded RayClusters ready..."
 
   if [[ "$num_succeeded" == "$expect_succeeded" ]]; then
