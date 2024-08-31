be805f16b7e98ed99da08aefe3bfd08c87e44ad9
kaihsun <kaihsun@anyscale.com>
2024-08-31 00:32:53 +0000

    update
    Signed-off-by: kaihsun <kaihsun@anyscale.com>

--- ray-operator/controllers/ray/utils/constant.go
+++ ray-operator/controllers/ray/utils/constant.go
@@ -182,7 +182,7 @@ const (
 	// The version is included in the RAY_USAGE_STATS_EXTRA_TAGS environment variable
 	// as well as the user-agent. This constant is updated before release.
 	// TODO: Update KUBERAY_VERSION to be a build-time variable.
-	KUBERAY_VERSION = "1.2.1"
+	KUBERAY_VERSION = "v1.2.1"
 )
 
 type ServiceType string
