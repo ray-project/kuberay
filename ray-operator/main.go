cf6cc95b95902b650d8786f244c599703c56edc9
Weiwei Yang <wwei@apache.org>
2024-09-11 15:00:04 -0700

    address review comments

--- ray-operator/main.go
+++ ray-operator/main.go
@@ -91,9 +91,9 @@ func main() {
 	flag.StringVar(&logStdoutEncoder, "log-stdout-encoder", "json",
 		"Encoder to use for logging stdout. Valid values are 'json' and 'console'. Defaults to 'json'")
 	flag.BoolVar(&enableBatchScheduler, "enable-batch-scheduler", false,
-		"(Deprecated) Enable batch scheduler. Currently is volcano, which supports gang scheduler policy.")
+		"(Deprecated) Enable batch scheduler. Currently is volcano, which supports gang scheduler policy. Please use --batch-scheduler instead.")
 	flag.StringVar(&batchScheduler, "batch-scheduler", "",
-		"Batch scheduler name, supported values are default, volcano, yunikorn.")
+		"Batch scheduler name, supported values are volcano and yunikorn.")
 	flag.StringVar(&configFile, "config", "", "Path to structured config file. Flags are ignored if config file is set.")
 	flag.BoolVar(&useKubernetesProxy, "use-kubernetes-proxy", false,
 		"Use Kubernetes proxy subresource when connecting to the Ray Head node.")
