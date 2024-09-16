7c48e85ee365a2599cf635e983ece150a50b40e4
leoliao <leoyeepaa@gmail.com>
2024-09-16 23:54:16 +0800

    1. Use  instead. 2. don't install  for user, and add t he install info at  3. Store the  at  4. Call , 5. check the version of  in
    Signed-off-by: leoliao <leoyeepaa@gmail.com>

--- scripts/validate-helm.sh
+++ scripts/validate-helm.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
-
-if [ -L ${BASH_SOURCE-$0} ]; then
-  PWD=$(dirname $(readlink "${BASH_SOURCE-$0}"))
-else
-  PWD=$(dirname ${BASH_SOURCE-$0})
-fi
-export CURRENT_PATH=$(cd "${PWD}" >/dev/null; pwd)
-export KUBERAY_HOME=${CURRENT_PATH}/..
-
-# Install kubeconform
-KUBECONFORM_INSTALL=$(mktemp -d)
-trap 'rm -rf "${KUBECONFORM_INSTALL}"' EXIT
-curl -L https://github.com/yannh/kubeconform/releases/latest/download/kubeconform-linux-amd64.tar.gz | tar xz -C "${KUBECONFORM_INSTALL}"
-curl -o "${KUBECONFORM_INSTALL}/crd2schema.py" https://raw.githubusercontent.com/yannh/kubeconform/master/scripts/openapi2jsonschema.py
-SCRIPT_PATH="${KUBECONFORM_INSTALL}/crd2schema.py"
+export KUBERAY_HOME=$(git rev-parse --show-toplevel)
+SCRIPT_PATH="${KUBERAY_HOME}/scripts/openapi2jsonschema.py"
 RAYCLUSTER_CRD_PATH="$KUBERAY_HOME/ray-operator/config/crd/bases/ray.io_rayclusters.yaml"
+tmp=$(mktemp -d)
+trap 'rm -rf "$tmp"' EXIT
 
-# Convert Ray Cluster CRD YAML to JSON Schema
-pushd "${KUBECONFORM_INSTALL}" > /dev/null
-python3 "$SCRIPT_PATH" "$RAYCLUSTER_CRD_PATH"
+# Convert CRD YAML to JSON Schema
+pushd "${tmp}" > /dev/null
+chmod +x "$SCRIPT_PATH"
+"$SCRIPT_PATH" "$RAYCLUSTER_CRD_PATH"
 popd > /dev/null
-RAYCLUSTER_CRD_SCHEMA="${KUBECONFORM_INSTALL}/raycluster_v1.json"
+RAYCLUSTER_CRD_SCHEMA="${tmp}/raycluster_v1.json"
 
 # Validate Helm charts with kubeconform
 echo "Validating Helm Charts with kubeconform..."
-helm template "$KUBERAY_HOME/helm-chart/kuberay-apiserver" | "${KUBECONFORM_INSTALL}/kubeconform" --summary -schema-location default
-helm template "$KUBERAY_HOME/helm-chart/kuberay-operator" | "${KUBECONFORM_INSTALL}/kubeconform" --summary -schema-location default
-helm template "$KUBERAY_HOME/helm-chart/ray-cluster" | "${KUBECONFORM_INSTALL}/kubeconform" --summary -schema-location default -schema-location "$RAYCLUSTER_CRD_SCHEMA"
+helm template "$KUBERAY_HOME/helm-chart/kuberay-apiserver" | kubeconform --summary -schema-location default
+helm template "$KUBERAY_HOME/helm-chart/kuberay-operator" | kubeconform --summary -schema-location default
+helm template "$KUBERAY_HOME/helm-chart/ray-cluster" | kubeconform --summary -schema-location default -schema-location "$RAYCLUSTER_CRD_SCHEMA"
 
+# Check for errors during validation
 if [ $? -eq 1 ]; then
     exit 1
 fi
