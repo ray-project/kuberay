aaf3eb69748f21708fb9d20692a6bb6ed807083a
leoliao <leoyeepaa@gmail.com>
2024-09-17 14:07:17 +0800

    fix error catch & version validation
    Signed-off-by: leoliao <leoyeepaa@gmail.com>

--- scripts/validate-helm.sh
+++ scripts/validate-helm.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 export KUBERAY_HOME=$(git rev-parse --show-toplevel)
 SCRIPT_PATH="${KUBERAY_HOME}/scripts/openapi2jsonschema.py"
 RAYCLUSTER_CRD_PATH="$KUBERAY_HOME/ray-operator/config/crd/bases/ray.io_rayclusters.yaml"
@@ -7,7 +8,6 @@ trap 'rm -rf "$tmp"' EXIT
 
 # Convert CRD YAML to JSON Schema
 pushd "${tmp}" > /dev/null
-chmod +x "$SCRIPT_PATH"
 "$SCRIPT_PATH" "$RAYCLUSTER_CRD_PATH"
 popd > /dev/null
 RAYCLUSTER_CRD_SCHEMA="${tmp}/raycluster_v1.json"
@@ -17,8 +17,3 @@ echo "Validating Helm Charts with kubeconform..."
 helm template "$KUBERAY_HOME/helm-chart/kuberay-apiserver" | kubeconform --summary -schema-location default
 helm template "$KUBERAY_HOME/helm-chart/kuberay-operator" | kubeconform --summary -schema-location default
 helm template "$KUBERAY_HOME/helm-chart/ray-cluster" | kubeconform --summary -schema-location default -schema-location "$RAYCLUSTER_CRD_SCHEMA"
-
-# Check for errors during validation
-if [ $? -eq 1 ]; then
-    exit 1
-fi
