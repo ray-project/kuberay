a01c7e8527bb6b2db561c818562aa6e06bf2c7ff
Chi-Sheng Liu <chishengliu@chishengliu.com>
2024-10-10 20:18:45 +0800

    [Test][sample-yaml] Check RayCluster create correct number of pods
    Closes: ray-project/kuberay#2431
    Signed-off-by: Chi-Sheng Liu <chishengliu@chishengliu.com>

--- ray-operator/test/sampleyaml/support.go
+++ ray-operator/test/sampleyaml/support.go
@@ -1,1 +1,53 @@
 package sampleyaml
+
+import (
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+
+	"github.com/onsi/gomega"
+
+	rayv1 "github.com/ray-project/kuberay/ray-operator/apis/ray/v1"
+	rayscheme "github.com/ray-project/kuberay/ray-operator/pkg/client/clientset/versioned/scheme"
+	. "github.com/ray-project/kuberay/ray-operator/test/support"
+)
+
+func getSampleYAMLDir(t Test) string {
+	t.T().Helper()
+	_, b, _, _ := runtime.Caller(0)
+	sampleYAMLDir := filepath.Join(filepath.Dir(b), "../../config/samples")
+	info, err := os.Stat(sampleYAMLDir)
+	t.Expect(err).NotTo(gomega.HaveOccurred())
+	t.Expect(info.IsDir()).To(gomega.BeTrue())
+	return sampleYAMLDir
+}
+
+func readYAML(t Test, filename string) []byte {
+	t.T().Helper()
+	sampleYAMLDir := getSampleYAMLDir(t)
+	yamlFile := filepath.Join(sampleYAMLDir, filename)
+	yamlFileContent, err := os.ReadFile(yamlFile)
+	t.Expect(err).NotTo(gomega.HaveOccurred())
+	return yamlFileContent
+}
+
+func DeserializeRayClusterSampleYAML(t Test, filename string) *rayv1.RayCluster {
+	t.T().Helper()
+	yamlFileContent := readYAML(t, filename)
+	decoder := rayscheme.Codecs.UniversalDecoder()
+	rayCluster := &rayv1.RayCluster{}
+	_, _, err := decoder.Decode(yamlFileContent, nil, rayCluster)
+	t.Expect(err).NotTo(gomega.HaveOccurred())
+	return rayCluster
+}
+
+func KubectlApplyYAML(t Test, filename string, namespace string) {
+	t.T().Helper()
+	sampleYAMLDir := getSampleYAMLDir(t)
+	sampleYAMLPath := filepath.Join(sampleYAMLDir, filename)
+	kubectlCmd := exec.CommandContext(t.Ctx(), "kubectl", "apply", "-f", sampleYAMLPath, "-n", namespace)
+	err := kubectlCmd.Run()
+	t.Expect(err).NotTo(gomega.HaveOccurred())
+	t.T().Logf("Successfully applied %s", filename)
+}
