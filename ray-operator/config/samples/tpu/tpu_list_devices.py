047699ffd4b6583777180db657ca38b2011e7d79
ryanaoleary <113500783+ryanaoleary@users.noreply.github.com>
2024-10-16 05:55:46 +0000

    Fix v6e TPU Scripts and RayJob CRs (#2447)

--- ray-operator/config/samples/tpu/tpu_list_devices.py
+++ ray-operator/config/samples/tpu/tpu_list_devices.py
@@ -1,19 +1,12 @@
 import ray
+import jax
 
-ray.init(
-    runtime_env={
-        "pip": [
-            "jax[tpu]==0.4.33",
-            "-f https://storage.googleapis.com/jax-releases/libtpu_releases.html",
-        ]
-    }
-)
+ray.init()
 
 @ray.remote(resources={"TPU": 4})
 def tpu_cores():
-    import jax
     return "TPU cores:" + str(jax.device_count())
 
-num_workers = int(os.environ['TPU_HOSTS']) # Set in env of RayJob or RayCluster.
+num_workers = int(ray.available_resources()["TPU"]) // 4
 result = [tpu_cores.remote() for _ in range(num_workers)]
 print(ray.get(result))
