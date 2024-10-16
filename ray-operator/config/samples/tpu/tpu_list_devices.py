c4a233c4a07d45914185a99cfb5e85278114e44d
Ryan O'Leary <ryanaoleary@google.com>
2024-10-16 05:01:15 +0000

    Fix num_workers calculation
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>

--- ray-operator/config/samples/tpu/tpu_list_devices.py
+++ ray-operator/config/samples/tpu/tpu_list_devices.py
@@ -7,6 +7,6 @@ ray.init()
 def tpu_cores():
     return "TPU cores:" + str(jax.device_count())
 
-num_workers = int(ray.available_resources()["TPU"])
+num_workers = int(ray.available_resources()["TPU"]) // 4
 result = [tpu_cores.remote() for _ in range(num_workers)]
 print(ray.get(result))
