c841fd315391bafc44a30dc5bc660f87135a5d78
Ryan O'Leary <ryanaoleary@google.com>
2024-10-16 02:23:08 +0000

    Add v6e TPU Ray CR manifests
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>

---
+++ ray-operator/config/samples/tpu-print-resources.py
@@ -0,0 +1,19 @@
+import ray
+
+ray.init(
+    runtime_env={
+        "pip": [
+            "jax[tpu]==0.4.33",
+            "-f https://storage.googleapis.com/jax-releases/libtpu_releases.html",
+        ]
+    }
+)
+
+@ray.remote(resources={"TPU": 4})
+def tpu_cores():
+    import jax
+    return "TPU cores:" + str(jax.device_count())
+
+num_workers = os.environ['TPU_HOSTS'] # Set in env of RayJob or RayCluster.
+result = [tpu_cores.remote() for _ in range(num_workers)]
+print(ray.get(result))
