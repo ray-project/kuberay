0a8102d896d2074f63c033c2632c87f5a072f8c9
Ryan O'Leary <ryanaoleary@google.com>
2024-10-24 22:26:15 +0000

    Print TPU_WORKER_ID
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>

--- ray-operator/config/samples/tpu/tpu_list_devices.py
+++ ray-operator/config/samples/tpu/tpu_list_devices.py
@@ -8,7 +8,7 @@ ray.init()
 @ray.remote(resources={"TPU": 4})
 def tpu_cores():
     multihost_utils.sync_global_devices("sync")
-    return "TPU cores:" + str(jax.device_count())
+    return "TPU Worker: " + os.environ.get("TPU_WORKER_ID") + "TPU cores:" + str(jax.device_count())
 
 num_workers = int(ray.available_resources()["TPU"]) // 4
 printf("Number of TPU Workers: %d" + num_workers)
