f39445414fe9a3ecf7664324b2be4a1b87827802
Chi-Sheng Liu <chishengliu@chishengliu.com>
2024-10-31 23:23:50 +0800

    [Test][HA] Add test for autoscaling RayService
    Closes: ray-project/kuberay#2480
    Signed-off-by: Chi-Sheng Liu <chishengliu@chishengliu.com>

--- ray-operator/test/e2e/rayservice_ha_test.go
+++ ray-operator/test/e2e/rayservice_ha_test.go
@@ -11,6 +11,9 @@ import (
 )
 
 func TestStaticRayService(t *testing.T) {
+	rayserviceYamlFile := "testdata/rayservice.static.yaml"
+	locustYamlFile := "testdata/locust-cluster.const-rate.yaml"
+
 	test := With(t)
 	g := NewWithT(t)
 
@@ -25,7 +28,7 @@ func TestStaticRayService(t *testing.T) {
 	test.T().Logf("Created ConfigMap %s/%s successfully", configMap.Namespace, configMap.Name)
 
 	// Create the RayService for testing
-	KubectlApplyYAML(test, "testdata/rayservice.static.yaml", namespace.Name)
+	KubectlApplyYAML(test, rayserviceYamlFile, namespace.Name)
 	rayService, err := GetRayService(test, namespace.Name, "test-rayservice")
 	g.Expect(err).NotTo(HaveOccurred())
 	test.T().Logf("Created RayService %s/%s successfully", rayService.Namespace, rayService.Name)
@@ -35,7 +38,57 @@ func TestStaticRayService(t *testing.T) {
 		Should(WithTransform(RayServiceStatus, Equal(rayv1.Running)))
 
 	// Create Locust RayCluster
-	KubectlApplyYAML(test, "testdata/locust-cluster.const-rate.yaml", namespace.Name)
+	KubectlApplyYAML(test, locustYamlFile, namespace.Name)
+	locustCluster, err := GetRayCluster(test, namespace.Name, "locust-cluster")
+	g.Expect(err).NotTo(HaveOccurred())
+	test.T().Logf("Created Locust RayCluster %s/%s successfully", locustCluster.Namespace, locustCluster.Name)
+
+	g.Eventually(RayCluster(test, locustCluster.Namespace, locustCluster.Name), TestTimeoutMedium).
+		Should(WithTransform(RayClusterState, Equal(rayv1.Ready)))
+	g.Expect(GetRayCluster(test, locustCluster.Namespace, locustCluster.Name)).To(WithTransform(RayClusterDesiredWorkerReplicas, Equal(int32(0))))
+
+	headPod, err := GetHeadPod(test, locustCluster)
+	g.Expect(err).NotTo(HaveOccurred())
+	test.T().Logf("Found head pod %s/%s", headPod.Namespace, headPod.Name)
+
+	// Install Locust in the head Pod
+	ExecPodCmd(test, headPod, common.RayHeadContainer, []string{"pip", "install", "locust"})
+
+	// Run Locust test
+	ExecPodCmd(test, headPod, common.RayHeadContainer, []string{
+		"python", "/locust-runner/locust_runner.py", "-f", "/locustfile/locustfile.py", "--host", "http://test-rayservice-serve-svc:8000",
+	})
+}
+
+func TestAutoscalingRayService(t *testing.T) {
+	rayserviceYamlFile := "testdata/rayservice.autoscaling.yaml"
+	locustYamlFile := "testdata/locust-cluster.burst.yaml"
+
+	test := With(t)
+	g := NewWithT(t)
+
+	// Create a namespace
+	namespace := test.NewTestNamespace()
+	test.StreamKubeRayOperatorLogs()
+
+	// Create a ConfigMap with Locust runner script
+	configMapAC := newConfigMap(namespace.Name, "locust-runner-script", files(test, "locust_runner.py"))
+	configMap, err := test.Client().Core().CoreV1().ConfigMaps(namespace.Name).Apply(test.Ctx(), configMapAC, TestApplyOptions)
+	g.Expect(err).NotTo(HaveOccurred())
+	test.T().Logf("Created ConfigMap %s/%s successfully", configMap.Namespace, configMap.Name)
+
+	// Create the RayService for testing
+	KubectlApplyYAML(test, rayserviceYamlFile, namespace.Name)
+	rayService, err := GetRayService(test, namespace.Name, "test-rayservice")
+	g.Expect(err).NotTo(HaveOccurred())
+	test.T().Logf("Created RayService %s/%s successfully", rayService.Namespace, rayService.Name)
+
+	test.T().Logf("Waiting for RayService %s/%s to running", rayService.Namespace, rayService.Name)
+	g.Eventually(RayService(test, rayService.Namespace, rayService.Name), TestTimeoutMedium).
+		Should(WithTransform(RayServiceStatus, Equal(rayv1.Running)))
+
+	// Create Locust RayCluster
+	KubectlApplyYAML(test, locustYamlFile, namespace.Name)
 	locustCluster, err := GetRayCluster(test, namespace.Name, "locust-cluster")
 	g.Expect(err).NotTo(HaveOccurred())
 	test.T().Logf("Created Locust RayCluster %s/%s successfully", locustCluster.Namespace, locustCluster.Name)
