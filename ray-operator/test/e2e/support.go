5aaf78d8a39fc92a90b056c133010eb024b3ad42
Cheyu Wu <cheyu1220@gmail.com>
2024-11-23 16:27:41 +0800

    Revert "chore: increase resourceCPU" This reverts commit 5e2b8b1f89940ace4afa47c3a1a1f31a747b8c71.

--- ray-operator/test/e2e/support.go
+++ ray-operator/test/e2e/support.go
@@ -135,11 +135,11 @@ func headPodTemplateApplyConfiguration() *corev1ac.PodTemplateSpecApplyConfigura
 				).
 				WithResources(corev1ac.ResourceRequirements().
 					WithRequests(corev1.ResourceList{
-						corev1.ResourceCPU:    resource.MustParse("2"),
+						corev1.ResourceCPU:    resource.MustParse("300m"),
 						corev1.ResourceMemory: resource.MustParse("1G"),
 					}).
 					WithLimits(corev1.ResourceList{
-						corev1.ResourceCPU:    resource.MustParse("2"),
+						corev1.ResourceCPU:    resource.MustParse("500m"),
 						corev1.ResourceMemory: resource.MustParse("2G"),
 					}))))
 }
