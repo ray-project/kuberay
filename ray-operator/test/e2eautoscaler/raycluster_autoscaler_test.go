7e59297482df1b257dbed3b8c1e6b572520d63d3
hjiang <hjiang@anyscale.com>
2024-12-04 01:58:57 +0000

    Add test for autoscaler and its desired state
    Signed-off-by: hjiang <hjiang@anyscale.com>

--- ray-operator/test/e2eautoscaler/raycluster_autoscaler_test.go
+++ ray-operator/test/e2eautoscaler/raycluster_autoscaler_test.go
@@ -1,7 +1,9 @@
 package e2eautoscaler
 
 import (
+	"sync"
 	"testing"
+	"time"
 
 	"github.com/onsi/gomega"
 
@@ -193,4 +195,85 @@ func TestRayClusterAutoscalerWithCustomResource(t *testing.T) {
 		g.Eventually(RayCluster(test, rayCluster.Namespace, rayCluster.Name), TestTimeoutMedium).
 			Should(gomega.WithTransform(RayClusterDesiredWorkerReplicas, gomega.Equal(int32(0))))
 	})
+}
+
+func TestRayClusterAutoscalerWithDesiredState(t *testing.T) {
+	test := With(t)
+	g := gomega.NewWithT(t)
+
+	var rayCluster *rayv1.RayCluster
+	const maxReplica = 3
+
+	// Used in main goroutine to block wait until kubernetes setup completion.
+	var setupReadyWg sync.WaitGroup
+	// Used in main goroutine to block wait until test script execution completion.
+	var taskExecWg sync.WaitGroup
+
+	// Create a namespace
+	namespace := test.NewTestNamespace()
+	test.StreamKubeRayOperatorLogs()
+
+	// Scripts for creating and terminating detached actors to trigger autoscaling
+	scriptsAC := newConfigMap(namespace.Name, "scripts", files(test, "create_concurrent_tasks.py"))
+	scripts, err := test.Client().Core().CoreV1().ConfigMaps(namespace.Name).Apply(test.Ctx(), scriptsAC, TestApplyOptions)
+	g.Expect(err).NotTo(gomega.HaveOccurred())
+	test.T().Logf("Created ConfigMap %s/%s successfully", scripts.Namespace, scripts.Name)
+
+	setupReadyWg.Add(1)
+	taskExecWg.Add(1)
+	go func() {
+		test.T().Run("Create a RayCluster with autoscaling enabled", func(_ *testing.T) {
+			groupName := "custom-resource-group"
+
+			rayClusterSpecAC := rayv1ac.RayClusterSpec().
+				WithEnableInTreeAutoscaling(true).
+				WithRayVersion(GetRayVersion()).
+				WithHeadGroupSpec(rayv1ac.HeadGroupSpec().
+					WithRayStartParams(map[string]string{"num-cpus": "0"}).
+					WithTemplate(headPodTemplateApplyConfiguration())).
+				WithWorkerGroupSpecs(rayv1ac.WorkerGroupSpec().
+					WithReplicas(0).
+					WithMinReplicas(0).
+					WithMaxReplicas(maxReplica).
+					WithGroupName(groupName).
+					WithRayStartParams(map[string]string{"num-cpus": "1", "resources": `'{"CustomResource": 1}'`}).
+					WithTemplate(workerPodTemplateApplyConfiguration()))
+			rayClusterAC := rayv1ac.RayCluster("ray-cluster", namespace.Name).
+				WithSpec(apply(rayClusterSpecAC, mountConfigMap[rayv1ac.RayClusterSpecApplyConfiguration](scripts, "/home/ray/test_scripts")))
+
+			var err error
+			rayCluster, err = test.Client().Ray().RayV1().RayClusters(namespace.Name).Apply(test.Ctx(), rayClusterAC, TestApplyOptions)
+			g.Expect(err).NotTo(gomega.HaveOccurred())
+			test.T().Logf("Created RayCluster %s/%s successfully", rayCluster.Namespace, rayCluster.Name)
+			setupReadyWg.Done()
+
+			// Wait for RayCluster to become ready and verify the number of available worker replicas.
+			g.Eventually(RayCluster(test, rayCluster.Namespace, rayCluster.Name), TestTimeoutMedium).
+				Should(gomega.WithTransform(RayClusterState, gomega.Equal(rayv1.Ready)))
+			g.Expect(GetRayCluster(test, rayCluster.Namespace, rayCluster.Name)).To(gomega.WithTransform(RayClusterDesiredWorkerReplicas, gomega.Equal(int32(0))))
+
+			headPod, err := GetHeadPod(test, rayCluster)
+			g.Expect(err).NotTo(gomega.HaveOccurred())
+			test.T().Logf("Found head pod %s/%s", headPod.Namespace, headPod.Name)
+
+			// Create a number of tasks and wait for their completion, and a worker in the "custom-resource-group" should be created.
+			ExecPodCmd(test, headPod, common.RayHeadContainer, []string{"python", "/home/ray/test_scripts/create_concurrent_tasks.py"})
+			taskExecWg.Done()
+		})
+	}()
+
+	// Periodically check whether replica count has autoscaled to maximum value.
+	setupReadyWg.Wait()
+	ticker := time.NewTicker(200 * time.Millisecond)
+	defer ticker.Stop()
+
+	// Block until current replica has reached the preset maximum value, which proves the autoscaling works.
+	for range ticker.C {
+		pods, _ := GetWorkerPods(test, rayCluster)
+		if len(pods) == maxReplica {
+			break
+		}
+	}
+
+	taskExecWg.Wait()
 }
