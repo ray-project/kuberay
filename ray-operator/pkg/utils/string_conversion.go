c3cc2182af58e321f6f723518eb62fedf5499950
hjiang <hjiang@anyscale.com>
2024-12-09 21:55:46 +0000

    add comments and use case
    Signed-off-by: hjiang <hjiang@anyscale.com>

--- ray-operator/pkg/utils/string_conversion.go
+++ ray-operator/pkg/utils/string_conversion.go
@@ -1,4 +1,12 @@
-// Util functions for type conversion between byte array and string.
+// Util functions for type conversion between byte array and string without copy.
+//
+// Example usage:
+// func TakeByteSlice(bs []byte) {...}
+//
+// func f() {
+//   s := "helloworld"
+//   TakeByteSlice(ConvertStringToByteArray(s))  // convert string to byte slice with zero-copy
+// }
 
 package utils
 
@@ -7,11 +15,15 @@ import (
 )
 
 // Convert a byte array to string w/o copy.
+//
+// WARNING: The returned byte slice is not expected to change.
 func ConvertByteArrayToString(arr []byte) string {
 	return unsafe.String(&arr[0], len(arr))
 }
 
 // Convert a string to byte array w/o copy.
+//
+// WARNING: The returned byte slice is not expected to change.
 func ConvertStringToByteArray(s string) (arr []byte) {
 	return unsafe.Slice(unsafe.StringData(s), len(s))
 }
