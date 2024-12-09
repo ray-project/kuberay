edbb73107bf3ece055fe3ab368b28d4671a8ed7f
hjiang <hjiang@anyscale.com>
2024-12-09 23:03:06 +0000

    check address unchanged
    Signed-off-by: hjiang <hjiang@anyscale.com>

--- ray-operator/test/utils/string_conversion_test.go
+++ ray-operator/test/utils/string_conversion_test.go
@@ -2,6 +2,7 @@ package strconv
 
 import (
 	"testing"
+	"unsafe"
 
 	"github.com/onsi/gomega"
 
@@ -16,8 +17,10 @@ func TestStringConversion(t *testing.T) {
 	// Test string to byte array conversion.
 	arr := utils.ConvertStringToByteSlice(str)
 	g.Expect(arr).Should(gomega.Equal([]byte(str)))
+	g.Expect(&arr[0]).Should(gomega.Equal(unsafe.StringData(str)))
 
 	// Test byte array to string conversion.
 	convStr := utils.ConvertByteSliceToString(arr)
 	g.Expect(str).Should(gomega.Equal(convStr))
+	g.Expect(unsafe.StringData(convStr)).Should(gomega.Equal(&arr[0]))
 }
