d22ebe63e8a21a1eb910b6e76630e0b970cff7fe
Aaron Liang <aaronliang@google.com>
2024-12-10 15:53:06 -0800

    Add kubectl ray delete rayservice/job/cluster

--- kubectl-plugin/pkg/cmd/ray.go
+++ kubectl-plugin/pkg/cmd/ray.go
@@ -6,6 +6,7 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/ray-project/kuberay/kubectl-plugin/pkg/cmd/create"
+	kubectlraydelete "github.com/ray-project/kuberay/kubectl-plugin/pkg/cmd/delete"
 	"github.com/ray-project/kuberay/kubectl-plugin/pkg/cmd/get"
 	"github.com/ray-project/kuberay/kubectl-plugin/pkg/cmd/job"
 	"github.com/ray-project/kuberay/kubectl-plugin/pkg/cmd/log"
@@ -33,6 +34,7 @@ func NewRayCommand(streams genericiooptions.IOStreams) *cobra.Command {
 	cmd.AddCommand(job.NewJobCommand(streams))
 	cmd.AddCommand(version.NewVersionCommand(streams))
 	cmd.AddCommand(create.NewCreateCommand(streams))
+	cmd.AddCommand(kubectlraydelete.NewDeleteCommand(streams))
 
 	return cmd
 }
