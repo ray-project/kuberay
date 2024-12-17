87388748484183f2fb88ffbd35b9a90754109ec8
Rueian <rueiancsie@gmail.com>
2024-12-18 07:55:01 +0800

    [RayCluster][CI] fail fast all support functions in test/support/yaml.go
    Signed-off-by: Rueian <rueiancsie@gmail.com>

--- ray-operator/test/support/yaml.go
+++ ray-operator/test/support/yaml.go
@@ -4,7 +4,7 @@ import (
 	"os"
 	"os/exec"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"k8s.io/apimachinery/pkg/runtime"
 
@@ -28,7 +28,7 @@ func DeserializeRayClusterYAML(t Test, filename string) *rayv1.RayCluster {
 	t.T().Helper()
 	rayCluster := &rayv1.RayCluster{}
 	err := deserializeYAML(filename, rayCluster)
-	assert.NoError(t.T(), err)
+	require.NoError(t.T(), err)
 	return rayCluster
 }
 
@@ -36,7 +36,7 @@ func DeserializeRayJobYAML(t Test, filename string) *rayv1.RayJob {
 	t.T().Helper()
 	rayJob := &rayv1.RayJob{}
 	err := deserializeYAML(filename, rayJob)
-	assert.NoError(t.T(), err)
+	require.NoError(t.T(), err)
 	return rayJob
 }
 
@@ -44,7 +44,7 @@ func DeserializeRayServiceYAML(t Test, filename string) *rayv1.RayService {
 	t.T().Helper()
 	rayService := &rayv1.RayService{}
 	err := deserializeYAML(filename, rayService)
-	assert.NoError(t.T(), err)
+	require.NoError(t.T(), err)
 	return rayService
 }
 
@@ -52,9 +52,7 @@ func KubectlApplyYAML(t Test, filename string, namespace string) {
 	t.T().Helper()
 	kubectlCmd := exec.CommandContext(t.Ctx(), "kubectl", "apply", "-f", filename, "-n", namespace)
 	err := kubectlCmd.Run()
-	if err != nil {
-		t.T().Fatalf("Failed to apply %s to namespace %s: %v", filename, namespace, err)
-	}
+	require.NoError(t.T(), err)
 	t.T().Logf("Successfully applied %s to namespace %s", filename, namespace)
 }
 
@@ -62,7 +60,7 @@ func KubectlApplyQuota(t Test, namespace, quota string) {
 	t.T().Helper()
 	kubectlCmd := exec.CommandContext(t.Ctx(), "kubectl", "create", "quota", namespace, "-n", namespace, quota)
 	err := kubectlCmd.Run()
-	assert.NoError(t.T(), err)
+	require.NoError(t.T(), err)
 	t.T().Logf("Successfully applied quota %s in %s", quota, namespace)
 }
 
@@ -70,6 +68,6 @@ func KubectlDeleteAllPods(t Test, namespace string) {
 	t.T().Helper()
 	kubectlCmd := exec.CommandContext(t.Ctx(), "kubectl", "delete", "--all", "pods", "-n", namespace)
 	err := kubectlCmd.Run()
-	assert.NoError(t.T(), err)
+	require.NoError(t.T(), err)
 	t.T().Logf("Successfully delete pods in %s", namespace)
 }
