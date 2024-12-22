f8f798911fdfadb8306f738c8d853557a437f2be
Cheyu Wu <cheyu1220@gmail.com>
2024-12-22 15:03:36 +0800

    feat: Add a new event type FailedToDeleteWorkerPodCollection

--- ray-operator/controllers/ray/utils/constant.go
+++ ray-operator/controllers/ray/utils/constant.go
@@ -246,10 +246,11 @@ const (
 	FailedToDeleteHeadPod K8sEventType = "FailedToDeleteHeadPod"
 
 	// Worker Pod event list
-	CreatedWorkerPod        K8sEventType = "CreatedWorkerPod"
-	FailedToCreateWorkerPod K8sEventType = "FailedToCreateWorkerPod"
-	DeletedWorkerPod        K8sEventType = "DeletedWorkerPod"
-	FailedToDeleteWorkerPod K8sEventType = "FailedToDeleteWorkerPod"
+	CreatedWorkerPod                  K8sEventType = "CreatedWorkerPod"
+	FailedToCreateWorkerPod           K8sEventType = "FailedToCreateWorkerPod"
+	DeletedWorkerPod                  K8sEventType = "DeletedWorkerPod"
+	FailedToDeleteWorkerPod           K8sEventType = "FailedToDeleteWorkerPod"
+	FailedToDeleteWorkerPodCollection K8sEventType = "FailedToDeleteWorkerPodCollection"
 
 	// Redis Cleanup Job event list
 	CreatedRedisCleanupJob        K8sEventType = "CreatedRedisCleanupJob"
