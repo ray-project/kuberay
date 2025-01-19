98f33d41b742ced6cf09f42cd92b2d042cc211dd
Cheyu Wu <cheyu1220@gmail.com>
2025-01-19 19:49:43 +0800

    test: add unit test for TestIsGCSFaultToleranceEnabled

---
+++ ray-operator/apis/ray/v1/pod_test.go
@@ -0,0 +1,75 @@
+package v1
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestIsGCSFaultToleranceEnabled(t *testing.T) {
+	tests := []struct {
+		name     string
+		instance RayCluster
+		expected bool
+	}{
+		{
+			name: "ray.io/ft-enabled is true",
+			instance: RayCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						RayFTEnabledAnnotationKey: "true",
+					},
+				},
+			},
+			expected: true,
+		},
+		{
+			name: "ray.io/ft-enabled is not set and GcsFaultToleranceOptions is set",
+			instance: RayCluster{
+				Spec: RayClusterSpec{
+					GcsFaultToleranceOptions: &GcsFaultToleranceOptions{},
+				},
+			},
+			expected: true,
+		},
+		{
+			name: "ray.io/ft-enabled is false",
+			instance: RayCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						RayFTEnabledAnnotationKey: "false",
+					},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "ray.io/ft-enabled is not set and GcsFaultToleranceOptions is not set",
+			instance: RayCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{},
+				},
+			},
+			expected: false,
+		},
+		{
+			name: "ray.io/ft-enabled is using uppercase true",
+			instance: RayCluster{
+				ObjectMeta: metav1.ObjectMeta{
+					Annotations: map[string]string{
+						RayFTEnabledAnnotationKey: "TRUE",
+					},
+				},
+			},
+			expected: true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := IsGCSFaultToleranceEnabled(test.instance)
+			assert.Equal(t, test.expected, result)
+		})
+	}
+}
