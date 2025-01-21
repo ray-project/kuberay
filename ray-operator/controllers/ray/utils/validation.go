8dd249602fb9c17a352c465b8d9baae6ad4776d5
Cheyu Wu <cheyu1220@gmail.com>
2025-01-22 02:18:50 +0800

    [Refactor] Move `validateRayClusterStatus` function to `validation.go`  and move unit test to  `validation_test.go` (#2780)

---
+++ ray-operator/controllers/ray/utils/validation.go
@@ -0,0 +1,18 @@
+package utils
+
+import (
+	errstd "errors"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+
+	rayv1 "github.com/ray-project/kuberay/ray-operator/apis/ray/v1"
+)
+
+func ValidateRayClusterStatus(instance *rayv1.RayCluster) error {
+	suspending := meta.IsStatusConditionTrue(instance.Status.Conditions, string(rayv1.RayClusterSuspending))
+	suspended := meta.IsStatusConditionTrue(instance.Status.Conditions, string(rayv1.RayClusterSuspended))
+	if suspending && suspended {
+		return errstd.New("invalid RayCluster State: rayv1.RayClusterSuspending and rayv1.RayClusterSuspended conditions should not be both true")
+	}
+	return nil
+}
