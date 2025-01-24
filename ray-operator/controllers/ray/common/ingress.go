8382d595898fb600e2fcdec95e9203f4360dadfd
fscnick <fscnick.dev@gmail.com>
2025-01-24 20:03:58 +0800

    [Chore] make ingressClassName as a local variable
    Signed-off-by: fscnick <fscnick.dev@gmail.com>

--- ray-operator/controllers/ray/common/ingress.go
+++ ray-operator/controllers/ray/common/ingress.go
@@ -87,8 +87,7 @@ func BuildIngressForHeadService(ctx context.Context, cluster rayv1.RayCluster) (
 	}
 
 	// Get ingress class name from rayCluster annotations. this is a required field to use ingress.
-	ingressClassName, ok := cluster.Annotations[IngressClassAnnotationKey]
-	if !ok {
+	if ingressClassName, ok := cluster.Annotations[IngressClassAnnotationKey]; !ok {
 		log.Info("Ingress class annotation is not set for the cluster.", "clusterNamespace", cluster.Namespace, "clusterName", cluster.Name)
 	} else {
 		// TODO: in AWS EKS, set up IngressClassName will cause an error due to conflict with annotation.
