ba6a7a2fbf5bddc2cb6e4bf31fbb2f69f8975488
Owen Lin <106612301+owenowenisme@users.noreply.github.com>
2025-02-07 02:18:18 +0800

    [CI] Add shellcheck and fix error of it (#2933)

--- apiserver/hack/update-swagger-ui.bash
+++ apiserver/hack/update-swagger-ui.bash
@@ -1,19 +1,20 @@
 #!/bin/bash
 set -e
 set -x
-readonly REPO_ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"/../.. &> /dev/null && pwd)
+REPO_ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )"/../.. &> /dev/null && pwd)
+readonly REPO_ROOT_DIR
 readonly TARGET_DIR="${REPO_ROOT_DIR}/third_party/swagger-ui"
 readonly SWAGGER_UI_VERSION=${1:-"5.4.1"}
 readonly SWAGGER_UI_TAR_URL="https://github.com/swagger-api/swagger-ui/archive/refs/tags/v${SWAGGER_UI_VERSION}.tar.gz"
 
-if [ -f ${TARGET_DIR}/swagger-initializer.js ];then
-    cp -v ${TARGET_DIR}/swagger-initializer.js ${TARGET_DIR}/swagger-initializer.js.backup
+if [ -f "${TARGET_DIR}"/swagger-initializer.js ];then
+    cp -v "${TARGET_DIR}"/swagger-initializer.js "${TARGET_DIR}"/swagger-initializer.js.backup
 fi
 echo "Downloading '${SWAGGER_UI_TAR_URL}' to update ${TARGET_DIR}"
 tmp="$(mktemp -d)"
 #pushd .
-curl --output-dir ${tmp} --fail --silent --location --remote-header-name --remote-name ${SWAGGER_UI_TAR_URL}
-tar -xzvf ${tmp}/swagger-ui-${SWAGGER_UI_VERSION}.tar.gz -C ${tmp}
+curl --output-dir "${tmp}" --fail --silent --location --remote-header-name --remote-name "${SWAGGER_UI_TAR_URL}"
+tar -xzvf "${tmp}"/swagger-ui-"${SWAGGER_UI_VERSION}".tar.gz -C "${tmp}"
 #popd
 cp -rv "$tmp/swagger-ui-${SWAGGER_UI_VERSION}/dist/"* "${TARGET_DIR}"
 rm -rf "$tmp"
--- helm-chart/script/chart-test.sh
+++ helm-chart/script/chart-test.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
-if [ -L ${BASH_SOURCE-$0} ]; then
-  PWD=$(dirname $(readlink "${BASH_SOURCE-$0}"))
+if [ -L "${BASH_SOURCE-$0}" ]; then
+  PWD=$(dirname "$(readlink "${BASH_SOURCE-$0}")")
 else
-  PWD=$(dirname ${BASH_SOURCE-$0})
+  PWD=$(dirname "${BASH_SOURCE-$0}")
 fi
-export CURRENT_PATH=$(cd "${PWD}">/dev/null; pwd)
+CURRENT_PATH=$(cd "${PWD}">/dev/null || exit 1; pwd)
+export CURRENT_PATH
 export KUBERAY_HOME=${CURRENT_PATH}/../../
 
-cd $KUBERAY_HOME
+cd "$KUBERAY_HOME" || exit 1
 if [ "$#" == 1 ] && [ "$1" == "local" ]; then
   ct lint --all --chart-dirs helm-chart/ --validate-maintainers=false
 else
   docker run -it --network host --workdir=/data --volume ~/.kube/config:/root/.kube/config:ro \
-  --volume $(pwd):/data quay.io/helmpack/chart-testing:v3.5.0 \
+  --volume "$(pwd)":/data quay.io/helmpack/chart-testing:v3.5.0 \
   ct lint --all --chart-dirs helm-chart/ --validate-maintainers=false
 fi
