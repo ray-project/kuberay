165a351d0e2b6a9749f03e2a560435b727c8b201
fscnick <6858627+fscnick@users.noreply.github.com>
2025-02-08 03:50:54 +0800

    [Test] Use GcsFaultToleranceOptions in test and backward compatibility (#2972)

--- ray-operator/test/sampleyaml/raycluster_test.go
+++ ray-operator/test/sampleyaml/raycluster_test.go
@@ -29,6 +29,12 @@ func TestRayCluster(t *testing.T) {
 			name: "ray-cluster.custom-head-service.yaml",
 		},
 		{
+			name: "ray-cluster.deprecate-gcs-ft.yaml",
+		},
+		{
+			name: "ray-cluster.persistent-redis.yaml",
+		},
+		{
 			name: "ray-cluster.embed-grafana.yaml",
 		},
 		{
