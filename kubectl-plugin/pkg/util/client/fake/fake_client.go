edecdbc1db0f6d41a985998d351b8d826d094642
Owen Lin <106612301+owenowenisme@users.noreply.github.com>
2025-02-28 03:17:49 +0800

    [Refactor][kubectl-plugin] Move fakeClient in version_test.go to client/fake (#3113)

---
+++ kubectl-plugin/pkg/util/client/fake/fake_client.go
@@ -0,0 +1,50 @@
+package fake
+
+import (
+	"context"
+	"time"
+
+	"github.com/ray-project/kuberay/kubectl-plugin/pkg/util"
+	"k8s.io/client-go/kubernetes"
+
+	rayclient "github.com/ray-project/kuberay/ray-operator/pkg/client/clientset/versioned"
+)
+
+type FakeClient struct {
+	err                 error
+	kuberayImageVersion string
+}
+
+func NewFakeClient() *FakeClient {
+	return &FakeClient{}
+}
+
+func (c *FakeClient) WithKubeRayImageVersion(version string) *FakeClient {
+	c.kuberayImageVersion = version
+	return c
+}
+
+func (c *FakeClient) WithKubeRayOperatorVersionError(err error) *FakeClient {
+	c.err = err
+	return c
+}
+
+func (c *FakeClient) GetKubeRayOperatorVersion(_ context.Context) (string, error) {
+	return c.kuberayImageVersion, c.err
+}
+
+func (c *FakeClient) GetRayHeadSvcName(_ context.Context, _ string, _ util.ResourceType, _ string) (string, error) {
+	return "", nil
+}
+
+func (c *FakeClient) WaitRayClusterProvisioned(_ context.Context, _ string, _ string, _ time.Duration) error {
+	return nil
+}
+
+func (c *FakeClient) KubernetesClient() kubernetes.Interface {
+	return nil
+}
+
+func (c *FakeClient) RayClient() rayclient.Interface {
+	return nil
+}
