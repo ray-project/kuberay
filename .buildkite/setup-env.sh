79c7c87ffe75e3566a730caf7192d11801a65dbf
Owen Lin <106612301+owenowenisme@users.noreply.github.com>
2025-03-01 06:02:50 +0800

    [Bug] Re-enable flaky kubectl plugin e2e test in kubectl_ray_job_submit_test.go (#3124)

--- .buildkite/setup-env.sh
+++ .buildkite/setup-env.sh
@@ -27,5 +27,12 @@ mv linux-amd64/helm /usr/local/bin/helm
 helm repo add kuberay https://ray-project.github.io/kuberay-helm/
 helm repo update
 
+# Install python 3.11 and pip
+apt-get update
+apt-get install -y python3.11 python3-pip
+
+# Install requirements
+pip install --break-system-packages ray[default]==2.41.0
+
 # Bypass Git's ownership check due to unconventional user IDs in Docker containers
 git config --global --add safe.directory /workdir
