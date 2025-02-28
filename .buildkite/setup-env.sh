5f8e3312b41c21cfa8e0fcf96d6e34ec7f013293
You-Cheng <mses010108@gmail.com>
2025-02-28 06:34:02 +0000

    remove using subshell in setup-env
    Signed-off-by: You-Cheng <mses010108@gmail.com>

--- .buildkite/setup-env.sh
+++ .buildkite/setup-env.sh
@@ -27,5 +27,19 @@ mv linux-amd64/helm /usr/local/bin/helm
 helm repo add kuberay https://ray-project.github.io/kuberay-helm/
 helm repo update
 
+# Install python 3.11 and pip
+apt-get update
+apt-get install -y python3.11 python3.11-venv
+python3 -m venv .venv
+
+# Activate the virtual environment and then execute the subsequent commands
+
+# shellcheck disable=SC1091  # Ignore: activate script is created by venv
+source .venv/bin/activate
+
+# Install requirements
+pip install -r tests/framework/config/requirements.txt
+
+
 # Bypass Git's ownership check due to unconventional user IDs in Docker containers
 git config --global --add safe.directory /workdir
