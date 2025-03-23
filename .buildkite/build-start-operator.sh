23211ae714bb1c41ceed755d895fe32810262db5
dayshah <dhyey2019@gmail.com>
2025-03-23 14:01:07 -0700

    fix script
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- .buildkite/build-start-operator.sh
+++ .buildkite/build-start-operator.sh
@@ -4,10 +4,6 @@ if [ "$IS_FROM_RAY_RELEASE_AUTOMATION" = 1 ]; then
     helm install kuberay/kuberay-operator --version 1.3.0;
 fi
 
-if [ "$IS_FROM_RAY_RELEASE_AUTOMATION" != 0 ]; then
-    helm install kuberay/kuberay-operator --version 1.3.0 --set image.tag=latest;
-fi
-
 if [ "$IS_FROM_RAY_RELEASE_AUTOMATION" != 1 ];
     then IMG=kuberay/operator:nightly make docker-image &&
     kind load docker-image kuberay/operator:nightly &&
