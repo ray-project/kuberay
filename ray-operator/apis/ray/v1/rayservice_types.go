7b10c461101c4346d91b8750ef2c87d759bfc561
Ryan O'Leary <ryanaoleary@google.com>
2025-03-25 01:50:30 +0000

    Fix field alignment
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>

--- ray-operator/apis/ray/v1/rayservice_types.go
+++ ray-operator/apis/ray/v1/rayservice_types.go
@@ -156,7 +156,10 @@ type RayServiceStatus struct {
 	TargetCapacity *int32 `json:"targetCapacity,omitempty"`
 	// Represents the percentage weight of traffic routed to the RayCluster associated with this RayServiceStatus.
 	TrafficRoutedPercent *int32 `json:"trafficRoutedPercent,omitempty"`
-	RayClusterName       string `json:"rayClusterName,omitempty"`
+	// Represents the last time the RayService controller migrated traffic during an IncrementalUpgrade.
+	LastTrafficMigratedTime *metav1.Time     `json:"lastTrafficMigratedTime,omitempty"`
+	RayClusterName          string           `json:"rayClusterName,omitempty"`
+	RayClusterStatus        RayClusterStatus `json:"rayClusterStatus,omitempty"`
 }
 
 type AppStatus struct {
