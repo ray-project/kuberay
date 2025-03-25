5ad9846537538b8e7cbf1a7d2623eafae07ee85e
win5923 <ken89@kimo.com>
2025-03-25 15:27:01 +0000

    [Prometheus] Add ray_services_created_total metric

--- ray-operator/controllers/ray/common/metrics.go
+++ ray-operator/controllers/ray/common/metrics.go
@@ -8,31 +8,10 @@ import (
 
 // Define all the prometheus counters for all clusters
 var (
-	clustersCreatedCount = promauto.NewCounterVec(
+	rayServicesCreatedCounter = promauto.NewCounterVec(
 		prometheus.CounterOpts{
-			Name: "ray_operator_clusters_created_total",
-			Help: "Counts number of clusters created",
-		},
-		[]string{"namespace"},
-	)
-	clustersDeletedCount = promauto.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "ray_operator_clusters_deleted_total",
-			Help: "Counts number of clusters deleted",
-		},
-		[]string{"namespace"},
-	)
-	clustersSuccessfulCount = promauto.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "ray_operator_clusters_successful_total",
-			Help: "Counts number of clusters successful",
-		},
-		[]string{"namespace"},
-	)
-	clustersFailedCount = promauto.NewCounterVec(
-		prometheus.CounterOpts{
-			Name: "ray_operator_clusters_failed_total",
-			Help: "Counts number of clusters failed",
+			Name: "ray_services_created_total",
+			Help: "The total number of RayServices created",
 		},
 		[]string{"namespace"},
 	)
@@ -40,25 +19,9 @@ var (
 
 func init() {
 	// Register custom metrics with the global prometheus registry
-	metrics.Registry.MustRegister(clustersCreatedCount,
-		clustersDeletedCount,
-		clustersSuccessfulCount,
-		clustersFailedCount)
+	metrics.Registry.MustRegister(rayServicesCreatedCounter)
 }
 
-func CreatedClustersCounterInc(namespace string) {
-	clustersCreatedCount.WithLabelValues(namespace).Inc()
-}
-
-// TODO: We don't handle the delete events in new reconciler mode, how to emit deletion metrics?
-func DeletedClustersCounterInc(namespace string) {
-	clustersDeletedCount.WithLabelValues(namespace).Inc()
-}
-
-func SuccessfulClustersCounterInc(namespace string) {
-	clustersSuccessfulCount.WithLabelValues(namespace).Inc()
-}
-
-func FailedClustersCounterInc(namespace string) {
-	clustersFailedCount.WithLabelValues(namespace).Inc()
+func CreatedRayServicesCounterInc(namespace string) {
+	rayServicesCreatedCounter.WithLabelValues(namespace).Inc()
 }
