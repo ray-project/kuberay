c728d9d5df266a2a7e5ecb1394e09f69bb56efab
Cheyu Wu <cheyu1220@gmail.com>
2025-03-30 16:11:06 +0800

    fix: rayStartParams error when creating workgroup

--- kubectl-plugin/pkg/cmd/create/create_workergroup_test.go
+++ kubectl-plugin/pkg/cmd/create/create_workergroup_test.go
@@ -14,54 +14,115 @@ import (
 )
 
 func TestCreateWorkerGroupSpec(t *testing.T) {
-	rayStartParams := map[string]string{"dashboard-host": "0.0.0.0", "num-cpus": "2"}
-	options := &CreateWorkerGroupOptions{
-		groupName:         "example-group",
-		image:             "DEADBEEF",
-		workerReplicas:    3,
-		workerMinReplicas: 1,
-		workerMaxReplicas: 5,
-		workerCPU:         "2",
-		workerMemory:      "5Gi",
-		workerGPU:         "1",
-		rayStartParams:    rayStartParams,
-		workerNodeSelectors: map[string]string{
-			"worker-node-selector": "worker-node-selector-value",
-		},
-	}
-
-	expected := rayv1.WorkerGroupSpec{
-		RayStartParams: rayStartParams,
-		GroupName:      "example-group",
-		Template: corev1.PodTemplateSpec{
-			Spec: corev1.PodSpec{
-				Containers: []corev1.Container{
-					{
-						Name:  "ray-worker",
-						Image: "DEADBEEF",
-						Resources: corev1.ResourceRequirements{
-							Requests: corev1.ResourceList{
-								corev1.ResourceCPU:     resource.MustParse("2"),
-								corev1.ResourceMemory:  resource.MustParse("5Gi"),
-								util.ResourceNvidiaGPU: resource.MustParse("1"),
-							},
-							Limits: corev1.ResourceList{
-								corev1.ResourceCPU:     resource.MustParse("2"),
-								corev1.ResourceMemory:  resource.MustParse("5Gi"),
-								util.ResourceNvidiaGPU: resource.MustParse("1"),
-							},
-						},
-					},
-				},
-				NodeSelector: map[string]string{
+	tests := []struct {
+		name     string
+		options  *CreateWorkerGroupOptions
+		expected rayv1.WorkerGroupSpec
+	}{
+		{
+			name: "default worker group spec",
+			options: &CreateWorkerGroupOptions{
+				groupName:         "example-group",
+				image:             "DEADBEEF",
+				workerReplicas:    3,
+				workerMinReplicas: 1,
+				workerMaxReplicas: 5,
+				workerCPU:         "2",
+				workerMemory:      "5Gi",
+				workerGPU:         "1",
+				rayStartParams:    map[string]string{"dashboard-host": "0.0.0.0", "num-cpus": "2"},
+				workerNodeSelectors: map[string]string{
 					"worker-node-selector": "worker-node-selector-value",
 				},
 			},
+			expected: rayv1.WorkerGroupSpec{
+				RayStartParams: map[string]string{"dashboard-host": "0.0.0.0", "num-cpus": "2"},
+				GroupName:      "example-group",
+				Template: corev1.PodTemplateSpec{
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Name:  "ray-worker",
+								Image: "DEADBEEF",
+								Resources: corev1.ResourceRequirements{
+									Requests: corev1.ResourceList{
+										corev1.ResourceCPU:     resource.MustParse("2"),
+										corev1.ResourceMemory:  resource.MustParse("5Gi"),
+										util.ResourceNvidiaGPU: resource.MustParse("1"),
+									},
+									Limits: corev1.ResourceList{
+										corev1.ResourceCPU:     resource.MustParse("2"),
+										corev1.ResourceMemory:  resource.MustParse("5Gi"),
+										util.ResourceNvidiaGPU: resource.MustParse("1"),
+									},
+								},
+							},
+						},
+						NodeSelector: map[string]string{
+							"worker-node-selector": "worker-node-selector-value",
+						},
+					},
+				},
+				Replicas:    ptr.To[int32](3),
+				MinReplicas: ptr.To[int32](1),
+				MaxReplicas: ptr.To[int32](5),
+			},
 		},
-		Replicas:    ptr.To[int32](3),
-		MinReplicas: ptr.To[int32](1),
-		MaxReplicas: ptr.To[int32](5),
+		{
+			name: "worker group spec with nil ray start params",
+			options: &CreateWorkerGroupOptions{
+				groupName:         "example-group",
+				image:             "DEADBEEF",
+				workerReplicas:    3,
+				workerMinReplicas: 1,
+				workerMaxReplicas: 5,
+				workerCPU:         "2",
+				workerMemory:      "5Gi",
+				workerGPU:         "1",
+				rayStartParams:    nil,
+				workerNodeSelectors: map[string]string{
+					"worker-node-selector": "worker-node-selector-value",
+				},
+			},
+			expected: rayv1.WorkerGroupSpec{
+				RayStartParams: map[string]string{},
+				GroupName:      "example-group",
+				Template: corev1.PodTemplateSpec{
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Name:  "ray-worker",
+								Image: "DEADBEEF",
+								Resources: corev1.ResourceRequirements{
+									Requests: corev1.ResourceList{
+										corev1.ResourceCPU:     resource.MustParse("2"),
+										corev1.ResourceMemory:  resource.MustParse("5Gi"),
+										util.ResourceNvidiaGPU: resource.MustParse("1"),
+									},
+									Limits: corev1.ResourceList{
+										corev1.ResourceCPU:     resource.MustParse("2"),
+										corev1.ResourceMemory:  resource.MustParse("5Gi"),
+										util.ResourceNvidiaGPU: resource.MustParse("1"),
+									},
+								},
+							},
+						},
+						NodeSelector: map[string]string{
+							"worker-node-selector": "worker-node-selector-value",
+						},
+					},
+				},
+				Replicas:    ptr.To[int32](3),
+				MinReplicas: ptr.To[int32](1),
+				MaxReplicas: ptr.To[int32](5),
+			},
+		},
 	}
 
-	assert.Equal(t, expected, createWorkerGroupSpec(options))
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, tt.expected, createWorkerGroupSpec(tt.options))
+		})
+	}
+
 }
