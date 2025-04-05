77918094df385b43a09922572cfbea35969f5b26
machichima <nary12321@gmail.com>
2025-04-05 17:50:00 +0800

    refactor: remove unnecessary comments -e Signed-off-by: machichima <nary12321@gmail.com>

--- ray-operator/controllers/ray/common/service.go
+++ ray-operator/controllers/ray/common/service.go
@@ -385,10 +385,6 @@ func getServicePorts(cluster rayv1.RayCluster) (map[string]int32, error) {
 		usedPorts[port] = true
 	}
 
-	// 1. If a value is provided in the YAML file, overwrite the default value
-	// 2. If no port value is provided, use the default name and port
-	// 3. If a port value is provided in the YAML file, retain the original name
-
 	for name, defaultPort := range defaultPorts {
 		if _, defined := ports[name]; !defined {
 			// Only assign defaultPort if the port is not already in use
