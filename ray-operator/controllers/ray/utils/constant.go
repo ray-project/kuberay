f814ce8231cddaee2706eccbeaccf86ae6caeb3c
dayshah <dhyey2019@gmail.com>
2025-04-07 17:38:41 -0700

    new comments
    Signed-off-by: dayshah <dhyey2019@gmail.com>

--- ray-operator/controllers/ray/utils/constant.go
+++ ray-operator/controllers/ray/utils/constant.go
@@ -143,7 +143,8 @@ const (
 	// If set to true, the RayJob CR itself will be deleted if shutdownAfterJobFinishes is set to true. Note that all resources created by the RayJob CR will be deleted, including the K8s Job.
 	DELETE_RAYJOB_CR_AFTER_JOB_FINISHES = "DELETE_RAYJOB_CR_AFTER_JOB_FINISHES"
 
-	// What to to run when the Kubernetes Pod PreStop hook is called.
+	// If this environment variable is set, its value will be injected into the PreStop hooks of the Ray
+	// containers in all Ray Pods—including both head and worker Pods.
 	PRE_STOP_COMMAND_LIST_JSON = "PRE_STOP_COMMAND_LIST_JSON"
 
 	// Ray core default configurations
