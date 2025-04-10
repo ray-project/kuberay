f789988b95a12ce2751595a97912d31b4b821dcb
David Xia <david@davidxia.com>
2025-04-10 17:51:39 -0400

    [feat][plugin] support creating RayCluster with config file (#3225)

--- kubectl-plugin/pkg/cmd/create/create_cluster.go
+++ kubectl-plugin/pkg/cmd/create/create_cluster.go
@@ -5,17 +5,20 @@ import (
 	"fmt"
 	"time"
 
+	"k8s.io/utils/ptr"
+
 	"github.com/ray-project/kuberay/kubectl-plugin/pkg/util"
 	"github.com/ray-project/kuberay/kubectl-plugin/pkg/util/generation"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
-	"k8s.io/utils/ptr"
 
 	"github.com/ray-project/kuberay/kubectl-plugin/pkg/util/client"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	cmdutil "k8s.io/kubectl/pkg/cmd/util"
 	"k8s.io/kubectl/pkg/util/templates"
 
+	rayv1ac "github.com/ray-project/kuberay/ray-operator/pkg/client/applyconfiguration/ray/v1"
 	rayclient "github.com/ray-project/kuberay/ray-operator/pkg/client/clientset/versioned"
 )
 
@@ -28,6 +31,7 @@ type CreateClusterOptions struct {
 	headRayStartParams     map[string]string
 	headNodeSelectors      map[string]string
 	workerNodeSelectors    map[string]string
+	rayClusterConfig       *generation.RayClusterConfig
 	namespace              string
 	clusterName            string
 	rayVersion             string
@@ -41,6 +45,7 @@ type CreateClusterOptions struct {
 	workerEphemeralStorage string
 	workerGPU              string
 	workerTPU              string
+	configFile             string
 	timeout                time.Duration
 	numOfHosts             int32
 	workerReplicas         int32
@@ -70,6 +75,9 @@ var (
 
 		# For more details on TPU-related node selectors like %s and %s, refer to:
 		# https://cloud.google.com/kubernetes-engine/docs/concepts/plan-tpus#availability
+
+		# Create a Ray cluster from a YAML configuration file
+		kubectl ray create cluster sample-cluster --file ray-cluster-config.yaml
 	`, util.RayVersion, util.RayImage, util.NodeSelectorGKETPUAccelerator, util.NodeSelectorGKETPUTopology, util.NodeSelectorGKETPUAccelerator, util.NodeSelectorGKETPUTopology))
 )
 
@@ -93,7 +101,7 @@ func NewCreateClusterCommand(cmdFactory cmdutil.Factory, streams genericclioptio
 			if err := options.Complete(cmd, args); err != nil {
 				return err
 			}
-			if err := options.Validate(); err != nil {
+			if err := options.Validate(cmd); err != nil {
 				return err
 			}
 
@@ -106,34 +114,35 @@ func NewCreateClusterCommand(cmdFactory cmdutil.Factory, streams genericclioptio
 		},
 	}
 
+	cmd.Flags().StringToStringVar(&options.labels, "labels", nil, "K8s labels (e.g. --labels app=ray,env=dev)")
+	cmd.Flags().StringToStringVar(&options.annotations, "annotations", nil, "K8s annotations (e.g. --annotations ttl-hours=24,owner=chthulu)")
 	cmd.Flags().StringVar(&options.rayVersion, "ray-version", util.RayVersion, "Ray version to use")
 	cmd.Flags().StringVar(&options.image, "image", fmt.Sprintf("rayproject/ray:%s", options.rayVersion), "container image to use")
-	cmd.Flags().StringVar(&options.headCPU, "head-cpu", "2", "number of CPUs in the Ray head")
-	cmd.Flags().StringVar(&options.headMemory, "head-memory", "4Gi", "amount of memory in the Ray head")
-	cmd.Flags().StringVar(&options.headGPU, "head-gpu", "0", "number of GPUs in the Ray head")
-	cmd.Flags().StringVar(&options.headEphemeralStorage, "head-ephemeral-storage", "", "amount of ephemeral storage in the Ray head")
+	cmd.Flags().StringVar(&options.headCPU, "head-cpu", util.DefaultHeadCPU, "number of CPUs in the Ray head")
+	cmd.Flags().StringVar(&options.headMemory, "head-memory", util.DefaultHeadMemory, "amount of memory in the Ray head")
+	cmd.Flags().StringVar(&options.headGPU, "head-gpu", util.DefaultHeadGPU, "number of GPUs in the Ray head")
+	cmd.Flags().StringVar(&options.headEphemeralStorage, "head-ephemeral-storage", util.DefaultHeadEphemeralStorage, "amount of ephemeral storage in the Ray head")
 	cmd.Flags().StringToStringVar(&options.headRayStartParams, "head-ray-start-params", options.headRayStartParams, "a map of arguments to the Ray head's 'ray start' entrypoint, e.g. '--head-ray-start-params dashboard-host=0.0.0.0,num-cpus=2'")
-	cmd.Flags().Int32Var(&options.workerReplicas, "worker-replicas", 1, "desired worker group replicas")
-	cmd.Flags().Int32Var(&options.numOfHosts, "num-of-hosts", 1, "number of hosts in default worker group per replica")
-	cmd.Flags().StringVar(&options.workerCPU, "worker-cpu", "2", "number of CPUs in each worker group replica")
-	cmd.Flags().StringVar(&options.workerMemory, "worker-memory", "4Gi", "amount of memory in each worker group replica")
-	cmd.Flags().StringVar(&options.workerGPU, "worker-gpu", "0", "number of GPUs in each worker group replica")
-	cmd.Flags().StringVar(&options.workerTPU, "worker-tpu", "0",
+	cmd.Flags().StringToStringVar(&options.headNodeSelectors, "head-node-selectors", nil, "Node selectors to apply to all head pods in the cluster (e.g. --head-node-selector=cloud.google.com/gke-accelerator=nvidia-l4,cloud.google.com/gke-nodepool=my-node-pool)")
+	cmd.Flags().Int32Var(&options.workerReplicas, "worker-replicas", util.DefaultWorkerReplicas, "desired worker group replicas")
+	cmd.Flags().StringVar(&options.workerCPU, "worker-cpu", util.DefaultWorkerCPU, "number of CPUs in each worker group replica")
+	cmd.Flags().StringVar(&options.workerMemory, "worker-memory", util.DefaultWorkerMemory, "amount of memory in each worker group replica")
+	cmd.Flags().StringVar(&options.workerGPU, "worker-gpu", util.DefaultWorkerGPU, "number of GPUs in each worker group replica")
+	cmd.Flags().StringVar(&options.workerEphemeralStorage, "worker-ephemeral-storage", util.DefaultWorkerEphemeralStorage, "amount of ephemeral storage in each worker group replica")
+	cmd.Flags().StringVar(&options.workerTPU, "worker-tpu", util.DefaultWorkerTPU,
 		fmt.Sprintf(
 			"number of TPUs in each worker group replica. If greater than 0, you must also set %s and %s in --worker-node-selectors.",
 			util.NodeSelectorGKETPUAccelerator,
 			util.NodeSelectorGKETPUTopology,
 		),
 	)
-	cmd.Flags().StringVar(&options.workerEphemeralStorage, "worker-ephemeral-storage", "", "amount of ephemeral storage in each worker group replica")
 	cmd.Flags().StringToStringVar(&options.workerRayStartParams, "worker-ray-start-params", options.workerRayStartParams, "a map of arguments to the Ray workers' 'ray start' entrypoint, e.g. '--worker-ray-start-params metrics-export-port=8080,num-cpus=2'")
+	cmd.Flags().StringToStringVar(&options.workerNodeSelectors, "worker-node-selectors", nil, "Node selectors to apply to all worker pods in the cluster (e.g. --worker-node-selector=cloud.google.com/gke-accelerator=nvidia-l4,cloud.google.com/gke-nodepool=my-node-pool)")
+	cmd.Flags().Int32Var(&options.numOfHosts, "num-of-hosts", util.DefaultNumOfHosts, "number of hosts in default worker group per replica")
+	cmd.Flags().StringVar(&options.configFile, "file", "", "path to a YAML file containing Ray cluster configuration")
 	cmd.Flags().BoolVar(&options.dryRun, "dry-run", false, "print the generated YAML instead of creating the cluster")
 	cmd.Flags().BoolVar(&options.wait, "wait", false, "wait for the cluster to be provisioned before returning. Returns an error if the cluster is not provisioned by the timeout specified")
 	cmd.Flags().DurationVar(&options.timeout, "timeout", defaultProvisionedTimeout, "the timeout for --wait")
-	cmd.Flags().StringToStringVar(&options.headNodeSelectors, "head-node-selectors", nil, "Node selectors to apply to all head pods in the cluster (e.g. --head-node-selectors cloud.google.com/gke-accelerator=nvidia-l4,cloud.google.com/gke-nodepool=my-node-pool)")
-	cmd.Flags().StringToStringVar(&options.workerNodeSelectors, "worker-node-selectors", nil, "Node selectors to apply to all worker pods in the cluster (e.g. --worker-node-selectors cloud.google.com/gke-accelerator=nvidia-l4,cloud.google.com/gke-nodepool=my-node-pool)")
-	cmd.Flags().StringToStringVar(&options.labels, "labels", nil, "K8s labels (e.g. --labels app=ray,env=dev)")
-	cmd.Flags().StringToStringVar(&options.annotations, "annotations", nil, "K8s annotations (e.g. --annotations ttl-hours=24,owner=chthulu)")
 
 	return cmd
 }
@@ -144,10 +153,6 @@ func (options *CreateClusterOptions) Complete(cmd *cobra.Command, args []string)
 		return fmt.Errorf("failed to get namespace: %w", err)
 	}
 	options.namespace = namespace
-
-	if options.namespace == "" {
-		options.namespace = "default"
-	}
 
 	if len(args) != 1 {
 		return cmdutil.UsageErrorf(cmd, "%s", cmd.Use)
@@ -161,72 +166,145 @@ func (options *CreateClusterOptions) Complete(cmd *cobra.Command, args []string)
 	return nil
 }
 
-func (options *CreateClusterOptions) Validate() error {
-	resourceFields := map[string]string{
-		"head-cpu":                 options.headCPU,
-		"head-gpu":                 options.headGPU,
-		"head-memory":              options.headMemory,
-		"head-ephemeral-storage":   options.headEphemeralStorage,
-		"worker-cpu":               options.workerCPU,
-		"worker-gpu":               options.workerGPU,
-		"worker-tpu":               options.workerTPU,
-		"worker-memory":            options.workerMemory,
-		"worker-ephemeral-storage": options.workerEphemeralStorage,
+func (options *CreateClusterOptions) Validate(cmd *cobra.Command) error {
+	if options.configFile != "" {
+		if err := flagsIncompatibleWithConfigFilePresent(cmd); err != nil {
+			return err
+		}
+
+		// If a cluster config file is provided, check it can be parsed into a RayClusterConfig object
+		rayClusterConfig, err := generation.ParseConfigFile(options.configFile)
+		if err != nil {
+			return fmt.Errorf("failed to parse config file: %w", err)
+		}
+
+		if err := generation.ValidateConfig(rayClusterConfig); err != nil {
+			return fmt.Errorf("failed to validate config file: %w", err)
+		}
+
+		// store the returned RayClusterConfig object for use in Run()
+		options.rayClusterConfig = rayClusterConfig
+	} else {
+		resourceFields := map[string]string{
+			"head-cpu":                 options.headCPU,
+			"head-gpu":                 options.headGPU,
+			"head-memory":              options.headMemory,
+			"head-ephemeral-storage":   options.headEphemeralStorage,
+			"worker-cpu":               options.workerCPU,
+			"worker-gpu":               options.workerGPU,
+			"worker-tpu":               options.workerTPU,
+			"worker-memory":            options.workerMemory,
+			"worker-ephemeral-storage": options.workerEphemeralStorage,
+		}
+
+		for name, value := range resourceFields {
+			if (name == "head-ephemeral-storage" || name == "worker-ephemeral-storage") && value == "" {
+				continue
+			}
+			if err := util.ValidateResourceQuantity(value, name); err != nil {
+				return fmt.Errorf("%w", err)
+			}
+		}
 	}
 
-	for name, value := range resourceFields {
-		if (name == "head-ephemeral-storage" || name == "worker-ephemeral-storage") && value == "" {
-			continue
-		}
-		if err := util.ValidateResourceQuantity(value, name); err != nil {
-			return fmt.Errorf("%w", err)
-		}
+	if err := util.ValidateTPU(&options.workerTPU, &options.numOfHosts, options.workerNodeSelectors); err != nil {
+		return fmt.Errorf("%w", err)
 	}
-	// we must assign gke-tpu-accelerator and gke-tpu-topology in nodeSelector
-	// if worker-tpu is not 0
-	if options.workerTPU != "0" {
-		if err := util.ValidateTPUNodeSelector(options.numOfHosts, options.workerNodeSelectors); err != nil {
-			return fmt.Errorf("%w", err)
-		}
-	}
+
 	return nil
 }
 
+// resolveClusterName resolves the cluster name from the CLI flag and the config file
+func (options *CreateClusterOptions) resolveClusterName() (string, error) {
+	var name string
+
+	if options.rayClusterConfig.Name != nil && *options.rayClusterConfig.Name != "" && options.clusterName != "" && options.clusterName != *options.rayClusterConfig.Name {
+		return "", fmt.Errorf("the cluster name in the config file %q does not match the cluster name %q. You must use the same name to perform this operation", *options.rayClusterConfig.Name, options.clusterName)
+	}
+
+	if options.clusterName != "" {
+		name = options.clusterName
+	} else if options.rayClusterConfig.Name != nil && *options.rayClusterConfig.Name != "" {
+		name = *options.rayClusterConfig.Name
+	} else {
+		return "", fmt.Errorf("the cluster name is required")
+	}
+
+	return name, nil
+}
+
+// resolveNamespace resolves the namespace from the CLI flag and the config file
+func (options *CreateClusterOptions) resolveNamespace() (string, error) {
+	namespace := "default"
+
+	if options.rayClusterConfig.Namespace != nil && *options.rayClusterConfig.Namespace != "" && options.namespace != "" && options.namespace != *options.rayClusterConfig.Namespace {
+		return "", fmt.Errorf("the namespace in the config file %q does not match the namespace %q. You must pass --namespace=%s to perform this operation", *options.rayClusterConfig.Namespace, options.namespace, *options.rayClusterConfig.Namespace)
+	}
+
+	if options.namespace != "" {
+		namespace = options.namespace
+	} else if options.rayClusterConfig.Namespace != nil && *options.rayClusterConfig.Namespace != "" {
+		namespace = *options.rayClusterConfig.Namespace
+	}
+
+	return namespace, nil
+}
+
 func (options *CreateClusterOptions) Run(ctx context.Context, k8sClient client.Client) error {
-	if clusterExists(k8sClient.RayClient(), options.namespace, options.clusterName) {
-		return fmt.Errorf("the Ray cluster %s in namespace %s already exists", options.clusterName, options.namespace)
-	}
+	var rayClusterac *rayv1ac.RayClusterApplyConfiguration
+	var err error
 
-	rayClusterSpecObject := generation.RayClusterSpecObject{
-		Namespace:            &options.namespace,
-		Name:                 &options.clusterName,
-		Labels:               options.labels,
-		Annotations:          options.annotations,
-		RayVersion:           &options.rayVersion,
-		Image:                &options.image,
-		HeadCPU:              &options.headCPU,
-		HeadMemory:           &options.headMemory,
-		HeadEphemeralStorage: &options.headEphemeralStorage,
-		HeadGPU:              &options.headGPU,
-		HeadRayStartParams:   options.headRayStartParams,
-		HeadNodeSelectors:    options.headNodeSelectors,
-		WorkerGroups: []generation.WorkerGroupConfig{
-			{
-				Name:                   ptr.To("default-group"),
-				WorkerReplicas:         &options.workerReplicas,
-				NumOfHosts:             &options.numOfHosts,
-				WorkerCPU:              &options.workerCPU,
-				WorkerMemory:           &options.workerMemory,
-				WorkerEphemeralStorage: &options.workerEphemeralStorage,
-				WorkerGPU:              &options.workerGPU,
-				WorkerTPU:              &options.workerTPU,
-				WorkerRayStartParams:   options.workerRayStartParams,
-				WorkerNodeSelectors:    options.workerNodeSelectors,
+	// If options.rayClusterConfig is set, use it exclusively because it means the user provided a config file
+	if options.rayClusterConfig != nil {
+		name, err := options.resolveClusterName()
+		if err != nil {
+			return err
+		}
+		options.rayClusterConfig.Name = &name
+
+		namespace, err := options.resolveNamespace()
+		if err != nil {
+			return err
+		}
+		options.rayClusterConfig.Namespace = &namespace
+	} else {
+		if options.namespace == "" {
+			options.namespace = "default"
+		}
+
+		options.rayClusterConfig = &generation.RayClusterConfig{
+			Namespace:   &options.namespace,
+			Name:        &options.clusterName,
+			Labels:      options.labels,
+			Annotations: options.annotations,
+			RayVersion:  &options.rayVersion,
+			Image:       &options.image,
+			Head: &generation.Head{
+				CPU:              &options.headCPU,
+				Memory:           &options.headMemory,
+				EphemeralStorage: &options.headEphemeralStorage,
+				GPU:              &options.headGPU,
+				RayStartParams:   options.headRayStartParams,
+				NodeSelectors:    options.headNodeSelectors,
 			},
-		},
+			WorkerGroups: []generation.WorkerGroup{
+				{
+					Name:             ptr.To("default-group"),
+					Replicas:         options.workerReplicas,
+					NumOfHosts:       &options.numOfHosts,
+					CPU:              &options.workerCPU,
+					Memory:           &options.workerMemory,
+					EphemeralStorage: &options.workerEphemeralStorage,
+					GPU:              &options.workerGPU,
+					TPU:              &options.workerTPU,
+					RayStartParams:   options.workerRayStartParams,
+					NodeSelectors:    options.workerNodeSelectors,
+				},
+			},
+		}
 	}
 
-	rayClusterac := rayClusterSpecObject.GenerateRayClusterApplyConfig()
+	rayClusterac = options.rayClusterConfig.GenerateRayClusterApplyConfig()
 
 	// If dry run is enabled, it will call the YAML converter and print out the YAML
 	if options.dryRun {
@@ -238,16 +316,18 @@ func (options *CreateClusterOptions) Run(ctx context.Context, k8sClient client.C
 		return nil
 	}
 
-	// TODO: Decide whether to save YAML to file or not.
+	if clusterExists(k8sClient.RayClient(), *options.rayClusterConfig.Namespace, *options.rayClusterConfig.Name) {
+		return fmt.Errorf("the Ray cluster %s in namespace %s already exists", *options.rayClusterConfig.Name, *options.rayClusterConfig.Namespace)
+	}
 
-	result, err := k8sClient.RayClient().RayV1().RayClusters(options.namespace).Apply(ctx, rayClusterac, metav1.ApplyOptions{FieldManager: util.FieldManager})
+	result, err := k8sClient.RayClient().RayV1().RayClusters(*options.rayClusterConfig.Namespace).Apply(ctx, rayClusterac, metav1.ApplyOptions{FieldManager: util.FieldManager})
 	if err != nil {
 		return fmt.Errorf("failed to create Ray cluster: %w", err)
 	}
 	fmt.Printf("Created Ray cluster: %s\n", result.GetName())
 
 	if options.wait {
-		err = k8sClient.WaitRayClusterProvisioned(ctx, options.namespace, result.GetName(), options.timeout)
+		err = k8sClient.WaitRayClusterProvisioned(ctx, *options.rayClusterConfig.Namespace, result.GetName(), options.timeout)
 		if err != nil {
 			return err
 		}
@@ -263,4 +343,33 @@ func clusterExists(client rayclient.Interface, namespace, name string) bool {
 		return true
 	}
 	return false
+}
+
+// flagsIncompatibleWithConfigFilePresent returns an error if there are command-line flags that are incompatible with a config file
+func flagsIncompatibleWithConfigFilePresent(cmd *cobra.Command) error {
+	incompatibleFlagsUsed := []string{}
+
+	// Define which flags are allowed to be used with --file.
+	// These are typically flags that modify the command's behavior but not the cluster configuration.
+	allowedWithFile := map[string]bool{
+		"file":      true,
+		"context":   true,
+		"namespace": true,
+		"dry-run":   true,
+		"wait":      true,
+		"timeout":   true,
+	}
+
+	// Check all flags to see if any incompatible flags are set
+	cmd.Flags().Visit(func(f *pflag.Flag) {
+		if !allowedWithFile[f.Name] {
+			incompatibleFlagsUsed = append(incompatibleFlagsUsed, f.Name)
+		}
+	})
+
+	if len(incompatibleFlagsUsed) > 0 {
+		return fmt.Errorf("the following flags are incompatible with --file: %v", incompatibleFlagsUsed)
+	}
+
+	return nil
 }
--- kubectl-plugin/pkg/cmd/create/create_workergroup.go
+++ kubectl-plugin/pkg/cmd/create/create_workergroup.go
@@ -137,12 +137,8 @@ func (options *CreateWorkerGroupOptions) Complete(cmd *cobra.Command, args []str
 }
 
 func (options *CreateWorkerGroupOptions) Validate() error {
-	// we must assign gke-tpu-accelerator and gke-tpu-topology in nodeSelector
-	// if worker-tpu is not 0
-	if options.workerTPU != "0" {
-		if err := util.ValidateTPUNodeSelector(options.numOfHosts, options.workerNodeSelectors); err != nil {
-			return fmt.Errorf("%w", err)
-		}
+	if err := util.ValidateTPU(&options.workerTPU, &options.numOfHosts, options.workerNodeSelectors); err != nil {
+		return fmt.Errorf("%w", err)
 	}
 	return nil
 }
