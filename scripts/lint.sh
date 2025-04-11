0addae2d008f15a51f6ae02bb6a55f10a659bdb8
dentiny <dentinyhao@gmail.com>
2025-04-11 01:41:46 +0000

    no need to print version
    Signed-off-by: dentiny <dentinyhao@gmail.com>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,7 +6,6 @@ dirs_to_lint="ray-operator kubectl-plugin apiserver"
 
 for dir in $dirs_to_lint; do
   pushd "$dir"
-  golangci-lint --version
   # exclude the SA1019 check which checks the usage of deprecated fields.
   golangci-lint run --fix --exclude='SA1019' --exclude-files _generated.go --timeout 10m0s --allow-parallel-runners
   popd
