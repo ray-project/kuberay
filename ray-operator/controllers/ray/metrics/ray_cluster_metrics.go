0ed471fe0aeeaa29b66a0a404194d762354f382b
Jun-Hao Wan <ken89@kimo.com>
2025-04-13 03:09:31 +0800

    [Refactor] Encapsulate RayCluster metrics in a custom Prometheus collector (#3310)

--- ray-operator/controllers/ray/metrics/ray_cluster_metrics.go
+++ ray-operator/controllers/ray/metrics/ray_cluster_metrics.go
@@ -1,3 +1,53 @@
 package metrics
 
-func registerRayClusterMetrics() {}
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type RayClusterMetricCollector struct {
+	// Metrics
+	rayClusterProvisionedDuration  *prometheus.GaugeVec
+	rayClusterHeadPodReadyDuration *prometheus.GaugeVec
+}
+
+func NewRayClusterMetricCollector() *RayClusterMetricCollector {
+	collector := &RayClusterMetricCollector{
+		rayClusterProvisionedDuration: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "kuberay_cluster_provisioned_duration_seconds",
+				Help: "The time, in seconds, when a RayCluster's `RayClusterProvisioned` status transitions from false (or unset) to true",
+			},
+			[]string{"name", "namespace"},
+		),
+		rayClusterHeadPodReadyDuration: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "kuberay_cluster_head_pod_ready_duration_seconds",
+				Help: "The time, in seconds, from RayClusters created to head pod ready",
+			},
+			[]string{"name", "namespace"},
+		),
+	}
+	return collector
+}
+
+// Describe implements prometheus.Collector interface Describe method.
+func (c *RayClusterMetricCollector) Describe(ch chan<- *prometheus.Desc) {
+	c.rayClusterProvisionedDuration.Describe(ch)
+	c.rayClusterHeadPodReadyDuration.Describe(ch)
+}
+
+// Collect implements prometheus.Collector interface Collect method.
+func (c *RayClusterMetricCollector) Collect(ch chan<- prometheus.Metric) {
+	c.rayClusterProvisionedDuration.Collect(ch)
+	c.rayClusterHeadPodReadyDuration.Collect(ch)
+}
+
+// ObserveRayClusterProvisionedDuration observes the duration of RayCluster from creation to provisioned
+func (c *RayClusterMetricCollector) ObserveRayClusterProvisionedDuration(name, namespace string, duration float64) {
+	c.rayClusterProvisionedDuration.WithLabelValues(name, namespace).Set(duration)
+}
+
+// ObserveRayClusterHeadPodReadyDuration observes the duration of RayCluster from creation to head pod ready
+func (c *RayClusterMetricCollector) ObserveRayClusterHeadPodReadyDuration(name, namespace string, duration float64) {
+	c.rayClusterHeadPodReadyDuration.WithLabelValues(name, namespace).Set(duration)
+}
