4a295399a4849fcc88aec9b4d5fd4c8c175a21bf
dentiny <dentinyhao@gmail.com>
2025-04-12 10:44:45 +0000

    undo apiserver lint

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # TODO(hjiang): Enable linter for apiserver after all issues addressed.
-dirs_to_lint="ray-operator kubectl-plugin apiserver"
+dirs_to_lint="ray-operator kubectl-plugin"
 
 for dir in $dirs_to_lint; do
   pushd "$dir"
