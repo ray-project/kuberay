fbada1cf4c282683b4c963eae44e408d181d6f29
dentiny <dentinyhao@gmail.com>
2025-04-14 19:14:28 +0000

    reovery back comment

--- apiserver/pkg/http/client.go
+++ apiserver/pkg/http/client.go
@@ -17,9 +17,14 @@ import (
 )
 
 type KuberayAPIServerClient struct {
-	httpClient         *http.Client
-	marshaler          *protojson.MarshalOptions
-	unmarshaler        *protojson.UnmarshalOptions
+	httpClient  *http.Client
+	marshaler   *protojson.MarshalOptions
+	unmarshaler *protojson.UnmarshalOptions
+	// TODO(hjiang): here we use function to allow customized http request handling logic in unit test, worth revisiting if there're better ways;
+	// for example, (1) wrap an interface to process request; (2) inject round-trip logic into http client.
+	// See https://github.com/ray-project/kuberay/pull/3334/files#r2041183495 for details.
+	//
+	// Store http request handling function for unit test purpose.
 	executeHttpRequest func(httpRequest *http.Request, URL string) ([]byte, *rpcStatus.Status, error)
 	baseURL            string
 }
