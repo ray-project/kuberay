3d924aa6d9da9407ac21f5be6eba07119b63f23b
dentiny <hjiang@anyscale.com>
2025-04-14 21:20:15 -0700

    [apiserver] Enable golang linter for apiserver (#3367)

--- apiserver/test/e2e/service_server_e2e_test.go
+++ apiserver/test/e2e/service_server_e2e_test.go
@@ -8,6 +8,7 @@ import (
 
 	kuberayHTTP "github.com/ray-project/kuberay/apiserver/pkg/http"
 	api "github.com/ray-project/kuberay/proto/go_client"
+
 	rayv1api "github.com/ray-project/kuberay/ray-operator/apis/ray/v1"
 
 	"github.com/stretchr/testify/assert"
