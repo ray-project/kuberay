f19e1f5452d8c3808e95a09b85b2daecc9c47502
owenowenisme <mses010108@gmail.com>
2025-04-20 19:53:16 +0800

    changed to gomega
    Signed-off-by: owenowenisme <mses010108@gmail.com>

--- apiserver/test/e2e/job_server_e2e_test.go
+++ apiserver/test/e2e/job_server_e2e_test.go
@@ -1,21 +1,16 @@
 package e2e
 
 import (
-	"context"
 	"fmt"
 	"net/http"
-	"slices"
 	"testing"
-	"time"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"k8s.io/apimachinery/pkg/util/wait"
 
 	kuberayHTTP "github.com/ray-project/kuberay/apiserver/pkg/http"
 	api "github.com/ray-project/kuberay/proto/go_client"
 
 	rayv1api "github.com/ray-project/kuberay/ray-operator/apis/ray/v1"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestCreateJobWithDisposableClusters(t *testing.T) {
@@ -94,7 +89,6 @@ func TestCreateJobWithDisposableClusters(t *testing.T) {
 					ShutdownAfterJobFinishes: true,
 					ClusterSpec:              clusterSpec,
 					TtlSecondsAfterFinished:  60,
-					ActiveDeadlineSeconds:    120,
 				},
 				Namespace: tCtx.GetNamespaceName(),
 			},
@@ -113,7 +107,6 @@ func TestCreateJobWithDisposableClusters(t *testing.T) {
 					ShutdownAfterJobFinishes: true,
 					ClusterSpec:              clusterSpec,
 					TtlSecondsAfterFinished:  60,
-					ActiveDeadlineSeconds:    120,
 				},
 				Namespace: tCtx.GetNamespaceName(),
 			},
@@ -730,34 +723,4 @@ func createTestJob(t *testing.T, tCtx *End2EndTestingContext, expectedJobStatues
 	require.NotNil(t, actualJob, "A job is expected")
 	waitForRayJob(t, tCtx, testJobRequest.Job.Name, expectedJobStatues)
 	return testJobRequest
-}
-
-func waitForRayJob(t *testing.T, tCtx *End2EndTestingContext, rayJobName string, expectedJobStatuses []rayv1api.JobStatus) {
-	// `expectedJobStatuses` is a slice of job statuses that we expect the job to be in
-	// wait for the job to be in any of the `expectedJobStatuses` state for 3 minutes
-	// if is not in that state, return an error
-	err := wait.PollUntilContextTimeout(tCtx.ctx, 500*time.Millisecond, 3*time.Minute, false, func(_ context.Context) (done bool, err error) {
-		rayJob, err := tCtx.GetRayJobByName(rayJobName)
-		if err != nil {
-			return true, err
-		}
-		t.Logf("Found ray job with state '%s' for ray job '%s'", rayJob.Status.JobStatus, rayJobName)
-		return slices.Contains(expectedJobStatuses, rayJob.Status.JobStatus), nil
-	})
-	require.NoErrorf(t, err, "No error expected when getting status for ray job: '%s', err %v", rayJobName, err)
-}
-
-func waitForDeletedRayJob(t *testing.T, tCtx *End2EndTestingContext, jobName string) {
-	// wait for the job to be deleted
-	// if is not in that state, return an error
-	err := wait.PollUntilContextTimeout(tCtx.ctx, 500*time.Millisecond, 3*time.Minute, false, func(_ context.Context) (done bool, err error) {
-		rayJob, err := tCtx.GetRayJobByName(jobName)
-		if err != nil &&
-			assert.EqualError(t, err, "rayjobs.ray.io \""+jobName+"\" not found") {
-			return true, nil
-		}
-		t.Logf("Found status of '%s' for ray cluster '%s'", rayJob.Status.JobStatus, jobName)
-		return false, err
-	})
-	require.NoErrorf(t, err, "No error expected when deleting ray job: '%s', err %v", jobName, err)
 }
