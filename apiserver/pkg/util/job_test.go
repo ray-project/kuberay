c20cfa9e36134c440dd2fe1104b456b7878961cc
Nary Yeh <60069744+machichima@users.noreply.github.com>
2025-04-22 02:21:42 +0800

    [Feature] [API Server] Support activeDeadlineSeconds in API Server RayJob resource (#3335)

--- apiserver/pkg/util/job_test.go
+++ apiserver/pkg/util/job_test.go
@@ -19,6 +19,7 @@ var apiJobNewCluster = &api.RayJob{
 	RuntimeEnv:               "pip:\n  - requests==2.26.0\n  - pendulum==2.1.2\nenv_vars:\n  counter_name: \"test_counter\"\n",
 	ShutdownAfterJobFinishes: true,
 	ClusterSpec:              rayCluster.ClusterSpec,
+	ActiveDeadlineSeconds:    120,
 }
 
 var apiJobExistingCluster = &api.RayJob{
@@ -87,6 +88,7 @@ func TestBuildRayJob(t *testing.T) {
 	assert.Len(t, job.Spec.Metadata, 1)
 	assert.Nil(t, job.Spec.ClusterSelector)
 	assert.NotNil(t, job.Spec.RayClusterSpec)
+	assert.Equal(t, 120, int(*job.Spec.ActiveDeadlineSeconds))
 
 	// Test request without cluster creation
 	job, err = NewRayJob(apiJobExistingCluster, map[string]*api.ComputeTemplate{"foo": &template})
