0b72901d93c4db87950175fbbc195a8e1cf38fe7
Troy Chiu <114708546+troychiu@users.noreply.github.com>
2025-04-23 23:31:01 -0700

    [Refactor] Encapsulate RayJob metrics in a custom Prometheus collector (#3444)
    Signed-off-by: Troy Chiu <y.troychiu@gmail.com>

--- ray-operator/controllers/ray/metrics/ray_job_metrics.go
+++ ray-operator/controllers/ray/metrics/ray_job_metrics.go
@@ -1,3 +1,24 @@
 package metrics
 
-func registerRayJobMetrics() {}
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+// RayJobMetricsCollector implements the prometheus.Collector interface to collect ray job metrics.
+type RayJobMetricsCollector struct {
+	// Metrics
+}
+
+// NewRayJobMetricsCollector creates a new RayJobMetricsCollector instance.
+func NewRayJobMetricsCollector() *RayJobMetricsCollector {
+	collector := &RayJobMetricsCollector{}
+	return collector
+}
+
+// Describe implements prometheus.Collector interface Describe method.
+func (c *RayJobMetricsCollector) Describe(_ chan<- *prometheus.Desc) {
+}
+
+// Collect implements prometheus.Collector interface Collect method.
+func (c *RayJobMetricsCollector) Collect(_ chan<- prometheus.Metric) {
+}
