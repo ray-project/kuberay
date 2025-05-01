02316d29935aa0e3d7db9c35b2da4bac7668b8e8
Rueian <rueiancsie@gmail.com>
2025-04-30 18:49:58 -0700

    [apiserversdk] update comments and tests
    Signed-off-by: Rueian <rueiancsie@gmail.com>

--- apiserversdk/proxy.go
+++ apiserversdk/proxy.go
@@ -14,12 +14,12 @@ type MuxConfig struct {
 }
 
 func NewMux(config MuxConfig) (*http.ServeMux, error) {
-	u, err := url.Parse(config.KubernetesConfig.Host)
+	u, err := url.Parse(config.KubernetesConfig.Host) // parse the K8s API server URL from the KubernetesConfig.
 	if err != nil {
 		return nil, err
 	}
 	proxy := httputil.NewSingleHostReverseProxy(u)
-	if proxy.Transport, err = rest.TransportFor(config.KubernetesConfig); err != nil {
+	if proxy.Transport, err = rest.TransportFor(config.KubernetesConfig); err != nil { // rest.TransportFor provides the auth to the K8s API server.
 		return nil, err
 	}
 	if config.Middleware == nil {
@@ -27,10 +27,10 @@ func NewMux(config MuxConfig) (*http.ServeMux, error) {
 	}
 	mux := http.NewServeMux()
 	// TODO: add template features to specify routes.
-	mux.Handle("/apis/ray.io/v1/", config.Middleware(proxy))
+	mux.Handle("/apis/ray.io/v1/", config.Middleware(proxy)) // forward KubeRay CR requests.
 	// TODO: add query filters to make sure only KubeRay events are queried.
-	mux.Handle("/api/v1/namespaces/{namespace}/events", config.Middleware(proxy))
+	mux.Handle("/api/v1/namespaces/{namespace}/events", config.Middleware(proxy)) // allow querying KubeRay CR events.
 	// TODO: check whether the service belongs to KubeRay first.
-	mux.Handle("/api/v1/namespaces/{namespace}/services/{service}/proxy", config.Middleware(proxy))
+	mux.Handle("/api/v1/namespaces/{namespace}/services/{service}/proxy", config.Middleware(proxy)) // allow accessing KubeRay dashboards and job submissions.
 	return mux, nil
 }
--- apiserversdk/proxy_test.go
+++ apiserversdk/proxy_test.go
@@ -5,18 +5,20 @@ import (
 	"net"
 	"net/http"
 	"path/filepath"
+	"sync/atomic"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
-	rayv1 "github.com/ray-project/kuberay/ray-operator/apis/ray/v1"
-	rayclient "github.com/ray-project/kuberay/ray-operator/pkg/client/clientset/versioned/typed/ray/v1"
 	corev1 "k8s.io/api/core/v1"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8sclient "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
+
+	rayv1 "github.com/ray-project/kuberay/ray-operator/apis/ray/v1"
+	rayclient "github.com/ray-project/kuberay/ray-operator/pkg/client/clientset/versioned/typed/ray/v1"
 )
 
 var (
@@ -25,6 +27,7 @@ var (
 	rayClient *rayclient.RayV1Client
 	k8sClient *k8sclient.CoreV1Client
 	testEnv   *envtest.Environment
+	lastReq   atomic.Pointer[http.Request]
 )
 
 func TestProxy(t *testing.T) {
@@ -32,7 +35,7 @@ func TestProxy(t *testing.T) {
 	RunSpecs(t, "Proxy Suite")
 }
 
-var _ = BeforeSuite(func(ctx SpecContext) {
+var _ = BeforeSuite(func(_ SpecContext) {
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
 		CRDDirectoryPaths:     []string{filepath.Join("..", "ray-operator", "config", "crd", "bases")},
@@ -44,14 +47,26 @@ var _ = BeforeSuite(func(ctx SpecContext) {
 	Expect(err).ToNot(HaveOccurred())
 	Expect(cfg).ToNot(BeNil())
 
-	mux, err := NewMux(MuxConfig{KubernetesConfig: cfg})
+	mux, err := NewMux(MuxConfig{
+		KubernetesConfig: cfg,
+		Middleware: func(handler http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				lastReq.Store(r.Clone(r.Context()))
+				handler.ServeHTTP(w, r)
+			})
+		},
+	})
 	Expect(err).ToNot(HaveOccurred())
 	Expect(mux).ToNot(BeNil())
 
 	ln, err = net.Listen("tcp", "127.0.0.1:0")
 	Expect(err).ToNot(HaveOccurred())
 	Expect(ln).ToNot(BeNil())
-	go http.Serve(ln, mux)
+	go func() {
+		svc := &http.Server{Handler: mux, ReadHeaderTimeout: time.Minute}
+		err := svc.Serve(ln)
+		Expect(err).To(MatchError(net.ErrClosed))
+	}()
 
 	proxyCfg := &rest.Config{Host: "http://" + ln.Addr().String()}
 	rayClient = rayclient.NewForConfigOrDie(proxyCfg)
@@ -67,7 +82,7 @@ var _ = AfterSuite(func() {
 var _ = Describe("RayCluster", Ordered, func() {
 	It("Create RayCluster", func() {
 		_, err := rayClient.RayClusters("default").Create(context.Background(), &rayv1.RayCluster{
-			ObjectMeta: v1.ObjectMeta{Name: "proxy-test"},
+			ObjectMeta: metav1.ObjectMeta{Name: "proxy-test"},
 			Spec: rayv1.RayClusterSpec{
 				HeadGroupSpec: rayv1.HeadGroupSpec{
 					RayStartParams: make(map[string]string),
@@ -83,23 +98,31 @@ var _ = Describe("RayCluster", Ordered, func() {
 					},
 				},
 			},
-		}, v1.CreateOptions{})
+		}, metav1.CreateOptions{})
 		Expect(err).ToNot(HaveOccurred())
+		Expect(lastReq.Load().Method).To(Equal(http.MethodPost))
+		Expect(lastReq.Load().RequestURI).To(Equal("/apis/ray.io/v1/namespaces/default/rayclusters"))
 	})
 	It("Get RayCluster", func() {
-		cluster, err := rayClient.RayClusters("default").Get(context.Background(), "proxy-test", v1.GetOptions{})
+		cluster, err := rayClient.RayClusters("default").Get(context.Background(), "proxy-test", metav1.GetOptions{})
 		Expect(err).ToNot(HaveOccurred())
 		Expect(cluster.Name).To(Equal("proxy-test"))
+		Expect(lastReq.Load().Method).To(Equal(http.MethodGet))
+		Expect(lastReq.Load().RequestURI).To(Equal("/apis/ray.io/v1/namespaces/default/rayclusters/proxy-test"))
 	})
 	It("List RayCluster", func() {
-		clusters, err := rayClient.RayClusters("default").List(context.Background(), v1.ListOptions{})
+		clusters, err := rayClient.RayClusters("default").List(context.Background(), metav1.ListOptions{})
 		Expect(err).ToNot(HaveOccurred())
 		Expect(clusters.Items).To(HaveLen(1))
 		Expect(clusters.Items[0].Name).To(Equal("proxy-test"))
+		Expect(lastReq.Load().Method).To(Equal(http.MethodGet))
+		Expect(lastReq.Load().RequestURI).To(Equal("/apis/ray.io/v1/namespaces/default/rayclusters"))
 	})
 	It("Delete RayCluster", func() {
-		err := rayClient.RayClusters("default").Delete(context.Background(), "proxy-test", v1.DeleteOptions{})
+		err := rayClient.RayClusters("default").Delete(context.Background(), "proxy-test", metav1.DeleteOptions{})
 		Expect(err).ToNot(HaveOccurred())
+		Expect(lastReq.Load().Method).To(Equal(http.MethodDelete))
+		Expect(lastReq.Load().RequestURI).To(Equal("/apis/ray.io/v1/namespaces/default/rayclusters/proxy-test"))
 	})
 })
 
@@ -108,8 +131,17 @@ var _ = Describe("RayCluster", Ordered, func() {
 
 var _ = Describe("events", Ordered, func() {
 	It("List events", func() {
-		events, err := k8sClient.Events("default").List(context.Background(), v1.ListOptions{})
+		events, err := k8sClient.Events("default").List(context.Background(), metav1.ListOptions{})
 		Expect(err).ToNot(HaveOccurred())
-		Expect(events.Items).To(HaveLen(0))
+		Expect(events.Items).To(BeEmpty())
+		Expect(lastReq.Load().Method).To(Equal(http.MethodGet))
+		Expect(lastReq.Load().RequestURI).To(Equal("/api/v1/namespaces/default/events"))
+	})
+})
+
+var _ = Describe("not match", Ordered, func() {
+	It("List Pods", func() {
+		_, err := k8sClient.Pods("default").List(context.Background(), metav1.ListOptions{})
+		Expect(err).To(MatchError(ContainSubstring("the server could not find the requested resource")))
 	})
 })
