7db8f692ec5c6288db23b14b2502777649c72923
Leo Liao <93932709+LeoLiao123@users.noreply.github.com>
2025-05-13 07:06:01 +0800

    [Feature] Add unit test for update service request validation (#3546)

--- apiserver/pkg/server/serve_server.go
+++ apiserver/pkg/server/serve_server.go
@@ -164,7 +164,7 @@ func ValidateCreateServiceRequest(request *api.CreateRayServiceRequest) error {
 	}
 
 	if request.Service.User == "" {
-		return util.NewInvalidInputError("User who create the Service is empty. Please specify a valid value.")
+		return util.NewInvalidInputError("User who created the Service is empty. Please specify a valid value.")
 	}
 
 	return ValidateClusterSpec(request.Service.ClusterSpec)
@@ -172,7 +172,7 @@ func ValidateCreateServiceRequest(request *api.CreateRayServiceRequest) error {
 
 func ValidateUpdateServiceRequest(request *api.UpdateRayServiceRequest) error {
 	if request.Name == "" {
-		return util.NewInvalidInputError("Service name is empty. Please specify a valid value.")
+		return util.NewInvalidInputError("Name is empty. Please specify a valid value.")
 	}
 	if request.Namespace == "" {
 		return util.NewInvalidInputError("Namespace is empty. Please specify a valid value.")
@@ -191,7 +191,7 @@ func ValidateUpdateServiceRequest(request *api.UpdateRayServiceRequest) error {
 	}
 
 	if request.Service.User == "" {
-		return util.NewInvalidInputError("User who create the Service is empty. Please specify a valid value.")
+		return util.NewInvalidInputError("User who updated the Service is empty. Please specify a valid value.")
 	}
 
 	return ValidateClusterSpec(request.Service.ClusterSpec)
