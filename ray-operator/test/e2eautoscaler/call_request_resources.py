b4cd46fc18bc6076df6071ed63ccf7bf7dc96282
Dongjun Na <kmu5544616@gmail.com>
2025-05-22 09:43:56 +0900

    Apply review feedback
    Signed-off-by: Dongjun Na <kmu5544616@gmail.com>

--- ray-operator/test/e2eautoscaler/call_request_resources.py
+++ ray-operator/test/e2eautoscaler/call_request_resources.py
@@ -1,5 +1,8 @@
 import ray
+import time
 from ray.autoscaler.sdk import request_resources
 
 ray.init(namespace="default_namespace")
 request_resources(num_cpus=3)
+while len(ray.nodes()) != 4: # 1 head + 3 workers
+    time.sleep(1)
