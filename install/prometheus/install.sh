9f013a3b9c7caafe4b32f6e01a009c24adf39ed8
Owen Lin (You-Cheng Lin) <106612301+owenowenisme@users.noreply.github.com>
2025-05-23 01:12:07 +0800

    [Grafana] Allow auto-load dashboard jsons (#3643)
    Signed-off-by: You-Cheng Lin <mses010108@gmail.com>
    Signed-off-by: Owen Lin (You-Cheng Lin) <106612301+owenowenisme@users.noreply.github.com>
    Co-authored-by: Jun-Hao Wan <ken89@kimo.com>

--- install/prometheus/install.sh
+++ install/prometheus/install.sh
@@ -11,7 +11,13 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 
 # Install the kube-prometheus-stack v48.2.1 helm chart with `overrides.yaml` file.
 # https://github.com/prometheus-community/helm-charts/tree/kube-prometheus-stack-48.2.1/charts/kube-prometheus-stack
-helm --namespace prometheus-system install prometheus prometheus-community/kube-prometheus-stack --create-namespace --version 48.2.1 -f "${DIR}"/overrides.yaml
+kubectl create namespace prometheus-system
+kubectl create configmap grafana-dashboards --from-file="$DIR/../../config/grafana/" --dry-run=client -o yaml | sed -e '/^metadata:/ a\
+  namespace: prometheus-system\
+  labels:\
+    grafana_dashboard: "1"' | kubectl create -f -
+
+helm --namespace prometheus-system install prometheus prometheus-community/kube-prometheus-stack --version 48.2.1 -f "${DIR}"/overrides.yaml
 
 # set the place of monitor files
 monitor_dir="${DIR}"/../../config/prometheus
