324e42e5b5ec682170b9c772785bd555887ed787
Troy Chiu <y.troychiu@gmail.com>
2025-05-24 15:50:26 -0700

    test
    Signed-off-by: Troy Chiu <y.troychiu@gmail.com>

--- apiserver/test/e2e/cluster_server_e2e_test.go
+++ apiserver/test/e2e/cluster_server_e2e_test.go
@@ -456,6 +456,7 @@ func TestCreateClusterEndpoint(t *testing.T) {
 				require.Nil(t, actualRPCStatus, "No RPC status expected")
 				require.NotNil(t, actualCluster, "A cluster is expected")
 				require.True(t, clusterSpecEqual(tc.Input.Cluster.ClusterSpec, actualCluster.ClusterSpec), "Cluster spec is not as expected")
+				waitForRunningCluster(t, tCtx, actualCluster.Name)
 				tCtx.DeleteRayCluster(t, actualCluster.Name)
 			} else {
 				require.EqualError(t, err, tc.ExpectedError.Error(), "Matching error expected")
