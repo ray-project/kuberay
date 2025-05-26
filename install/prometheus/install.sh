bd54eda12ae808b084598df4983d6a2292adce5c
You-Cheng Lin <mses010108@gmail.com>
2025-05-26 04:19:44 +0000

    update
    Signed-off-by: You-Cheng Lin <mses010108@gmail.com>

--- install/prometheus/install.sh
+++ install/prometheus/install.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+# Parse command line arguments
+AUTO_LOAD_DASHBOARD=false
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --auto-load-dashboard)
+      AUTO_LOAD_DASHBOARD="$2"
+      shift 2
+      ;;
+    *)
+      echo "Unknown option $1"
+      echo "Usage: $0 [--auto-load-dashboard true|false]"
+      exit 1
+      ;;
+  esac
+done
+
 set -x
-set errexit
+set -e
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
@@ -12,10 +29,14 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 # Install the kube-prometheus-stack v48.2.1 helm chart with `overrides.yaml` file.
 # https://github.com/prometheus-community/helm-charts/tree/kube-prometheus-stack-48.2.1/charts/kube-prometheus-stack
 kubectl create namespace prometheus-system
-kubectl create configmap grafana-dashboards --from-file="$DIR/../../config/grafana/" --dry-run=client -o yaml | sed -e '/^metadata:/ a\
+
+# Conditionally create grafana dashboards configmap based on the --auto-load-dashboard flag
+if [[ "$AUTO_LOAD_DASHBOARD" == "true" ]]; then
+  kubectl create configmap grafana-dashboards --from-file="$DIR/../../config/grafana/" --dry-run=client -o yaml | sed -e '/^metadata:/ a\
   namespace: prometheus-system\
   labels:\
     grafana_dashboard: "1"' | kubectl create -f -
+fi
 
 helm --namespace prometheus-system install prometheus prometheus-community/kube-prometheus-stack --version 48.2.1 -f "${DIR}"/overrides.yaml
 
