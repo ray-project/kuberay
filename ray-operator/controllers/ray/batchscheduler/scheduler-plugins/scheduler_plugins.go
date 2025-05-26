58d32d827a507fb68db585e378fc464817f0f9d3
kunwuluan <kunwuluan@gmail.com>
2025-05-26 17:32:21 +0800

    add unit test
    update ValidateBatchSchedulerConfig()
    
    update helm chart
    
    Rename the function.
    
    Signed-off-by: kunwuluan <kunwuluan@gmail.com>

--- ray-operator/controllers/ray/batchscheduler/scheduler-plugins/scheduler_plugins.go
+++ ray-operator/controllers/ray/batchscheduler/scheduler-plugins/scheduler_plugins.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientscheme "k8s.io/client-go/kubernetes/scheme"
@@ -34,11 +35,14 @@ func GetPluginName() string {
 	return SchedulerName
 }
 
-func (y *KubeScheduler) Name() string {
+func (k *KubeScheduler) Name() string {
 	return GetPluginName()
 }
 
-func (y *KubeScheduler) DoBatchSchedulingOnSubmission(ctx context.Context, rc *rayv1.RayCluster) error {
+func (k *KubeScheduler) DoBatchSchedulingOnSubmission(ctx context.Context, rc *rayv1.RayCluster) error {
+	if !k.isGangSchedulingEnabled(rc) {
+		return nil
+	}
 	replica := int32(1)
 	for _, workerGroup := range rc.Spec.WorkerGroupSpecs {
 		if workerGroup.Replicas == nil {
@@ -48,32 +52,47 @@ func (y *KubeScheduler) DoBatchSchedulingOnSubmission(ctx context.Context, rc *r
 	}
 
 	podGroup := &v1alpha1.PodGroup{
-		ObjectMeta: metav1.ObjectMeta{},
+		ObjectMeta: metav1.ObjectMeta{
+			Namespace: rc.Namespace,
+			Name:      rc.Name,
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					Name:       rc.Name,
+					UID:        rc.UID,
+					APIVersion: rc.APIVersion,
+					Kind:       rc.Kind,
+				},
+			},
+		},
 		Spec: v1alpha1.PodGroupSpec{
 			MinMember:    replica,
 			MinResources: utils.CalculateMinResources(rc),
 		},
 	}
 
-	return y.cli.Create(ctx, podGroup)
+	err := k.cli.Create(ctx, podGroup)
+	if errors.IsAlreadyExists(err) {
+		return nil
+	}
+	return err
 }
 
 // AddMetadataToPod adds essential labels and annotations to the Ray pods
-// the yunikorn scheduler needs these labels and annotations in order to do the scheduling properly
-func (y *KubeScheduler) AddMetadataToPod(_ context.Context, app *rayv1.RayCluster, groupName string, pod *corev1.Pod) {
+// the scheduler needs these labels and annotations in order to do the scheduling properly
+func (k *KubeScheduler) AddMetadataToPod(_ context.Context, app *rayv1.RayCluster, groupName string, pod *corev1.Pod) {
 	// when gang scheduling is enabled, extra annotations need to be added to all pods
-	if y.isGangSchedulingEnabled(app) {
+	if k.isGangSchedulingEnabled(app) {
 		// the group name for the head and each of the worker group should be different
-		pod.Annotations[KubeSchedulerPodGroupLabelKey] = groupName
+		pod.Labels[KubeSchedulerPodGroupLabelKey] = app.Name
 	}
 }
 
-func (y *KubeScheduler) isGangSchedulingEnabled(app *rayv1.RayCluster) bool {
+func (k *KubeScheduler) isGangSchedulingEnabled(app *rayv1.RayCluster) bool {
 	_, exist := app.Labels[utils.RayClusterGangSchedulingEnabled]
 	return exist
 }
 
-func (yf *KubeSchedulerFactory) New(ctx context.Context, c *rest.Config) (schedulerinterface.BatchScheduler, error) {
+func (kf *KubeSchedulerFactory) New(ctx context.Context, c *rest.Config) (schedulerinterface.BatchScheduler, error) {
 	scheme := runtime.NewScheme()
 	_ = clientscheme.AddToScheme(scheme)
 	_ = v1alpha1.AddToScheme(scheme)
@@ -106,11 +125,11 @@ func (yf *KubeSchedulerFactory) New(ctx context.Context, c *rest.Config) (schedu
 	}, nil
 }
 
-func (yf *KubeSchedulerFactory) AddToScheme(sche *runtime.Scheme) {
+func (kf *KubeSchedulerFactory) AddToScheme(sche *runtime.Scheme) {
 	// No extra scheme needs to be registered
 	_ = v1alpha1.AddToScheme(sche)
 }
 
-func (yf *KubeSchedulerFactory) ConfigureReconciler(b *builder.Builder) *builder.Builder {
+func (kf *KubeSchedulerFactory) ConfigureReconciler(b *builder.Builder) *builder.Builder {
 	return b
 }
