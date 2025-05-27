89cdda88f51ca62773cd708806d8cbc0c5fd5032
Rueian <rueiancsie@gmail.com>
2025-05-27 14:05:07 -0700

    [Test][Autoscaler] deflaky unexpected dead actors in tests by setting max_restarts=-1
    Signed-off-by: Rueian <rueiancsie@gmail.com>

--- ray-operator/test/e2eautoscaler/create_detached_actor.py
+++ ray-operator/test/e2eautoscaler/create_detached_actor.py
@@ -9,7 +9,8 @@ parser.add_argument('--num-gpus', type=float, default=0)
 parser.add_argument('--num-custom-resources', type=float, default=0)
 args = parser.parse_args()
 
-@ray.remote(num_cpus=args.num_cpus, num_gpus=args.num_gpus, resources={"CustomResource": args.num_custom_resources})
+# set max_restarts=-1 to restart unexpected death in tests.
+@ray.remote(max_restarts=-1, num_cpus=args.num_cpus, num_gpus=args.num_gpus, resources={"CustomResource": args.num_custom_resources})
 class Actor:
     pass
 
