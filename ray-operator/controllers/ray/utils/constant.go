da227773c3bb569f77619c6042af99f32fc8afc8
machichima <nary12321@gmail.com>
2025-07-16 20:04:32 +0800

    feat: add feature flag for deterministic head pod name
    Signed-off-by: machichima <nary12321@gmail.com>

--- ray-operator/controllers/ray/utils/constant.go
+++ ray-operator/controllers/ray/utils/constant.go
@@ -164,6 +164,9 @@ const (
 	// by default starting with v1.4.0.
 	ENABLE_LOGIN_SHELL = "ENABLE_LOGIN_SHELL"
 
+	// If set to true, we will use deterministic name for head pod. Otherwise, the non-deterministic name id used.
+	ENABLE_DETERMINISTIC_HEAD_POD_NAME = "ENABLE_DETERMINISTIC_HEAD_NAME"
+
 	// Ray core default configurations
 	DefaultWorkerRayGcsReconnectTimeoutS = "600"
 
--- ray-operator/controllers/ray/utils/util.go
+++ ray-operator/controllers/ray/utils/util.go
@@ -712,3 +712,12 @@ func GetContainerCommand(additionalOptions []string) []string {
 	bashOptionsStr := strings.Join(bashOptions, "")
 	return []string{"/bin/bash", "-" + bashOptionsStr, "--"}
 }
+
+// GetEnableDeterministicHeadName returns true if deterministic head pod name is enabled.
+func IsDeterministicHeadPodNameEnabled() bool {
+	enableDeterministicHeadPodName := false
+	if s := os.Getenv(ENABLE_DETERMINISTIC_HEAD_POD_NAME); strings.ToLower(s) == "true" {
+		enableDeterministicHeadPodName = true
+	}
+	return enableDeterministicHeadPodName
+}
