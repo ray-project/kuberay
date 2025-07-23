e115be115b50eb8c397267901f5d52048a205310
Alejandro Alvarez <aalvarez@runwayml.com>
2025-07-23 16:43:08 +0100

    allow changing the dashboard api url using an env var

--- dashboard/src/utils/constants.ts
+++ dashboard/src/utils/constants.ts
@@ -1,11 +1,11 @@
 export const ALL_NAMESPACES = "all"
 // I'm developing in the stage cluster, so I'm directly using the deployed backend
 const development = {
-  url: "http://localhost:31888/apis/v1",
+  url: process.env.NEXT_PUBLIC_API_URL || "http://localhost:31888/apis/v1",
 };
 
 const production = {
-  url: "http://localhost:31888/apis/v1",
+  url: process.env.NEXT_PUBLIC_API_URL || "http://localhost:31888/apis/v1",
 };
 
 export const config =
