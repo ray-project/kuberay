226113fde728d9b3823dc5cd7698d3ca63405182
DW-Han <kenny.x.han@gmail.com>
2025-07-24 08:25:42 +0000

    cleaning test

--- ray-operator/controllers/ray/rayjob_controller_scheduled_test.go
+++ ray-operator/controllers/ray/rayjob_controller_scheduled_test.go
@@ -199,36 +199,5 @@ var _ = Context("RayJob with schedule operation", func() {
 				},
 				time.Second*15, time.Millisecond*500).Should(BeTrue(), "Expected RayCluster to still exist")
 		})
-
-		It("should have a JobDeploymentStatus reflecting its scheduled, new, or initializing state", func() {
-			getStatusFunc := getRayJobDeploymentStatus(ctx, rayJob)
-
-			currentStatus, err := getStatusFunc()
-			Expect(err).NotTo(HaveOccurred(), "Failed to get RayJob status initially")
-
-			if currentStatus == rayv1.JobDeploymentStatusInitializing || currentStatus == rayv1.JobDeploymentStatusNew {
-				Expect(currentStatus).To(Equal(rayv1.JobDeploymentStatusInitializing), "RayJob was already Initializing")
-				return
-			}
-			// If it's not Initializing, then it should be sheduled
-			Eventually(
-				getStatusFunc,
-				time.Second*5,
-				time.Millisecond*500,
-			).Should(
-				Or(
-					Equal(rayv1.JobDeploymentStatusScheduled),
-				),
-				"JobDeploymentStatus should be Scheduled, New or Initializing within 5 seconds",
-			)
-		})
-
-		// The cron job runs every minute so it will take at most 1 minute to run
-		It("should transition to the Initializing", func() {
-			Eventually(
-				getRayJobDeploymentStatus(ctx, rayJob),
-				time.Second*60, time.Microsecond*500).Should(Equal(rayv1.JobDeploymentStatusInitializing),
-				"JobDeploymentStatus should be Initializing")
-		})
 	})
 })
