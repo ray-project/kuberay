aeab361e39d4c364ac12e6fcc3554761a42b90c2
Owen Lin (You-Cheng Lin) <106612301+owenowenisme@users.noreply.github.com>
2025-08-18 07:50:11 +0800

    [Dashboard-client] Add proper error checking in dashboard client (#3953)
    * update
    
    Signed-off-by: You-Cheng Lin (Owen) <mses010108@gmail.com>
    
    * update from Hao's suggestion
    
    Signed-off-by: Ubuntu <ubuntu@owen-ubuntu.infra.cloudnative.tw>
    
    ---------
    
    Signed-off-by: You-Cheng Lin (Owen) <mses010108@gmail.com>
    Signed-off-by: Ubuntu <ubuntu@owen-ubuntu.infra.cloudnative.tw>
    Co-authored-by: Ubuntu <ubuntu@owen-ubuntu.infra.cloudnative.tw>

--- ray-operator/controllers/ray/utils/dashboard_httpclient.go
+++ ray-operator/controllers/ray/utils/dashboard_httpclient.go
@@ -148,7 +148,10 @@ func (r *RayDashboardClient) UpdateDeployments(ctx context.Context, configJson [
 	}
 	defer resp.Body.Close()
 
-	body, _ := io.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read response when updating deployments: %w", err)
+	}
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
 		return fmt.Errorf("UpdateDeployments fail: %s %s", resp.Status, string(body))
 	}
@@ -178,7 +181,10 @@ func (r *RayDashboardClient) GetServeDetails(ctx context.Context) (*ServeDetails
 	}
 	defer resp.Body.Close()
 
-	body, _ := io.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response when getting serve details: %w", err)
+	}
 
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
 		return nil, fmt.Errorf("GetServeDetails fail: %s %s", resp.Status, string(body))
@@ -269,7 +275,7 @@ func (r *RayDashboardClient) GetJobInfo(ctx context.Context, jobId string) (*Ray
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to read response when getting job info: %w", err)
 	}
 
 	var jobInfo RayJobInfo
@@ -299,7 +305,7 @@ func (r *RayDashboardClient) ListJobs(ctx context.Context) (*[]RayJobInfo, error
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to read response when listing jobs: %w", err)
 	}
 
 	var jobInfo []RayJobInfo
@@ -341,7 +347,10 @@ func (r *RayDashboardClient) SubmitJobReq(ctx context.Context, request *RayJobRe
 	}
 	defer resp.Body.Close()
 
-	body, _ := io.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to read response when submitting job: %w", err)
+	}
 
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
 		return "", fmt.Errorf("SubmitJob fail: %s %s", resp.Status, string(body))
@@ -379,7 +388,7 @@ func (r *RayDashboardClient) GetJobLog(ctx context.Context, jobName string) (*st
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to read response when getting job log: %w", err)
 	}
 
 	var jobLog RayJobLogsResponse
@@ -407,7 +416,10 @@ func (r *RayDashboardClient) StopJob(ctx context.Context, jobName string) (err e
 	}
 	defer resp.Body.Close()
 
-	body, _ := io.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read response when stopping job: %w", err)
+	}
 
 	var jobStopResp RayJobStopResponse
 	if err = json.Unmarshal(body, &jobStopResp); err != nil {
