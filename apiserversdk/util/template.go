c8a8f7864a9b22dbdc0361512e8c594089ba4eb7
machichima <nary12321@gmail.com>
2025-08-20 16:45:03 +0800

    feat: parse anno/cpu/mem from compute template to request
    Signed-off-by: machichima <nary12321@gmail.com>

--- apiserversdk/util/template.go
+++ apiserversdk/util/template.go
@@ -13,6 +13,7 @@ import (
 	"sigs.k8s.io/yaml"
 
 	"github.com/ray-project/kuberay/apiserver/pkg/manager"
+	"github.com/ray-project/kuberay/apiserver/pkg/util"
 	api "github.com/ray-project/kuberay/proto/go_client"
 )
 
@@ -63,6 +64,45 @@ func NewComputeTemplateMiddleware(_ kubernetes.Interface) func(http.Handler) htt
 
 			klog.Infoln("Compute Templates: ", computeTemplateMap)
 
+			// TODO: a function to inject the compute template to the container
+			// 	- for head -> directly apply to the ray-head container
+			//  - For worker -> use the function within the loop, apply to the ray-worker container
+
+			// Type assert each level of the nested map
+			spec, ok := requestMap["spec"].(map[string]interface{})
+			if !ok {
+				klog.Errorf("spec is not a map")
+				return
+			}
+
+			// TODO: This is the format for RayCluster, need to modify to apply also RayJob and RayService
+			// For RayCluster, headGroupSpec is directly under spec
+			headGroupSpec, ok := spec["headGroupSpec"].(map[string]interface{})
+			if !ok {
+				klog.Errorf("headGroupSpec is not a map")
+				return
+			}
+			computeTemplate := computeTemplateMap[clusterSpec.HeadGroupSpec.ComputeTemplate]
+			applyComputeTemplateToRequest(computeTemplate, &headGroupSpec, "head")
+			klog.Infoln("head group spec after injection: ", headGroupSpec)
+
+			// Apply compute templates to worker groups
+			workerGroupSpecs, ok := spec["workerGroupSpecs"].([]interface{})
+			if !ok {
+				klog.Errorf("Cannot convert workerGroupSpecs to a map")
+				return
+			}
+			for i, workerSpec := range clusterSpec.WorkerGroupSpec {
+				if i < len(workerGroupSpecs) {
+					if workerGroupMap, ok := workerGroupSpecs[i].(map[string]interface{}); ok {
+						computeTemplate = computeTemplateMap[workerSpec.ComputeTemplate]
+						applyComputeTemplateToRequest(computeTemplate, &workerGroupMap, "worker")
+						klog.Infof("Applied compute template to workerGroupSpecs[%d]", i)
+					}
+				}
+			}
+			klog.Infoln("worker group spec after injection: ", workerGroupSpecs)
+
 			r.Body = io.NopCloser(bytes.NewReader(bodyBytes))
 			next.ServeHTTP(w, r)
 		})
@@ -96,4 +136,77 @@ func extractClusterSpec(requestMap map[string]any) (*api.ClusterSpec, error) {
 	klog.Infof("Extracted ClusterSpec with HeadGroup ComputeTemplate: %s", clusterSpec.HeadGroupSpec.GetComputeTemplate())
 
 	return &clusterSpec, nil
+}
+
+// Apply the computeTemplate into the clusterSpec map. The clusterSpec map is the map representation
+// for headGroupSpec or workerGroupSpec
+func applyComputeTemplateToRequest(computeTemplate *api.ComputeTemplate, clusterSpecMap *map[string]interface{}, group string) {
+	// put resources in the cmpute template into the containers' resource field
+	// group: head or worker
+
+	// 1. Add metadata.annotation: buildNodeGroupAnnotations(computeRuntime, spec.Image)
+	// 	-> template.metadata.annotation
+
+	// calculate resources
+	cpu := fmt.Sprint(computeTemplate.GetCpu())
+	memory := fmt.Sprintf("%d%s", computeTemplate.GetMemory(), "Gi")
+
+	if template, ok := (*clusterSpecMap)["template"].(map[string]interface{}); ok {
+		// Add compute template name to annotation
+
+		metadata, ok := template["metadata"].(map[string]interface{})
+		if !ok {
+			metadata = make(map[string]interface{})
+			template["metadata"] = metadata
+		}
+		annotations, ok := metadata["annotations"].(map[string]interface{})
+		if !ok {
+			annotations = make(map[string]interface{})
+			metadata["annotations"] = annotations
+		}
+		annotations[util.RayClusterComputeTemplateAnnotationKey] = computeTemplate.Name
+
+		// apply resources to containers
+		if spec, ok := template["spec"].(map[string]interface{}); ok {
+			if containers, ok := spec["containers"].([]interface{}); ok {
+				for i, container := range containers {
+					if containerMap, ok := container.(map[string]interface{}); ok {
+						// Get or create resources section for this container
+						resources, exists := containerMap["resources"].(map[string]interface{})
+						if !exists {
+							resources = make(map[string]interface{})
+							containerMap["resources"] = resources
+						}
+
+						// Set limits
+						limits, exists := resources["limits"].(map[string]interface{})
+						if !exists {
+							limits = make(map[string]interface{})
+							resources["limits"] = limits
+						}
+						limits["cpu"] = cpu
+						limits["memory"] = memory
+
+						// Set requests
+						requests, exists := resources["requests"].(map[string]interface{})
+						if !exists {
+							requests = make(map[string]interface{})
+							resources["requests"] = requests
+						}
+						requests["cpu"] = cpu
+						requests["memory"] = memory
+
+						klog.Infof("Applied resources to container[%d] in %s group: CPU=%s, Memory=%s", i, group, cpu, memory)
+
+						// Only apply followings if container name is "ray-head" for head group or "ray-worker"
+						// for worker group
+						if containerMap["name"] == fmt.Sprintf("ray-%s", group) {
+							// TODO: keep working from here
+							klog.Infoln("Parsing more resources")
+						}
+					}
+				}
+			}
+		}
+	}
 }
