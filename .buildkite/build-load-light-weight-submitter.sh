75bb86e400418fb22b8afe0669f3f454bfcf1472
Owen Lin (You-Cheng Lin) <106612301+owenowenisme@users.noreply.github.com>
2025-08-21 12:48:43 +0800

    Apply suggestion from @Future-Outlier
    Co-authored-by: Han-Ju Chen (Future-Outlier) <eric901201@gmail.com>
    Signed-off-by: Owen Lin (You-Cheng Lin) <106612301+owenowenisme@users.noreply.github.com>

--- .buildkite/build-load-light-weight-submitter.sh
+++ .buildkite/build-load-light-weight-submitter.sh
@@ -5,10 +5,5 @@
 # When starting from the ray ci release automation, we want to install the latest
 # released version from helm as actual users might. Ray ci is also always expected
 # to kick off from the release branch so tests should match up accordingly.
-
-if [ "$IS_FROM_RAY_RELEASE_AUTOMATION" = 1 ]; then
-    echo "Skipping light weight submitter build for ray release automation for now"
-else
-    IMG=kuberay/submitter:nightly make docker-image-rayjobsubmitter &&
+IMG=kuberay/submitter:nightly make docker-image-rayjobsubmitter &&
     kind load docker-image kuberay/submitter:nightly
-fi
