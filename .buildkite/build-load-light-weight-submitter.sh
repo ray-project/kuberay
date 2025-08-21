fb014e1956a98d48f397475a158119fb65b6c789
You-Cheng Lin (Owen) <mses010108@gmail.com>
2025-08-21 04:50:52 +0000

    update comment in build-and-load-submitter script
    Signed-off-by: You-Cheng Lin (Owen) <mses010108@gmail.com>

--- .buildkite/build-load-light-weight-submitter.sh
+++ .buildkite/build-load-light-weight-submitter.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# This script is used to start the operator in the buildkite test-e2e steps.
-
-# When starting from the ray ci release automation, we want to install the latest
-# released version from helm as actual users might. Ray ci is also always expected
-# to kick off from the release branch so tests should match up accordingly.
+# This script is used to build and load the light-weight submitter image into the kind cluster.
 IMG=kuberay/submitter:nightly make docker-image-rayjobsubmitter &&
     kind load docker-image kuberay/submitter:nightly
