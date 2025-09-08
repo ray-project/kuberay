b467c89b13fb5d0ec457e0c90efd9c42f33f47bc
You-Cheng Lin (Owen) <mses010108@gmail.com>
2025-09-08 11:54:35 +0000

    modify existing submission id msg
    Signed-off-by: You-Cheng Lin (Owen) <mses010108@gmail.com>

--- ray-operator/controllers/ray/utils/dashboardclient/dashboard_httpclient.go
+++ ray-operator/controllers/ray/utils/dashboardclient/dashboard_httpclient.go
@@ -224,7 +224,7 @@ func (r *RayDashboardClient) SubmitJobReq(ctx context.Context, request *utiltype
 
 	if resp.StatusCode < 200 || resp.StatusCode > 299 {
 		// If the submission_id is already used,dashboard will return status code 400, we return the submission_id directly.
-		if strings.Contains(string(body), "Please use a different submission_id") {
+		if strings.Contains(string(body), "Job is submitted") {
 			return request.SubmissionId, nil
 		}
 		return "", fmt.Errorf("SubmitJob fail: %s %s", resp.Status, string(body))
