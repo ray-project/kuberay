689682f0d40b214cc38f22e682788fb9ea71f8cc
Ryan O'Leary <ryanaoleary@google.com>
2025-10-01 13:58:37 +0000

    Fix port errors
    Signed-off-by: Ryan O'Leary <ryanaoleary@google.com>

--- ray-operator/controllers/ray/utils/util.go
+++ ray-operator/controllers/ray/utils/util.go
@@ -786,8 +786,9 @@ func GetGatewayListenersForRayService(rayServiceInstance *rayv1.RayService) []gw
 	listenerName := fmt.Sprintf("%s-listener", rayServiceInstance.Name)
 	listener := gwv1.Listener{
 		Name:     gwv1.SectionName(listenerName),
-		Protocol: gwv1.HTTPProtocolType,      // only support HTTP
-		Port:     utils.FindContainerPort(rayContainer, utils.ServingPortName, utils.DefaultServingPort)
+		Protocol: gwv1.HTTPProtocolType, // only support HTTP
+		Port:     gwv1.PortNumber(int32(80)),
+	}
 	listeners = append(listeners, listener)
 
 	return listeners
