75f20cdfc8be6b14db7690ca99b592beeb2f10ec
fscnick <fscnick.dev@gmail.com>
2025-10-28 15:36:14 +0800

    [RayJob] background job info poc

--- ray-operator/apis/config/v1alpha1/configuration_types.go
+++ ray-operator/apis/config/v1alpha1/configuration_types.go
@@ -88,6 +88,10 @@ type Configuration struct {
 
 	// EnableMetrics indicates whether KubeRay operator should emit control plane metrics.
 	EnableMetrics bool `json:"enableMetrics,omitempty"`
+
+	// UseBackgroundGoroutine indicates that it wil use goroutine to fetch the job info from ray dashboard and
+	// store the job info in the cache
+	UseBackgroundGoroutine bool `json:"useBackgroundGoroutine,omitempty"`
 }
 
 func (config Configuration) GetDashboardClient(ctx context.Context, mgr manager.Manager) func(rayCluster *rayv1.RayCluster, url string) (dashboardclient.RayDashboardClientInterface, error) {
