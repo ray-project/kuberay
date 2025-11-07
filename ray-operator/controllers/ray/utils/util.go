b51f885c1becb863166ab266a5b2daa4d6db653b
JustinYeh <justinyeh1995@gmail.com>
2025-11-07 09:30:45 +0800

    [Fix] Resolve int32 overflow by having the calculation in int64 and c… (#4158)
    * [Fix] Resolve int32 overflow by having the calculation in int64 and cap it if the count is over math.MaxInt32
    
    Signed-off-by: justinyeh1995 <justinyeh1995@gmail.com>
    
    * [Test] Add unit tests for CalculateReadyReplicas
    
    Signed-off-by: justinyeh1995 <justinyeh1995@gmail.com>
    
    * [Fix] Add a nosec comment to pass the Lint (pre-commit) test
    
    Signed-off-by: justinyeh1995 <justinyeh1995@gmail.com>
    
    * [Refactor] Add CapInt64ToInt32 to replace #nosec directives
    
    Signed-off-by: justinyeh1995 <justinyeh1995@gmail.com>
    
    * [Refactor] Rename function to SafeInt64ToInt32 and add a underflowing prevention (it also help pass the lint test)
    
    Signed-off-by: justinyeh1995 <justinyeh1995@gmail.com>
    
    * [Refactor] Remove the early return as SafeInt64ToInt32 handles the int32 overflow and underflow checking.
    
    Signed-off-by: justinyeh1995 <justinyeh1995@gmail.com>
    
    ---------
    
    Signed-off-by: justinyeh1995 <justinyeh1995@gmail.com>

--- ray-operator/controllers/ray/utils/util.go
+++ ray-operator/controllers/ray/utils/util.go
@@ -253,6 +253,18 @@ func SafeUint64ToInt64(n uint64) int64 {
 	return int64(n)
 }
 
+// SafeInt64ToInt32 converts int64 to int32, preventing overflow/underflow by
+// bounding the value between [math.MinInt32, math.MaxInt32]
+func SafeInt64ToInt32(n int64) int32 {
+	if n > math.MaxInt32 {
+		return math.MaxInt32
+	}
+	if n < math.MinInt32 {
+		return math.MinInt32
+	}
+	return int32(n)
+}
+
 // GetNamespace return namespace
 func GetNamespace(metaData metav1.ObjectMeta) string {
 	if metaData.Namespace == "" {
@@ -393,15 +405,15 @@ func CalculateMinReplicas(cluster *rayv1.RayCluster) int32 {
 
 // CalculateMaxReplicas calculates max worker replicas at the cluster level
 func CalculateMaxReplicas(cluster *rayv1.RayCluster) int32 {
-	count := int32(0)
+	count := int64(0)
 	for _, nodeGroup := range cluster.Spec.WorkerGroupSpecs {
 		if nodeGroup.Suspend != nil && *nodeGroup.Suspend {
 			continue
 		}
-		count += (*nodeGroup.MaxReplicas * nodeGroup.NumOfHosts)
+		count += int64(*nodeGroup.MaxReplicas) * int64(nodeGroup.NumOfHosts)
 	}
 
-	return count
+	return SafeInt64ToInt32(count)
 }
 
 // CalculateReadyReplicas calculates ready worker replicas at the cluster level
