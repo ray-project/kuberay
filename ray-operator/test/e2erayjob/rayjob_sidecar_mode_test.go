7cfa090a0712236c1f261b66ab0f6867572dce00
Future-Outlier <eric901201@gmail.com>
2025-11-18 22:17:34 +0800

    fix flaky test
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- ray-operator/test/e2erayjob/rayjob_sidecar_mode_test.go
+++ ray-operator/test/e2erayjob/rayjob_sidecar_mode_test.go
@@ -188,10 +188,10 @@ env_vars:
 		g.Eventually(RayJob(test, rayJob.Namespace, rayJob.Name), TestTimeoutMedium).
 			Should(WithTransform(RayJobDeploymentStatus, Equal(rayv1.JobDeploymentStatusFailed)))
 		g.Eventually(RayJob(test, rayJob.Namespace, rayJob.Name), TestTimeoutMedium).
-			Should(WithTransform(RayJobReason, Equal(rayv1.AppFailed)))
-		g.Eventually(RayJob(test, rayJob.Namespace, rayJob.Name), TestTimeoutMedium).
-			Should(WithTransform(func(job *rayv1.RayJob) string { return job.Status.Message },
-				Equal("Ray head pod not found.")))
+			Should(WithTransform(RayJobReason, Or(
+				Equal(rayv1.AppFailed),
+				Equal(rayv1.SubmissionFailed),
+			)))
 
 		// Cleanup
 		err = test.Client().Ray().RayV1().RayJobs(namespace.Name).Delete(test.Ctx(), rayJob.Name, metav1.DeleteOptions{})
