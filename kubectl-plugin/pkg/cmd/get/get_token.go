3570c0d88708ff6d4169f34a2f8af9281554615f
Rueian <rueiancsie@gmail.com>
2025-11-20 13:37:41 -0800

    feat: kubectl ray get token command
    Signed-off-by: Rueian <rueiancsie@gmail.com>

---
+++ kubectl-plugin/pkg/cmd/get/get_token.go
@@ -0,0 +1,79 @@
+package get
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+
+	"github.com/ray-project/kuberay/kubectl-plugin/pkg/util/client"
+	"github.com/ray-project/kuberay/kubectl-plugin/pkg/util/completion"
+)
+
+type GetTokenOptions struct {
+	cmdFactory cmdutil.Factory
+	ioStreams  *genericclioptions.IOStreams
+	namespace  string
+	secret     string
+}
+
+func NewGetTokenOptions(cmdFactory cmdutil.Factory, streams genericclioptions.IOStreams) *GetTokenOptions {
+	return &GetTokenOptions{
+		cmdFactory: cmdFactory,
+		ioStreams:  &streams,
+	}
+}
+
+func NewGetTokenCommand(cmdFactory cmdutil.Factory, streams genericclioptions.IOStreams) *cobra.Command {
+	options := NewGetTokenOptions(cmdFactory, streams)
+
+	cmd := &cobra.Command{
+		Use:               "token [SECRET NAME]",
+		Aliases:           []string{"token"},
+		Short:             "Get the auth token from the secret.",
+		SilenceUsage:      true,
+		ValidArgsFunction: completion.RayClusterCompletionFunc(cmdFactory),
+		Args:              cobra.MaximumNArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := options.Complete(args, cmd); err != nil {
+				return err
+			}
+			// running cmd.Execute or cmd.ExecuteE sets the context, which will be done by root
+			k8sClient, err := client.NewClient(cmdFactory)
+			if err != nil {
+				return fmt.Errorf("failed to create client: %w", err)
+			}
+			return options.Run(cmd.Context(), k8sClient)
+		},
+	}
+	return cmd
+}
+
+func (options *GetTokenOptions) Complete(args []string, cmd *cobra.Command) error {
+	namespace, err := cmd.Flags().GetString("namespace")
+	if err != nil {
+		return fmt.Errorf("failed to get namespace: %w", err)
+	}
+	options.namespace = namespace
+	if options.namespace == "" {
+		options.namespace = "default"
+	}
+
+	if len(args) >= 1 {
+		options.secret = args[0]
+	}
+
+	return nil
+}
+
+func (options *GetTokenOptions) Run(ctx context.Context, k8sClient client.Client) error {
+	secret, err := k8sClient.KubernetesClient().CoreV1().Secrets(options.namespace).Get(ctx, options.secret, v1.GetOptions{})
+	if err != nil {
+		return err
+	}
+	fmt.Fprint(options.ioStreams.Out, string(secret.Data["auth_token"]))
+	return nil
+}
---
+++ kubectl-plugin/pkg/cmd/get/get_token_test.go
@@ -0,0 +1,48 @@
+package get
+
+import (
+	"testing"
+
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	kubefake "k8s.io/client-go/kubernetes/fake"
+	cmdutil "k8s.io/kubectl/pkg/cmd/util"
+
+	"github.com/ray-project/kuberay/kubectl-plugin/pkg/util/client"
+	rayClientFake "github.com/ray-project/kuberay/ray-operator/pkg/client/clientset/versioned/fake"
+)
+
+// Tests the Run() step of the command and ensure that the output is as expected.
+func TestTokenGetRun(t *testing.T) {
+	cmdFactory := cmdutil.NewFactory(genericclioptions.NewConfigFlags(true))
+
+	testStreams, _, resBuf, _ := genericclioptions.NewTestIOStreams()
+	fakeTokenGetOptions := NewGetTokenOptions(cmdFactory, testStreams)
+
+	secret := &corev1.Secret{
+		ObjectMeta: v1.ObjectMeta{
+			Name:      "raycluster-kuberay",
+			Namespace: "test",
+		},
+		Data: map[string][]byte{
+			"auth_token": []byte("token"),
+		},
+	}
+
+	kubeClientSet := kubefake.NewClientset(secret)
+	rayClient := rayClientFake.NewSimpleClientset()
+	k8sClients := client.NewClientForTesting(kubeClientSet, rayClient)
+
+	cmd := &cobra.Command{}
+	cmd.Flags().StringVarP(&fakeTokenGetOptions.namespace, "namespace", "n", secret.Namespace, "")
+	err := fakeTokenGetOptions.Complete([]string{secret.Name}, cmd)
+	require.NoError(t, err)
+	err = fakeTokenGetOptions.Run(t.Context(), k8sClients)
+	require.NoError(t, err)
+
+	assert.Equal(t, resBuf.Bytes(), secret.Data["auth_token"])
+}
