b03a22df0610d3d8d0ac7538bba24a2b6dccbe30
machichima <nary12321@gmail.com>
2025-12-09 19:22:11 +0800

    build: add kubebuilder rbac config
    Signed-off-by: machichima <nary12321@gmail.com>

--- ray-operator/controllers/ray/raycronjob_controller.go
+++ ray-operator/controllers/ray/raycronjob_controller.go
@@ -45,16 +45,14 @@ func NewRayCronJobReconciler(mgr ctrl.Manager) *RayCronJobReconciler {
 //+kubebuilder:rbac:groups=ray.io,resources=raycronjobs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=ray.io,resources=raycronjobs/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=ray.io,resources=raycronjobs/finalizers,verbs=update
+//+kubebuilder:rbac:groups=ray.io,resources=rayjobs,verbs=create
+//+kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the RayCronJob object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.4/pkg/reconcile
+// [WARNING]: There MUST be a newline after kubebuilder markers.
+// Reconcile reads that state of a RayCronJob object and makes changes based on it
+// and what is in the RayCronJob.Spec
+// Automatically generate RBAC rules to allow the Controller to read and write workloads
+// Reconcile used to bridge the desired state with the current state
 func (r *RayCronJobReconciler) Reconcile(ctx context.Context, request ctrl.Request) (ctrl.Result, error) {
 	logger := ctrl.LoggerFrom(ctx)
 
