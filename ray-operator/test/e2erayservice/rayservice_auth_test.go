a8a785af901188ccf10b319cf85ca16296e0e9cf
Ryan Huang <ryankert01@gmail.com>
2025-12-10 02:20:17 +0800

    [RayService] auth token mode e2e test (#4225)
    * add ray service auth test
    
    Signed-off-by: Ryan <ryan980053@gmail.com>
    
    * try to fix the error
    
    Signed-off-by: Ryan <ryan980053@gmail.com>
    
    * add worker group
    
    Signed-off-by: Ryan <ryan980053@gmail.com>
    
    * Adjust resource requests and limits in tests
    
    Signed-off-by: Ryan Huang <ryankert01@gmail.com>
    
    * Simplify RayService auth test by removing worker group
    
    Removed worker group spec and related verification for auth token propagation in RayService tests.
    
    Signed-off-by: Ryan Huang <ryankert01@gmail.com>
    
    * Update rayservice_auth_test.go
    
    Signed-off-by: Ryan Huang <ryankert01@gmail.com>
    
    * Refactor TestRayServiceAuthToken for clarity
    
    Refactor test for RayService authentication to improve clarity and maintainability.
    
    Signed-off-by: Ryan Huang <ryankert01@gmail.com>
    
    * Update ray-operator/test/e2erayservice/rayservice_auth_test.go
    
    Co-authored-by: Han-Ju Chen (Future-Outlier) <eric901201@gmail.com>
    Signed-off-by: Ryan Huang <ryankert01@gmail.com>
    
    * Update ray-operator/test/e2erayservice/rayservice_auth_test.go
    
    Co-authored-by: Jun-Hao Wan <ken89@kimo.com>
    Signed-off-by: Ryan Huang <ryankert01@gmail.com>
    
    * address comments
    
    Signed-off-by: ryankert01 <ryan980053@gmail.com>
    
    * pre-commit check
    
    Signed-off-by: ryankert01 <ryan980053@gmail.com>
    
    * update test
    
    Signed-off-by: Future-Outlier <eric901201@gmail.com>
    
    * revert my update
    
    Signed-off-by: Future-Outlier <eric901201@gmail.com>
    
    ---------
    
    Signed-off-by: Ryan <ryan980053@gmail.com>
    Signed-off-by: Ryan Huang <ryankert01@gmail.com>
    Signed-off-by: ryankert01 <ryan980053@gmail.com>
    Signed-off-by: Future-Outlier <eric901201@gmail.com>
    Co-authored-by: Han-Ju Chen (Future-Outlier) <eric901201@gmail.com>
    Co-authored-by: Jun-Hao Wan <ken89@kimo.com>

---
+++ ray-operator/test/e2erayservice/rayservice_auth_test.go
@@ -0,0 +1,68 @@
+package e2erayservice
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	rayv1 "github.com/ray-project/kuberay/ray-operator/apis/ray/v1"
+	"github.com/ray-project/kuberay/ray-operator/controllers/ray/utils"
+	rayv1ac "github.com/ray-project/kuberay/ray-operator/pkg/client/applyconfiguration/ray/v1"
+	. "github.com/ray-project/kuberay/ray-operator/test/support"
+)
+
+func TestRayServiceAuthToken(t *testing.T) {
+	test := With(t)
+	g := NewWithT(t)
+
+	namespace := test.NewTestNamespace()
+
+	rayServiceName := "rayservice-auth"
+	rayServiceSpec := RayServiceSampleYamlApplyConfiguration()
+	rayServiceSpec.RayClusterSpec.WithAuthOptions(rayv1ac.AuthOptions().WithMode(rayv1.AuthModeToken))
+
+	rayServiceAC := rayv1ac.RayService(rayServiceName, namespace.Name).WithSpec(rayServiceSpec)
+
+	rayService, err := test.Client().Ray().RayV1().RayServices(namespace.Name).Apply(test.Ctx(), rayServiceAC, TestApplyOptions)
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(rayService).NotTo(BeNil())
+	LogWithTimestamp(test.T(), "Created RayService %s/%s successfully with AuthModeToken", rayService.Namespace, rayService.Name)
+
+	defer func() {
+		err := test.Client().Ray().RayV1().RayServices(namespace.Name).Delete(test.Ctx(), rayService.Name, metav1.DeleteOptions{})
+		if err != nil {
+			LogWithTimestamp(test.T(), "WARNING: Failed to delete RayService %s: %v", rayService.Name, err)
+		} else {
+			LogWithTimestamp(test.T(), "Deleted RayService %s/%s successfully", rayService.Namespace, rayService.Name)
+		}
+	}()
+
+	LogWithTimestamp(test.T(), "Waiting for RayService %s/%s to be ready", rayService.Namespace, rayService.Name)
+	g.Eventually(RayService(test, rayService.Namespace, rayService.Name), TestTimeoutMedium).
+		Should(WithTransform(IsRayServiceReady, BeTrue()))
+
+	rayService, err = GetRayService(test, namespace.Name, rayServiceName)
+	g.Expect(err).NotTo(HaveOccurred())
+	LogWithTimestamp(test.T(), "RayService %s/%s is ready", rayService.Namespace, rayService.Name)
+
+	rayClusterName := rayService.Status.ActiveServiceStatus.RayClusterName
+	g.Expect(rayClusterName).NotTo(BeEmpty(), "RayCluster name should be populated in status")
+	LogWithTimestamp(test.T(), "RayService %s/%s has active RayCluster %s", rayService.Namespace, rayService.Name, rayClusterName)
+
+	rayCluster, err := GetRayCluster(test, namespace.Name, rayClusterName)
+	g.Expect(err).NotTo(HaveOccurred())
+
+	headPod, err := GetHeadPod(test, rayCluster)
+	g.Expect(err).NotTo(HaveOccurred())
+	g.Expect(headPod).NotTo(BeNil())
+	LogWithTimestamp(test.T(), "Found head pod %s/%s", headPod.Namespace, headPod.Name)
+
+	// Verify Ray container has auth token env vars
+	VerifyContainerAuthTokenEnvVars(test, rayCluster, &headPod.Spec.Containers[utils.RayContainerIndex])
+
+	g.Expect(rayService.Status.NumServeEndpoints).To(BeNumerically(">", 0),
+		"RayService should have at least one serve endpoint")
+
+	LogWithTimestamp(test.T(), "RayService %s/%s completed successfully with auth token", rayService.Namespace, rayService.Name)
+}
