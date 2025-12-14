be7ce9915e2379191f615277e42beb1f920e198c
fscnick <fscnick.dev@gmail.com>
2025-12-14 21:39:24 +0800

    [RayJob] rename useBackgroundGoroutine to asyncJobInfoQuery
    Signed-off-by: fscnick <fscnick.dev@gmail.com>

--- ray-operator/apis/config/v1alpha1/configuration_types.go
+++ ray-operator/apis/config/v1alpha1/configuration_types.go
@@ -89,9 +89,9 @@ type Configuration struct {
 	// EnableMetrics indicates whether KubeRay operator should emit control plane metrics.
 	EnableMetrics bool `json:"enableMetrics,omitempty"`
 
-	// UseBackgroundGoroutine indicates that it wil use goroutine to fetch the job info from ray dashboard and
+	// AsyncJobInfoQuery indicates that it wil use goroutine to fetch the job info from ray dashboard and
 	// store the job info in the cache
-	UseBackgroundGoroutine bool `json:"useBackgroundGoroutine,omitempty"`
+	AsyncJobInfoQuery bool `json:"useBackgroundGoroutine,omitempty"`
 }
 
 func (config Configuration) GetDashboardClient(ctx context.Context, mgr manager.Manager) func(rayCluster *rayv1.RayCluster, url string) (dashboardclient.RayDashboardClientInterface, error) {
