401a76073ac33236cc297bf05f36ea913865176c
JiangJiaWei1103 <waynechuang97@gmail.com>
2025-12-25 08:19:20 +0800

    docs: Add script comment
    Signed-off-by: JiangJiaWei1103 <waynechuang97@gmail.com>

--- .buildkite/build-historyserver.sh
+++ .buildkite/build-historyserver.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# This script is used to build container images of history server components in
+# the buildkite test-historyserver-e2e step.
+
+# NOTE: For now, only the log collector is built and tested.
+
+# TODO(jwj): Skip building if starting from ray ci release automation.
 make docker-image &&
 kind load docker-image collector:v0.1.0
