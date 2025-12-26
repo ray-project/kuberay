474c6b2730a32f2f5b90a77046cac123a73486f3
JiangJiaWei1103 <waynechuang97@gmail.com>
2025-12-26 21:08:58 +0800

    revert: Make this PR focused on Collector setup guide only
    Signed-off-by: JiangJiaWei1103 <waynechuang97@gmail.com>

--- historyserver/cmd/collector/main.go
+++ historyserver/cmd/collector/main.go
@@ -5,11 +5,13 @@ import (
 	"encoding/json"
 	"flag"
 	"os"
+	"path"
 	"time"
 
 	"github.com/sirupsen/logrus"
 
 	"github.com/ray-project/kuberay/historyserver/pkg/collector"
+	"github.com/ray-project/kuberay/historyserver/pkg/collector/eventserver"
 	"github.com/ray-project/kuberay/historyserver/pkg/collector/logcollector/runtime"
 	"github.com/ray-project/kuberay/historyserver/pkg/collector/types"
 	"github.com/ray-project/kuberay/historyserver/pkg/utils"
@@ -24,6 +26,7 @@ func main() {
 	rayClusterId := ""
 	rayRootDir := ""
 	logBatching := 1000
+	eventsPort := 8080
 	pushInterval := time.Minute
 	runtimeClassConfigPath := "/var/collector-config/data"
 
@@ -33,6 +36,7 @@ func main() {
 	flag.StringVar(&rayClusterId, "ray-cluster-id", "default", "")
 	flag.StringVar(&rayRootDir, "ray-root-dir", "", "")
 	flag.IntVar(&logBatching, "log-batching", 1000, "")
+	flag.IntVar(&eventsPort, "events-port", 8080, "")
 	flag.StringVar(&runtimeClassConfigPath, "runtime-class-config-path", "", "") //"/var/collector-config/data"
 	flag.DurationVar(&pushInterval, "push-interval", time.Minute, "")
 
@@ -47,6 +51,8 @@ func main() {
 	if err != nil {
 		panic("Failed to get ray node id: " + err.Error())
 	}
+
+	sessionName := path.Base(sessionDir)
 
 	jsonData := make(map[string]interface{})
 	if runtimeClassConfigPath != "" {
@@ -83,11 +89,17 @@ func main() {
 		panic("Failed to create writer for runtime class name: " + runtimeClassName + " for role: " + role + ".")
 	}
 
-	// Initailize the log collector.
+	// Create and initialize EventServer
+	eventServer := eventserver.NewEventServer(writer, rayRootDir, sessionDir, rayNodeId, rayClusterName, rayClusterId, sessionName)
+	eventServer.InitServer(eventsPort)
+
 	collector := runtime.NewCollector(&globalConfig, writer)
 	_ = collector.Start(context.TODO().Done())
 
+	eventStop := eventServer.WaitForStop()
 	logStop := collector.WaitForStop()
+	<-eventStop
+	logrus.Info("Event server shutdown")
 	<-logStop
 	logrus.Info("Log server shutdown")
 	logrus.Info("All servers shutdown")
