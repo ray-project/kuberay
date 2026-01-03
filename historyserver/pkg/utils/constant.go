989654e369aecf0e5110a52b9ff4466e7b67ec0f
JiangJiaWei1103 <waynechuang97@gmail.com>
2026-01-03 11:37:26 +0800

    refactor: Rename environment variables
    Signed-off-by: JiangJiaWei1103 <waynechuang97@gmail.com>

--- historyserver/pkg/utils/constant.go
+++ historyserver/pkg/utils/constant.go
@@ -16,23 +16,23 @@ const (
 
 // OSS meta file keys used by history server.
 const (
-	OssMetaFile_BasicInfo = "ack__basicinfo"
+	RayMetaFile_BasicInfo = "ack__basicinfo"
 
-	OssMetaFile_NodeSummaryKey                        = "restful__nodes_view_summary"
-	OssMetaFile_Node_Prefix                           = "restful__nodes_"
-	OssMetaFile_JOBTASK_DETAIL_Prefix                 = "restful__api__v0__tasks_detail_job_id_"
-	OssMetaFile_JOBTASK_SUMMARIZE_BY_FUNC_NAME_Prefix = "restful__api__v0__tasks_summarize_by_func_name_job_id_"
-	OssMetaFile_JOBTASK_SUMMARIZE_BY_LINEAGE_Prefix   = "restful__api__v0__tasks_summarize_by_lineage_job_id_"
-	OssMetaFile_JOBDATASETS_Prefix                    = "restful__api__data__datasets_job_id_"
-	OssMetaFile_NodeLogs_Prefix                       = "restful__api__v0__logs_node_id_"
-	OssMetaFile_ClusterStatus                         = "restful__api__cluster_status"
-	OssMetaFile_LOGICAL_ACTORS                        = "restful__logical__actors"
-	OssMetaFile_ALLTASKS_DETAIL                       = "restful__api__v0__tasks_detail"
-	OssMetaFile_Events                                = "restful__events"
-	OssMetaFile_PlacementGroups                       = "restful__api__v0__placement_groups_detail"
-	OssMetaFile_ClusterSessionName                    = "static__api__cluster_session_name"
-	OssMetaFile_Jobs                                  = "restful__api__jobs"
-	OssMetaFile_Applications                          = "restful__api__serve__applications"
+	RayMetaFile_NodeSummaryKey                        = "restful__nodes_view_summary"
+	RayMetaFile_Node_Prefix                           = "restful__nodes_"
+	RayMetaFile_JOBTASK_DETAIL_Prefix                 = "restful__api__v0__tasks_detail_job_id_"
+	RayMetaFile_JOBTASK_SUMMARIZE_BY_FUNC_NAME_Prefix = "restful__api__v0__tasks_summarize_by_func_name_job_id_"
+	RayMetaFile_JOBTASK_SUMMARIZE_BY_LINEAGE_Prefix   = "restful__api__v0__tasks_summarize_by_lineage_job_id_"
+	RayMetaFile_JOBDATASETS_Prefix                    = "restful__api__data__datasets_job_id_"
+	RayMetaFile_NodeLogs_Prefix                       = "restful__api__v0__logs_node_id_"
+	RayMetaFile_ClusterStatus                         = "restful__api__cluster_status"
+	RayMetaFile_LOGICAL_ACTORS                        = "restful__logical__actors"
+	RayMetaFile_ALLTASKS_DETAIL                       = "restful__api__v0__tasks_detail"
+	RayMetaFile_Events                                = "restful__events"
+	RayMetaFile_PlacementGroups                       = "restful__api__v0__placement_groups_detail"
+	RayMetaFile_ClusterSessionName                    = "static__api__cluster_session_name"
+	RayMetaFile_Jobs                                  = "restful__api__jobs"
+	RayMetaFile_Applications                          = "restful__api__serve__applications"
 )
 
 // Ray history server log file name.
