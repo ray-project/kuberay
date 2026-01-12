ec5fafcb6c5015429530be1835c06a14a973a849
kash2104 <kparikh1104@gmail.com>
2026-01-12 20:28:29 +0530

    refactor: Standardize namespace resolution for kubectl-plugin.

--- kubectl-plugin/pkg/cmd/get/get_workergroup.go
+++ kubectl-plugin/pkg/cmd/get/get_workergroup.go
@@ -90,7 +90,7 @@ func NewGetWorkerGroupCommand(cmdFactory cmdutil.Factory, streams genericcliopti
 		Args:              cobra.MaximumNArgs(1),
 		ValidArgsFunction: completion.WorkerGroupCompletionFunc(cmdFactory),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if err := options.Complete(args, cmd); err != nil {
+			if err := options.Complete(args); err != nil {
 				return err
 			}
 			k8sClient, err := client.NewClient(cmdFactory)
@@ -112,19 +112,15 @@ func NewGetWorkerGroupCommand(cmdFactory cmdutil.Factory, streams genericcliopti
 	return cmd
 }
 
-func (options *GetWorkerGroupsOptions) Complete(args []string, cmd *cobra.Command) error {
+func (options *GetWorkerGroupsOptions) Complete(args []string) error {
 	if options.allNamespaces {
 		options.namespace = ""
 	} else {
-		namespace, err := cmd.Flags().GetString("namespace")
+		namespace, _, err := options.cmdFactory.ToRawKubeConfigLoader().Namespace()
 		if err != nil {
 			return fmt.Errorf("failed to get namespace: %w", err)
 		}
 		options.namespace = namespace
-
-		if options.namespace == "" {
-			options.namespace = "default"
-		}
 	}
 
 	if len(args) > 0 {
