62b71b54f7c183d0a71907d0213e184acae263d6
Future-Outlier <eric901201@gmail.com>
2026-01-13 13:03:42 +0800

    tesst
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- .buildkite/build-start-operator.sh
+++ .buildkite/build-start-operator.sh
@@ -11,12 +11,11 @@ if [ "$IS_FROM_RAY_RELEASE_AUTOMATION" = 1 ]; then
     echo "Installing helm chart with test override values (feature gates enabled as needed)"
     # NOTE: The override file is CI/test-only. It is NOT part of the released chart defaults.
     helm install kuberay-operator kuberay/kuberay-operator -f ../.buildkite/values-kuberay-operator-override.yaml
-    KUBERAY_TEST_RAY_IMAGE="rayproject/ray:nightly.$(date +'%y%m%d').${RAY_NIGHTLY_COMMIT:0:6}-py310" && export KUBERAY_TEST_RAY_IMAGE
+    KUBERAY_TEST_RAY_IMAGE="rayproject/ray:nightly-extra-py310-cpu" && export KUBERAY_TEST_RAY_IMAGE
 else
     IMG=kuberay/operator:nightly make docker-image &&
     kind load docker-image kuberay/operator:nightly &&
     echo "Deploying operator with test overrides (feature gates via test-overrides overlay)"
     IMG=kuberay/operator:nightly make deploy-with-override
-    RAY_NIGHTLY_COMMIT="a5d3e3c80ec604e4242dd5f5899d0851969aba0a"
-    KUBERAY_TEST_RAY_IMAGE="rayproject/ray:nightly.$(date +'%y%m%d').${RAY_NIGHTLY_COMMIT:0:6}-py310" && export KUBERAY_TEST_RAY_IMAGE
+    KUBERAY_TEST_RAY_IMAGE="rayproject/ray:nightly-extra-py310-cpu" && export KUBERAY_TEST_RAY_IMAGE
 fi
