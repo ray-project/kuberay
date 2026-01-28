1ed1ea0b970f6d26163e24342b71985fa9bd7797
fweilun <william20020503@gmail.com>
2026-01-28 23:58:48 +0800

    correct MetaKeyInfo path.

--- historyserver/pkg/historyserver/reader.go
+++ historyserver/pkg/historyserver/reader.go
@@ -165,7 +165,7 @@ func (h *ServerHandler) getGrafanaHealth(req *restful.Request, resp *restful.Res
 }
 
 func (s *ServerHandler) MetaKeyInfo(rayClusterNameID, key string) []byte {
-	baseObject := path.Join(utils.GetMetaDirByNameID(s.rootDir, rayClusterNameID), key)
+	baseObject := path.Join(utils.RAY_SESSIONDIR_METADIR_NAME, key)
 	logrus.Infof("Prepare to get object %s info ...", baseObject)
 	body := s.reader.GetContent(rayClusterNameID, baseObject)
 	if body == nil {
