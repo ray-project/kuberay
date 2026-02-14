b52ed315dcf8074ce9cf7264716f71917503f915
justinyeh1995 <justinyeh1995@gmail.com>
2026-02-14 23:30:27 +0800

    [Fix] Use helper function DeleteRayClusterAndWait
    Signed-off-by: justinyeh1995 <justinyeh1995@gmail.com>

--- historyserver/test/e2e/historyserver_test.go
+++ historyserver/test/e2e/historyserver_test.go
@@ -1262,16 +1262,7 @@ func testDeadClusterStatus(test Test, g *WithT, namespace *corev1.Namespace, s3C
 	rayCluster := PrepareTestEnv(test, g, namespace, s3Client)
 	ApplyRayJobAndWaitForCompletion(test, g, namespace, rayCluster)
 
-	// Delete RayCluster to trigger log upload
-	err := test.Client().Ray().RayV1().RayClusters(namespace.Name).Delete(test.Ctx(), rayCluster.Name, metav1.DeleteOptions{})
-	g.Expect(err).NotTo(HaveOccurred())
-	LogWithTimestamp(test.T(), "Deleted RayCluster %s/%s", namespace.Name, rayCluster.Name)
-
-	// Wait for cluster to be fully deleted (ensures logs are uploaded to S3)
-	g.Eventually(func() error {
-		_, err := GetRayCluster(test, namespace.Name, rayCluster.Name)
-		return err
-	}, TestTimeoutMedium).Should(WithTransform(k8serrors.IsNotFound, BeTrue()))
+	DeleteRayClusterAndWait(test, g, namespace.Name, rayCluster.Name)
 
 	ApplyHistoryServer(test, g, namespace, "")
 	historyServerURL := GetHistoryServerURL(test, g, namespace)
