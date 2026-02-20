e8c4e0037d94f70d030466c23056abacd663989a
Andrew Sy Kim <andrewsy@google.com>
2026-02-20 20:22:40 +0000

    fix failing CI checks
    Signed-off-by: Andrew Sy Kim <andrewsy@google.com>

--- ray-operator/apis/ray/v1/raycluster_types.go
+++ ray-operator/apis/ray/v1/raycluster_types.go
@@ -80,16 +80,17 @@ const (
 
 // AuthOptions defines the authentication options for a RayCluster.
 type AuthOptions struct {
+	// EnableK8sTokenAuth specifies whether to enable K8s token authentication.
+	// When set to true, all Ray Pods will set the environment variable RAY_ENABLE_K8S_TOKEN_AUTH to "true".
+	// +optional
+	EnableK8sTokenAuth *bool `json:"enableK8sTokenAuth,omitempty"`
+
 	// Mode specifies the authentication mode.
 	// Supported values are "disabled" and "token".
 	// Defaults to "token".
 	// +kubebuilder:validation:Enum=disabled;token
 	// +optional
 	Mode AuthMode `json:"mode,omitempty"`
-	// EnableK8sTokenAuth specifies whether to enable K8s token authentication.
-	// When set to true, all Ray Pods will set the environment variable RAY_ENABLE_K8S_TOKEN_AUTH to "true".
-	// +optional
-	EnableK8sTokenAuth *bool `json:"enableK8sTokenAuth,omitempty"`
 }
 
 // GcsFaultToleranceOptions contains configs for GCS FT
