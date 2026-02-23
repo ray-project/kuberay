7ae06579e4aba2a0809eea946cd7ec5a316710fd
Fangyin Cheng <staneyffer@gmail.com>
2026-02-23 14:18:36 +0800

    [Test][HistoryServer] E2E test for dead cluster actor endpoint (#4461)
    * [Test][HistoryServer] E2E test for dead cluster actor endpoint
    
    * chore: fix e2e dead cluster tests
    
    * chore: fix unsafe type assertions in actor endpoint tests
    
    * chore: Merge latest codes
    
    * Trigger CI
    
    Signed-off-by: Future-Outlier <eric901201@gmail.com>
    
    * update
    
    Signed-off-by: Future-Outlier <eric901201@gmail.com>
    
    * update
    
    Signed-off-by: Future-Outlier <eric901201@gmail.com>
    
    ---------
    
    Signed-off-by: Future-Outlier <eric901201@gmail.com>
    Co-authored-by: Future-Outlier <eric901201@gmail.com>

--- historyserver/test/support/constant.go
+++ historyserver/test/support/constant.go
@@ -2,6 +2,7 @@ package support
 
 const (
 	EndpointLogsFile      = "/api/v0/logs/file"
+	EndpointLogicalActors = "/logical/actors"
 	EndpointLogsStream    = "/api/v0/logs/stream"
 	EndpointTasks         = "/api/v0/tasks"
 	EndpointNodes         = "/nodes"
