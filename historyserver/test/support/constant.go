fb4a57c2ba75d2c7ac0864f6a17f6cdf5e17a1e8
Future-Outlier <eric901201@gmail.com>
2026-02-27 00:07:31 +0800

    update
    Signed-off-by: Future-Outlier <eric901201@gmail.com>

--- historyserver/test/support/constant.go
+++ historyserver/test/support/constant.go
@@ -1,13 +1,7 @@
 package support
 
 const (
-	EndpointLogs          = "/api/v0/logs"
-	EndpointLogsFile      = "/api/v0/logs/file"
-	EndpointLogicalActors = "/logical/actors"
-	EndpointLogsStream    = "/api/v0/logs/stream"
-	EndpointTasks         = "/api/v0/tasks"
-	EndpointNodes         = "/nodes"
-	EndpointClusterStatus = "/api/cluster_status"
+	EndpointLogs           = "/api/v0/logs"
 	EndpointLogsFile       = "/api/v0/logs/file"
 	EndpointLogicalActors  = "/logical/actors"
 	EndpointLogsStream     = "/api/v0/logs/stream"
