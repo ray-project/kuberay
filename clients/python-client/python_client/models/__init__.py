e2a9065aa779ef17f73cae61168086c0a5e8f689
Ankush Babbar <ankushbbbr@gmail.com>
2026-03-01 23:47:37 -0800

    ray job k8s model

---
+++ clients/python-client/python_client/models/__init__.py
@@ -0,0 +1,7 @@
+"""
+KubeRay Python Models - Auto-generated from CRD OpenAPI schemas.
+"""
+
+from .kuberay_models import RayJobBuilder, create_ray_job
+
+__all__ = ["RayJobBuilder", "create_ray_job"]
