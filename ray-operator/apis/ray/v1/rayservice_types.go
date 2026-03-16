5eef493d263056d398368d3179a7500cc9a79832
Han-Ju Chen (Future-Outlier) <eric901201@gmail.com>
2026-03-16 12:25:27 +0800

    Revert "[RayService] Rollback Support for Incremental Upgrades (#4109)"
    This reverts commit b8b66d42a5f93c7944fc0b5eb9bead528017c886.

--- ray-operator/apis/ray/v1/rayservice_types.go
+++ ray-operator/apis/ray/v1/rayservice_types.go
@@ -206,8 +206,6 @@ const (
 	RayServiceReady RayServiceConditionType = "Ready"
 	// UpgradeInProgress means the RayService is currently performing a zero-downtime upgrade.
 	UpgradeInProgress RayServiceConditionType = "UpgradeInProgress"
-	// RollbackInProgress means the RayService is currently rolling back an in-progress upgrade to the original cluster state.
-	RollbackInProgress RayServiceConditionType = "RollbackInProgress"
 )
 
 const (
@@ -219,7 +217,6 @@ const (
 	NoPendingCluster               RayServiceConditionReason = "NoPendingCluster"
 	NoActiveCluster                RayServiceConditionReason = "NoActiveCluster"
 	RayServiceValidationFailed     RayServiceConditionReason = "ValidationFailed"
-	TargetClusterChanged           RayServiceConditionReason = "TargetClusterChanged"
 )
 
 // +kubebuilder:object:root=true
