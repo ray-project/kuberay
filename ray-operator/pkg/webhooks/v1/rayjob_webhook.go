e11e1104831164797049d413695e42c83bfe267d
Mark Rossetti <marosset@microsoft.com>
2026-04-16 20:24:48 +0000

    Migrate webhooks from deprecated CustomValidator to admission.Validation[T]
    Signed-off-by: Mark Rossetti <marosset@microsoft.com>

--- ray-operator/pkg/webhooks/v1/rayjob_webhook.go
+++ ray-operator/pkg/webhooks/v1/rayjob_webhook.go
@@ -4,12 +4,10 @@ import (
 	"context"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/webhook"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
 	rayv1 "github.com/ray-project/kuberay/ray-operator/apis/ray/v1"
@@ -21,7 +19,7 @@ var rayJobLog = logf.Log.WithName("rayjob-resource")
 // SetupRayJobWebhookWithManager registers the webhook for RayJob in the manager.
 func SetupRayJobWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr, &rayv1.RayJob{}).
-		WithCustomValidator(&RayJobWebhook{}).
+		WithValidator(&RayJobWebhook{}).
 		Complete()
 }
 
@@ -29,24 +27,22 @@ type RayJobWebhook struct{}
 
 //+kubebuilder:webhook:path=/validate-ray-io-v1-rayjob,mutating=false,failurePolicy=fail,sideEffects=None,groups=ray.io,resources=rayjobs,verbs=create;update,versions=v1,name=vrayjob.kb.io,admissionReviewVersions=v1
 
-var _ webhook.CustomValidator = &RayJobWebhook{}
+var _ admission.Validator[*rayv1.RayJob] = &RayJobWebhook{}
 
-// ValidateCreate implements webhook.CustomValidator so a webhook will be registered for the type
-func (w *RayJobWebhook) ValidateCreate(_ context.Context, obj runtime.Object) (admission.Warnings, error) {
-	rayJob := obj.(*rayv1.RayJob)
+// ValidateCreate implements admission.Validator so a webhook will be registered for the type
+func (w *RayJobWebhook) ValidateCreate(_ context.Context, rayJob *rayv1.RayJob) (admission.Warnings, error) {
 	rayJobLog.Info("validate create", "name", rayJob.Name)
 	return nil, w.validateRayJob(rayJob)
 }
 
-// ValidateUpdate implements webhook.CustomValidator so a webhook will be registered for the type
-func (w *RayJobWebhook) ValidateUpdate(_ context.Context, _ runtime.Object, newObj runtime.Object) (admission.Warnings, error) {
-	rayJob := newObj.(*rayv1.RayJob)
+// ValidateUpdate implements admission.Validator so a webhook will be registered for the type
+func (w *RayJobWebhook) ValidateUpdate(_ context.Context, _ *rayv1.RayJob, rayJob *rayv1.RayJob) (admission.Warnings, error) {
 	rayJobLog.Info("validate update", "name", rayJob.Name)
 	return nil, w.validateRayJob(rayJob)
 }
 
-// ValidateDelete implements webhook.CustomValidator so a webhook will be registered for the type
-func (w *RayJobWebhook) ValidateDelete(_ context.Context, _ runtime.Object) (admission.Warnings, error) {
+// ValidateDelete implements admission.Validator so a webhook will be registered for the type
+func (w *RayJobWebhook) ValidateDelete(_ context.Context, _ *rayv1.RayJob) (admission.Warnings, error) {
 	return nil, nil
 }
 
