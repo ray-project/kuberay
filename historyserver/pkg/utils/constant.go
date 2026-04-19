c937bcdcba22a4ce62bb6d06c1915c4e540b5a7b
400Ping <jiekaichang@apache.org>
2026-04-19 11:24:44 +0800

    refractor constant.go
    Signed-off-by: 400Ping <jiekaichang@apache.org>

--- historyserver/pkg/utils/constant.go
+++ historyserver/pkg/utils/constant.go
@@ -1,12 +1,9 @@
 package utils
 
-import "path"
-
-const TmpRayRoot = "/tmp/ray"
-
-var (
-	RayPrevLogsPath        = path.Join(TmpRayRoot, "prev-logs")
-	RayPersistCompletePath = path.Join(TmpRayRoot, "persist-complete-logs")
-	RaySessionLatestPath   = path.Join(TmpRayRoot, "session_latest")
-	RayNodeIDPath          = path.Join(TmpRayRoot, "raylet_node_id")
+const (
+	TmpRayRoot             = "/tmp/ray"
+	RayPrevLogsPath        = TmpRayRoot + "/prev-logs"
+	RayPersistCompletePath = TmpRayRoot + "/persist-complete-logs"
+	RaySessionLatestPath   = TmpRayRoot + "/session_latest"
+	RayNodeIDPath          = TmpRayRoot + "/raylet_node_id"
 )
