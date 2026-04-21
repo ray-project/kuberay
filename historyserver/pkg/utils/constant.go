4235ac51feeed46b8a22a2d6f7b41e2815c63bd4
400Ping <jiekaichang@apache.org>
2026-04-21 18:39:51 +0800

    [Fix] Make Ray tmp root configurable via RAY_TMP_ROOT
    Signed-off-by: 400Ping <jiekaichang@apache.org>

--- historyserver/pkg/utils/constant.go
+++ historyserver/pkg/utils/constant.go
@@ -1,9 +1,18 @@
 package utils
 
-const (
-	TmpRayRoot             = "/tmp/ray"
+import "os"
+
+var (
+	TmpRayRoot             = getTmpRayRoot()
 	RayPrevLogsPath        = TmpRayRoot + "/prev-logs"
 	RayPersistCompletePath = TmpRayRoot + "/persist-complete-logs"
 	RaySessionLatestPath   = TmpRayRoot + "/session_latest"
 	RayNodeIDPath          = TmpRayRoot + "/raylet_node_id"
 )
+
+func getTmpRayRoot() string {
+	if tmpRoot := os.Getenv("RAY_TMP_ROOT"); tmpRoot != "" {
+		return tmpRoot
+	}
+	return "/tmp/ray"
+}
