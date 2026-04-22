ff63464fbf2f39bf3ea0cf025b1ea16e289d8c7b
dentinyhao <dentinyhao@gmail.com>
2026-04-22 03:04:24 -0700

    Check job deletion status code

--- ray-operator/controllers/ray/utils/dashboardclient/dashboard_httpclient.go
+++ ray-operator/controllers/ray/utils/dashboardclient/dashboard_httpclient.go
@@ -353,6 +353,9 @@ func (r *RayDashboardClient) DeleteJob(ctx context.Context, jobName string) erro
 	}
 	defer resp.Body.Close()
 
+	if resp.StatusCode < 200 || resp.StatusCode > 299 {
+		return fmt.Errorf("DeleteJob fail: %s %s", resp.Status, string(body))
+	}
 	return nil
 }
 
