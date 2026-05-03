edd2f3112e345757376994c91ce212515f8a5b94
machichima <nary12321@gmail.com>
2026-05-03 10:44:30 +0800

    refactor: update error string
    Signed-off-by: machichima <nary12321@gmail.com>

--- ray-operator/controllers/ray/utils/validation.go
+++ ray-operator/controllers/ray/utils/validation.go
@@ -725,7 +725,7 @@ func ValidateRayCronJobSpec(rayCronJob *rayv1.RayCronJob) error {
 	// Validate time zone if set
 	if rayCronJob.Spec.TimeZone != nil {
 		if *rayCronJob.Spec.TimeZone == "" {
-			return fmt.Errorf("timeZone must not be empty string, omit the field to use UTC")
+			return fmt.Errorf("timeZone must not be empty string, omit the field to use the local time zone of the KubeRay operator")
 		}
 		if _, err := time.LoadLocation(*rayCronJob.Spec.TimeZone); err != nil {
 			return fmt.Errorf("invalid time zone: %w", err)
