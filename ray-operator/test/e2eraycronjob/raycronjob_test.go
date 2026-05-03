bb80e5ef5aaa78f2048834ed9349c9b5362077a4
machichima <nary12321@gmail.com>
2026-05-03 10:52:47 +0800

    test: add test to ensure only one rayjob being created
    Signed-off-by: machichima <nary12321@gmail.com>

--- ray-operator/test/e2eraycronjob/raycronjob_test.go
+++ ray-operator/test/e2eraycronjob/raycronjob_test.go
@@ -75,6 +75,37 @@ func TestRayCronJobSuspend(t *testing.T) {
 	})
 }
 
+func TestRayCronJobNoDuplicateRayJob(t *testing.T) {
+	test := With(t)
+	g := NewWithT(t)
+
+	namespace := test.NewTestNamespace()
+	test.T().Run("RayCronJob should not create duplicate RayJobs for the same schedule tick.", func(_ *testing.T) {
+		rayCronJobAC := rayCronJobACTemplate("no-duplicate-raycronjob", namespace.Name, "*/1 * * * *")
+		rayCronJob, err := test.Client().Ray().RayV1().RayCronJobs(namespace.Name).Apply(test.Ctx(), rayCronJobAC, TestApplyOptions)
+		g.Expect(err).NotTo(HaveOccurred())
+		LogWithTimestamp(test.T(), "Created RayCronJob %s/%s successfully", rayCronJob.Namespace, rayCronJob.Name)
+
+		rayCronJob, err = GetRayCronJob(test, rayCronJob.Namespace, rayCronJob.Name)
+		g.Expect(err).NotTo(HaveOccurred())
+		ownerUID := rayCronJob.UID
+
+		// Wait for the first RayJob to be created
+		g.Eventually(func() (int, error) {
+			return countRayJobsOwnedByUID(test.Ctx(), test.Client().Ray(), namespace.Name, ownerUID)
+		}, TestTimeoutMedium, 5*time.Second).Should(BeNumerically(">", 0))
+
+		// Consistently check that no duplicate RayJob is created within the same schedule tick
+		g.Consistently(func() (int, error) {
+			return countRayJobsOwnedByUID(test.Ctx(), test.Client().Ray(), namespace.Name, ownerUID)
+		}, 30*time.Second, 5*time.Second).Should(BeNumerically("<=", 1))
+
+		err = test.Client().Ray().RayV1().RayCronJobs(namespace.Name).Delete(test.Ctx(), rayCronJob.Name, metav1.DeleteOptions{})
+		g.Expect(err).NotTo(HaveOccurred())
+		LogWithTimestamp(test.T(), "Deleted RayCronJob %s/%s successfully", rayCronJob.Namespace, rayCronJob.Name)
+	})
+}
+
 // countRayJobsOwnedByUID counts RayJobs in namespace whose ownerReferences contains ownerUID.
 func countRayJobsOwnedByUID(ctx context.Context, rayClient rayclientset.Interface, namespace string, ownerUID types.UID) (int, error) {
 	list, err := rayClient.RayV1().RayJobs(namespace).List(ctx, metav1.ListOptions{})
