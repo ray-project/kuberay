c8b2d56993a345829712ef04447b129faaeec44f
Ryan O'Leary <ryanaoleary@google.com>
2026-06-16 17:00:00 +0000

    test: wrap RayService updates in Eventually in rayservice_suspend_test.go to prevent 409 conflicts

--- ray-operator/test/e2eincrementalupgrade/rayservice_suspend_test.go
+++ ray-operator/test/e2eincrementalupgrade/rayservice_suspend_test.go
@@ -68,11 +68,15 @@ func TestRayServiceSuspendDuringIncrementalUpgrade(t *testing.T) {
 	}, TestTimeoutMedium).Should(Succeed())
 
 	LogWithTimestamp(test.T(), "Setting Spec.Suspend=true while incremental upgrade is in progress")
-	rayService, err = GetRayService(test, namespace.Name, rayServiceName)
-	g.Expect(err).NotTo(HaveOccurred())
-	rayService.Spec.Suspend = true
-	_, err = test.Client().Ray().RayV1().RayServices(namespace.Name).Update(test.Ctx(), rayService, metav1.UpdateOptions{})
-	g.Expect(err).NotTo(HaveOccurred())
+	g.Eventually(func() error {
+		rayService, err = GetRayService(test, namespace.Name, rayServiceName)
+		if err != nil {
+			return err
+		}
+		rayService.Spec.Suspend = true
+		_, err = test.Client().Ray().RayV1().RayServices(namespace.Name).Update(test.Ctx(), rayService, metav1.UpdateOptions{})
+		return err
+	}, TestTimeoutShort).Should(Succeed())
 
 	LogWithTimestamp(test.T(), "Waiting for the Suspended condition to be True")
 	g.Eventually(RayService(test, rayService.Namespace, rayService.Name), TestTimeoutMedium).
@@ -100,11 +104,15 @@ func TestRayServiceSuspendDuringIncrementalUpgrade(t *testing.T) {
 	}, TestTimeoutMedium).Should(Succeed())
 
 	LogWithTimestamp(test.T(), "Setting Spec.Suspend=false; the controller must recreate Gateway, HTTPRoute, RayCluster, and Services")
-	rayService, err = GetRayService(test, namespace.Name, rayServiceName)
-	g.Expect(err).NotTo(HaveOccurred())
-	rayService.Spec.Suspend = false
-	_, err = test.Client().Ray().RayV1().RayServices(namespace.Name).Update(test.Ctx(), rayService, metav1.UpdateOptions{})
-	g.Expect(err).NotTo(HaveOccurred())
+	g.Eventually(func() error {
+		rayService, err = GetRayService(test, namespace.Name, rayServiceName)
+		if err != nil {
+			return err
+		}
+		rayService.Spec.Suspend = false
+		_, err = test.Client().Ray().RayV1().RayServices(namespace.Name).Update(test.Ctx(), rayService, metav1.UpdateOptions{})
+		return err
+	}, TestTimeoutShort).Should(Succeed())
 
 	g.Eventually(RayService(test, rayService.Namespace, rayService.Name), TestTimeoutMedium).
 		Should(WithTransform(IsRayServiceReady, BeTrue()))
