0c28a2cfd13debaa12c7fa52a109d7795de77653
Pat O'Connor <paoconno@redhat.com>
2026-07-06 22:03:33 +0100

    remove stale FQDN mtls logic from buildHeadPod
    Signed-off-by: Pat O'Connor <paoconno@redhat.com>

--- ray-operator/controllers/ray/raycluster_controller.go
+++ ray-operator/controllers/ray/raycluster_controller.go
@@ -1451,13 +1451,7 @@ func (r *RayClusterReconciler) buildHeadPod(ctx context.Context, instance rayv1.
 	// The Ray head port used by workers to connect to the cluster (GCS server port for Ray >= 1.11.0, Redis port for older Ray.)
 	headPort := common.GetHeadPort(instance.Spec.HeadGroupSpec.RayStartParams)
 	autoscalingEnabled := utils.IsAutoscalingEnabled(&instance.Spec)
-	// Only pass the FQDN when mTLS is enabled so the head advertises a stable DNS name
-	// (which matches a cert SAN) instead of its pod IP. Non-TLS clusters are unaffected.
-	headFQDN := ""
-	if utils.IsTLSEnabled(&instance.Spec) {
-		headFQDN = fqdnRayIP
-	}
-	podConf := common.DefaultHeadPodTemplate(ctx, instance, instance.Spec.HeadGroupSpec, podName, headPort, headFQDN)
+	podConf := common.DefaultHeadPodTemplate(ctx, instance, instance.Spec.HeadGroupSpec, podName, headPort, fqdnRayIP)
 	if len(r.options.HeadSidecarContainers) > 0 {
 		podConf.Spec.Containers = append(podConf.Spec.Containers, r.options.HeadSidecarContainers...)
 	}
