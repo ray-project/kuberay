bca562dbfb91379e25eb0b322d44e3a360405628
Aaron Liang <aaronliang@google.com>
2026-08-05 00:39:35 +0000

    fix ci

--- ray-operator/controllers/ray/rayservice_controller_unit_test.go
+++ ray-operator/controllers/ray/rayservice_controller_unit_test.go
@@ -2634,7 +2634,7 @@ func TestHandleSuspendResumeResetsReadyToInitializing(t *testing.T) {
 	r := &RayServiceReconciler{
 		Client:   fakeClient,
 		Scheme:   scheme.Scheme,
-		Recorder: events.NewFakeRecorder(10),
+		Recorder: &events.FakeRecorder{},
 	}
 
 	_, err := r.handleSuspend(ctx, rs)
