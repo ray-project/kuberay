4af2e32a6d456e2eaebeba6b06c7c03254b0046f
Cheyu Wu <cheyu1220@gmail.com>
2026-08-07 22:42:16 +0800

    chore: use enum
    Signed-off-by: Cheyu Wu <cheyu1220@gmail.com>

--- historyserver/pkg/historyserver/router.go
+++ historyserver/pkg/historyserver/router.go
@@ -447,7 +447,7 @@ func (s *ServerHandler) redirectRequest(req *restful.Request, resp *restful.Resp
 		}
 		// In auth-token mode, drop the client-supplied x-ray-authorization so it cannot bypass
 		// the server-managed token injected below.
-		if s.useAuthTokenMode && strings.EqualFold(key, "x-ray-authorization") {
+		if s.useAuthTokenMode && strings.EqualFold(key, utils.RayAuthHeader) {
 			continue
 		}
 		// Authorization has to be dropped in two cases. In auth-token mode, Ray only falls back to
