3dfa5e55983ff10144b265ee30f4e5569fb581bb
justinyeh1995 <justinyeh1995@gmail.com>
2026-09-02 21:08:54 +0800

    reuse suspend flow for idleTerminate; enhance validation; make generate; make sync
    Signed-off-by: justinyeh1995 <justinyeh1995@gmail.com>

--- ray-operator/apis/ray/v1/raycluster_types.go
+++ ray-operator/apis/ray/v1/raycluster_types.go
@@ -22,6 +22,11 @@ type RayClusterSpec struct {
 	// A suspended RayCluster will have head pods and worker pods deleted.
 	// +optional
 	Suspend *bool `json:"suspend,omitempty"`
+	// IdleTerminate is set to true by the Ray autoscaler when the RayCluster has
+	// had no attached driver for noDriverTimeoutSeconds and the policy is
+	// Suspend. Setting it back to false resumes the RayCluster.
+	// +optional
+	IdleTerminate *bool `json:"idleTerminate,omitempty"`
 	// ManagedBy is an optional configuration for the controller or entity that manages a RayCluster.
 	// The value must be either 'ray.io/kuberay-operator' or 'kueue.x-k8s.io/multikueue'.
 	// The kuberay-operator reconciles a RayCluster which doesn't have this field at all or
@@ -574,6 +579,7 @@ type TopologyLabelMapping struct {
 	MapTo string `json:"mapTo,omitempty"`
 }
 
+// +kubebuilder:validation:Enum=Delete;Suspend
 type NoDriverTimeoutPolicy string
 
 const (
@@ -761,6 +767,8 @@ const (
 	RayClusterSuspending RayClusterConditionType = "RayClusterSuspending"
 	// RayClusterSuspended is set to true when all Pods belonging to a suspending RayCluster are deleted. Note that RayClusterSuspending and RayClusterSuspended cannot both be true at the same time.
 	RayClusterSuspended RayClusterConditionType = "RayClusterSuspended"
+	// RayClusterIdleTerminated indicates that the RayCluster was suspended because it had no attached driver for the configured timeout.
+	RayClusterIdleTerminated RayClusterConditionType = "RayClusterIdleTerminated"
 )
 
 // HeadInfo gives info about head
--- ray-operator/apis/ray/v1/zz_generated.deepcopy.go
+++ ray-operator/apis/ray/v1/zz_generated.deepcopy.go
@@ -681,6 +681,11 @@ func (in *RayClusterSpec) DeepCopyInto(out *RayClusterSpec) {
 		*out = new(bool)
 		**out = **in
 	}
+	if in.IdleTerminate != nil {
+		in, out := &in.IdleTerminate, &out.IdleTerminate
+		*out = new(bool)
+		**out = **in
+	}
 	if in.ManagedBy != nil {
 		in, out := &in.ManagedBy, &out.ManagedBy
 		*out = new(string)
