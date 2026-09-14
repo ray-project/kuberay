9fe6cc41b7ae330e8f3457f588103c7d90961ae2
chenshi5012 <chenshi5012@163.com>
2026-09-14 06:22:31 +0000

    [APIServer] Restrict compute template ConfigMap access
    Signed-off-by: chenshi5012 <chenshi5012@163.com>

--- apiserver/pkg/manager/resource_manager_test.go
+++ apiserver/pkg/manager/resource_manager_test.go
@@ -25,6 +25,9 @@ func TestPopulateComputeTemplate(t *testing.T) {
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      headConfigMapName,
 			Namespace: namespace,
+			Labels: map[string]string{
+				"ray.io/config-type": "compute-template",
+			},
 		},
 		Data: map[string]string{
 			"name":      headConfigMapName,
@@ -37,6 +40,9 @@ func TestPopulateComputeTemplate(t *testing.T) {
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      workerConfigMapName,
 			Namespace: namespace,
+			Labels: map[string]string{
+				"ray.io/config-type": "compute-template",
+			},
 		},
 		Data: map[string]string{
 			"name":      workerConfigMapName,
@@ -79,4 +85,94 @@ func TestPopulateComputeTemplate(t *testing.T) {
 	assert.Len(t, computeTemplates, 2)
 	assert.Contains(t, computeTemplates, headConfigMapName)
 	assert.Contains(t, computeTemplates, workerConfigMapName)
+}
+
+func TestGetComputeTemplateRequiresComputeTemplateLabel(t *testing.T) {
+	ctx := context.Background()
+	namespace := "test-namespace"
+	unlabelledConfigMap := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "unlabelled",
+			Namespace: namespace,
+		},
+	}
+	computeTemplateConfigMap := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "compute-template",
+			Namespace: namespace,
+			Labels: map[string]string{
+				"ray.io/config-type": "compute-template",
+			},
+		},
+	}
+
+	configMapClient := kubernetesfake.NewClientset(unlabelledConfigMap, computeTemplateConfigMap).CoreV1().ConfigMaps(namespace)
+	ctrl := gomock.NewController(t)
+	mockClientManager := NewMockClientManagerInterface(ctrl)
+	mockKubeClient := client.NewMockKubernetesClientInterface(ctrl)
+	mockClientManager.EXPECT().KubernetesClient().Return(mockKubeClient).Times(2)
+	mockKubeClient.EXPECT().ConfigMapClient(namespace).Return(configMapClient).Times(2)
+
+	resourceManager := NewResourceManager(mockClientManager)
+
+	configMap, err := resourceManager.GetComputeTemplate(ctx, unlabelledConfigMap.Name, namespace)
+	require.Error(t, err)
+	assert.Nil(t, configMap)
+
+	configMap, err = resourceManager.GetComputeTemplate(ctx, computeTemplateConfigMap.Name, namespace)
+	require.NoError(t, err)
+	assert.Equal(t, computeTemplateConfigMap.Name, configMap.Name)
+}
+
+func TestDeleteComputeTemplateDoesNotDeleteUnlabelledConfigMap(t *testing.T) {
+	ctx := context.Background()
+	namespace := "test-namespace"
+	configMap := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "unlabelled",
+			Namespace: namespace,
+		},
+	}
+	configMapClient := kubernetesfake.NewClientset(configMap).CoreV1().ConfigMaps(namespace)
+
+	ctrl := gomock.NewController(t)
+	mockClientManager := NewMockClientManagerInterface(ctrl)
+	mockKubeClient := client.NewMockKubernetesClientInterface(ctrl)
+	mockClientManager.EXPECT().KubernetesClient().Return(mockKubeClient)
+	mockKubeClient.EXPECT().ConfigMapClient(namespace).Return(configMapClient)
+
+	resourceManager := NewResourceManager(mockClientManager)
+	err := resourceManager.DeleteComputeTemplate(ctx, configMap.Name, namespace)
+	require.Error(t, err)
+
+	_, err = configMapClient.Get(ctx, configMap.Name, metav1.GetOptions{})
+	require.NoError(t, err)
+}
+
+func TestPopulateComputeTemplateRejectsUnlabelledConfigMap(t *testing.T) {
+	ctx := context.Background()
+	namespace := "test-namespace"
+	configMap := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "unlabelled",
+			Namespace: namespace,
+		},
+	}
+	clusterSpec := &api.ClusterSpec{
+		HeadGroupSpec: &api.HeadGroupSpec{
+			ComputeTemplate: configMap.Name,
+		},
+	}
+	configMapClient := kubernetesfake.NewClientset(configMap).CoreV1().ConfigMaps(namespace)
+
+	ctrl := gomock.NewController(t)
+	mockClientManager := NewMockClientManagerInterface(ctrl)
+	mockKubeClient := client.NewMockKubernetesClientInterface(ctrl)
+	mockClientManager.EXPECT().KubernetesClient().Return(mockKubeClient)
+	mockKubeClient.EXPECT().ConfigMapClient(namespace).Return(configMapClient)
+
+	resourceManager := NewResourceManager(mockClientManager)
+	computeTemplates, err := resourceManager.PopulateComputeTemplate(ctx, clusterSpec, namespace)
+	require.Error(t, err)
+	assert.Nil(t, computeTemplates)
 }
